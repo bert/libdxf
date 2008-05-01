e528db1c64c88d3999a4e360772f0e93d8caadc3
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-05-01 22:19:06 +0200

    Added polyline.c and polyline.h

---
+++ src/polyline.c
@@ -0,0 +1,260 @@
+/*!
+ * \file polyline.c
+ * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief DXF polyline entity (\c POLYLINE).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+#include "global.h"
+#include "polyline.h"
+
+/*!
+ * \brief Write DXF output to a file for a polyline entity.
+ */
+int
+dxf_write_polyline
+(
+        FILE *fp,
+                /*!< file pointer to output file (or device). */
+        int id_code,
+                /*!< group code = 5. */
+        char *linetype,
+                /*!< group code = 6\n
+                 * optional, if omitted defaults to \c BYLAYER. */
+        char *layer,
+                /*!< group code = 8. */
+        double x0,
+                /*!< group code = 10\n
+                 * if omitted defaults to 0.0. */
+        double y0,
+                /*!< group code = 20\n
+                 * if omitted defaults to 0.0. */
+        double z0,
+                /*!< group code = 30\n
+                 * default elevation for vertices. */
+        double thickness,
+                /*!< group code = 39\n
+                 * optional, if omitted defaults to 0.0. */
+        double start_width,
+                /*!< group code = 40\n
+                 * optional, if omitted defaults to 0.0. */
+        double end_width,
+                /*!< group code = 41\n
+                 * optional, if omitted defaults to 0.0. */
+        int color,
+                /*!< group code = 62\n
+                 * optional, if omitted defaults to \c BYLAYER. */
+        int vertices_follow,
+                /*!< group code = 66\n
+                 * mandatory, always 1 (one or more vertices make up a
+                 * polyline). */
+        int paperspace,
+                /*!< group code = 67\n
+                 * optional, if omitted defaults to 0 (modelspace). */
+        int flag,
+                /*!< group code = 70\n
+                 * optional, if omitted defaults to 0. */
+        int polygon_mesh_M_vertex_count,
+                /*!< group code = 71\n
+                 * optional, if omitted defaults to 0. */
+        int polygon_mesh_N_vertex_count,
+                /*!< group code = 72\n
+                 * optional, if omitted defaults to 0. */
+        int smooth_M_surface_density,
+                /*!< group code = 73\n
+                 * optional, if omitted defaults to 0. */
+        int smooth_N_surface_density,
+                /*!< group code = 74\n
+                 * optional, if omitted defaults to 0. */
+        int surface_type
+                /*!< group code = 75\n
+                 * optional, if omitted defaults to 0. */
+)
+{
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Entering dxf_write_polyline () function.\n", __FILE__, __LINE__);
+#endif
+        char *dxf_entity_name = strdup ("POLYLINE");
+
+        if (x0 != 0.0)
+        {
+                fprintf (stderr, "Error: start point has an invalid X-value for the %s entity with id-code: %x\n", dxf_entity_name, id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        if (y0 != 0.0)
+        {
+                fprintf (stderr, "Error: start point has an invalid Y-value for the %s entity with id-code: %x\n", dxf_entity_name, id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        if (strcmp (layer, "") == 0)
+        {
+                fprintf (stderr, "Warning: empty layer string for the %s entity with id-code: %x\n", dxf_entity_name, id_code);
+                fprintf (stderr, "    %s entity is relocated to layer 0\n", dxf_entity_name);
+                layer = strdup (DXF_DEFAULT_LAYER);
+        }
+        if (vertices_follow != 1)
+        {
+                fprintf (stderr, "Error: vertices follow flag has an invalid value for the %s entity with id-code: %x\n", dxf_entity_name, id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        fprintf (fp, "  0\n%s\n", dxf_entity_name);
+        if (id_code != -1)
+        {
+                fprintf (fp, "  5\n%x\n", id_code);
+        }
+        if (strcmp (linetype, DXF_DEFAULT_LINETYPE) != 0)
+        {
+                fprintf (fp, "  6\n%s\n", linetype);
+        }
+        fprintf (fp, "  8\n%s\n", layer);
+        fprintf (fp, " 10\n%f\n", x0);
+        fprintf (fp, " 20\n%f\n", y0);
+        fprintf (fp, " 30\n%f\n", z0);
+        if (thickness != 0.0)
+        {
+                fprintf (fp, " 39\n%f\n", thickness);
+        }
+        if (start_width != 0.0)
+        {
+                fprintf (fp, " 40\n%f\n", start_width);
+        }
+        if (end_width != 0.0)
+        {
+                fprintf (fp, " 41\n%f\n", end_width);
+        }
+        if (color != DXF_COLOR_BYLAYER)
+        {
+                fprintf (fp, " 62\n%d\n", color);
+        }
+        fprintf (fp, " 66\n%d\n", vertices_follow);
+        if (paperspace == DXF_PAPERSPACE)
+        {
+                fprintf (fp, " 67\n%d\n", DXF_PAPERSPACE);
+        }
+        fprintf (fp, " 70\n%d\n", flag);
+        fprintf (fp, " 71\n%d\n", polygon_mesh_M_vertex_count);
+        fprintf (fp, " 72\n%d\n", polygon_mesh_N_vertex_count);
+        fprintf (fp, " 73\n%d\n", smooth_M_surface_density);
+        fprintf (fp, " 74\n%d\n", smooth_N_surface_density);
+        fprintf (fp, " 75\n%d\n", surface_type);
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_write_polyline () function.\n", __FILE__, __LINE__);
+#endif
+}
+
+
+/*!
+ * \brief Write DXF output to fp for a polyline entity.
+ */
+int
+dxf_write_polyline_struct
+(
+        FILE *fp,
+                /*!< file pointer to output file (or device). */
+        DxfPolyline dxf_polyline
+                /*!< DXF polyline entity. */
+)
+{
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Entering dxf_write_polyline2 () function.\n", __FILE__, __LINE__);
+#endif
+        char *dxf_entity_name = strdup ("POLYLINE");
+
+        if (dxf_polyline.x0 != 0.0)
+        {
+                fprintf (stderr, "Error: start point has an invalid X-value for the %s entity with id-code: %x\n", dxf_entity_name, dxf_polyline.id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        if (dxf_polyline.y0 != 0.0)
+        {
+                fprintf (stderr, "Error: start point has an invalid Y-value for the %s entity with id-code: %x\n", dxf_entity_name, dxf_polyline.id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        if (strcmp (dxf_polyline.layer, "") == 0)
+        {
+                fprintf (stderr, "Warning: empty layer string for the %s entity with id-code: %x\n", dxf_entity_name, dxf_polyline.id_code);
+                fprintf (stderr, "    %s entity is relocated to layer 0\n", dxf_entity_name);
+                dxf_polyline.layer = strdup (DXF_DEFAULT_LAYER);
+        }
+        if (dxf_polyline.vertices_follow != 1)
+        {
+                fprintf (stderr, "Error: vertices follow flag has an invalid value for the %s entity with id-code: %x\n", dxf_entity_name, dxf_polyline.id_code);
+                dxf_skip_entity (dxf_entity_name);
+                return (EXIT_FAILURE);
+        }
+        fprintf (fp, "  0\n%s\n", dxf_entity_name);
+        if (dxf_polyline.id_code != -1)
+        {
+                fprintf (fp, "  5\n%x\n", dxf_polyline.id_code);
+        }
+        if (strcmp (dxf_polyline.linetype, DXF_DEFAULT_LINETYPE) != 0)
+        {
+                fprintf (fp, "  6\n%s\n", dxf_polyline.linetype);
+        }
+        fprintf (fp, "  8\n%s\n", dxf_polyline.layer);
+        fprintf (fp, " 10\n%f\n", dxf_polyline.x0);
+        fprintf (fp, " 20\n%f\n", dxf_polyline.y0);
+        fprintf (fp, " 30\n%f\n", dxf_polyline.z0);
+        if (dxf_polyline.thickness != 0.0)
+        {
+                fprintf (fp, " 39\n%f\n", dxf_polyline.thickness);
+        }
+        if (dxf_polyline.start_width != 0.0)
+        {
+                fprintf (fp, " 40\n%f\n", dxf_polyline.start_width);
+        }
+        if (dxf_polyline.end_width != 0.0)
+        {
+                fprintf (fp, " 41\n%f\n", dxf_polyline.end_width);
+        }
+        if (dxf_polyline.color != DXF_COLOR_BYLAYER)
+        {
+                fprintf (fp, " 62\n%d\n", dxf_polyline.color);
+        }
+        fprintf (fp, " 66\n%d\n", dxf_polyline.vertices_follow);
+        if (dxf_polyline.paperspace == DXF_PAPERSPACE)
+        {
+                fprintf (fp, " 67\n%d\n", DXF_PAPERSPACE);
+        }
+        fprintf (fp, " 70\n%d\n", dxf_polyline.flag);
+        fprintf (fp, " 71\n%d\n", dxf_polyline.polygon_mesh_M_vertex_count);
+        fprintf (fp, " 72\n%d\n", dxf_polyline.polygon_mesh_N_vertex_count);
+        fprintf (fp, " 73\n%d\n", dxf_polyline.smooth_M_surface_density);
+        fprintf (fp, " 74\n%d\n", dxf_polyline.smooth_N_surface_density);
+        fprintf (fp, " 75\n%d\n", dxf_polyline.surface_type);
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_write_polyline2 () function.\n", __FILE__, __LINE__);
+#endif
+        return (EXIT_SUCCESS);
+}
+
+/* EOF */
---
+++ src/polyline.h
@@ -0,0 +1,160 @@
+/*!
+ * \file polyline.h
+ * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief Definition of a DXF polyline entity (\c PLINE).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+#include "global.h"
+
+/*!
+ * \brief Definition of a DXF polyline entity.
+ *
+ * A polyface mesh is represented in \c DXF as a variant of a Polyline
+ * entity.\n
+ * The Polyline header is identified as introducing a polyface mesh by the
+ * presence of the 64 bit in the Polyline flags (70) group.\n
+ * The 71 group specifies the number of vertices in the mesh, and the 72
+ * group, the number of faces.\n
+ * While these counts are correct for all meshes created with the \c PFACE
+ * command, applications are not required to place correct values in these
+ * fields, and AutoCAD actually never relies upon their accuracy.\n
+ * \n
+ * Following the Polyline header is a sequence of Vertex entities that
+ * specify the vertex coordinates and faces that compose the mesh.\n
+ * Vertices such as these are described in the following subsection on
+ * Vertex.\n
+ * \n
+ * Applications might want to represent polygons with an arbitrarily large
+ * number of sides in polyface meshes.\n
+ * However, the AutoCAD entity structure imposes a limit on the number of
+ * vertices that a given face entity can specify.\n
+ * You can represent more complex polygons by decomposing them into
+ * triangular wedges.\n
+ * Their edges should be made invisible to prevent visible artifacts of this
+ * subdivision from being drawn.\n
+ * The \c PFACE command performs this subdivision automatically, but when
+ * applications generate polyface meshes directly, the applications must do
+ * this themselves.\n
+ * \n
+ * The number of vertices per face is the key parameter in this subdivision
+ * process.\n
+ * The \c PFACEVMAX system variable provides an application with the number
+ * of vertices per face entity.\n
+ * This value is read-only, and is set to 4.\n
+ * \n
+ * Polyface meshes created with the \c PFACE command are always generated
+ * with all the vertex coordinate entities first, followed by the face
+ * definition entities.\n
+ * The code within AutoCAD that processes polyface meshes does not, at
+ * present, require this ordering; it works even with interleaved vertex
+ * coordinates and face definitions as long as no face specifies a vertex
+ * with an index that appears after it in the database.\n
+ * Programs that read polyface meshes from \c DXF would be wise to be as
+ * tolerant of odd vertex and face ordering as AutoCAD is.\n
+ */
+typedef struct
+dxf_polyline
+{
+        int id_code;
+                /*!< group code = 5. */
+        char *linetype;
+                /*!< group code = 6\n
+                 * optional, defaults to \c BYLAYER. */
+        char *layer;
+                /*!< group code = 8. */
+        double x0;
+                /*!< group code = 10\n
+                 * defaults to 0.0. */
+        double y0;
+                /*!< group code = 20\n
+                 * defaults to 0.0. */
+        double z0;
+                /*!< group code = 30\n
+                 * default elevation for vertices. */
+        double thickness;
+                /*!< group code = 39\n
+                 * optional, defaults to 0.0. */
+        double start_width;
+                /*!< group code = 40\n
+                 * optional, defaults to 0.0\n
+                 * the default widths apply to any vertex that doesn't supply
+                 * widths.\n */
+        double end_width;
+                /*!< group code = 41\n
+                 * optional, defaults to 0.0\n
+                 * the default widths apply to any vertex that doesn't supply
+                 * widths.\n */
+        int color;
+                /*!< group code = 62\n
+                 * optional, defaults to \c BYLAYER. */
+        int vertices_follow;
+                /*!< group code = 66\n
+                 * always 1\n
+                 * The "vertices follow" flag is always 1, indicating that a
+                 * series of Vertex entities is expected to follow the
+                 * \c Polyline, terminated by a "sequence end" entity. */
+        int paperspace;
+                /*!< group code = 67\n
+                 * optional, defaults to 0 (modelspace). */
+        int flag;
+                /*!< group code = 70\n
+                 * optional, defaults to 0\n
+                 * bit coded:\n
+                 * 1 = This is a closed Polyline (or a polygon mesh closed in
+                 * the M direction)\n
+                 * 2 = Curve-fit vertices have been added\n
+                 * 4 = Spline-fit vertices have been added\n
+                 * 8 = This is a 3D Polyline\n
+                 * 16 = This is a 3D polygon mesh\n
+                 * 32 = The polygon mesh is closed in the N direction\n
+                 * 64 = This Polyline is a polyface mesh\n
+                 * 128 = The linetype pattern is generated continuously
+                 *       around the vertices of this Polyline\n. */
+        int polygon_mesh_M_vertex_count;
+                /*!< group code = 71\n
+                 * optional, defaults to 0. */
+        int polygon_mesh_N_vertex_count;
+                /*!< group code = 72\n
+                 * optional, defaults to 0. */
+        int smooth_M_surface_density;
+                /*!< group code = 73\n
+                 * optional, defaults to 0. */
+        int smooth_N_surface_density;
+                /*!< group code = 74\n
+                 * optional, defaults to 0. */
+        int surface_type;
+                /*!< group code = 75\n
+                 * optional, defaults to 0\n
+                 * bit coded:\n
+                 * 0 = no smooth surface fitted\n
+                 * 5 = quadratic B-spline surface\n
+                 * 6 = cubic B-spline surface\n
+                 * 8 = Bezier surface. */                       |
+} DxfPolyline, * DxfPolylinePtr;
+
+/* EOF */
