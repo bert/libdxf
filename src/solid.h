bbd3eae5411f35af6870a3e4bac9e3ec3e634661
Bert Timmerman <bert.timmerman@xs4all.nl>
2008-05-18 16:22:24 +0200

    Added solid.h

---
+++ src/solid.h
@@ -0,0 +1,104 @@
+/*!
+ * \file solid.h
+ * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief Definition of a DXF solid entity (\c SOLID).
+ *
+ * A DXF \c SOLID entity is a filled shape made with up to four point
+ * (X, Y, Z)-coordinates.\n
+ * A filled triangle can be formed by duplicating the third coordinate at the
+ * fourth coordinate position.\n
+ * A diabolo shaped solid can be formed by "twisting" the \SHAPE form in it's
+ * plane, by "swapping" the third and fourth coordinate set.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+#include "global.h"
+
+/*!
+ * \brief DXF definition of an AutoCAD solid entity.
+ *
+ * If only three points are entered (forming a triangular \c SOLID), the
+ * third and fourth points will coincide.
+ */
+typedef struct
+dxf_solid
+{
+        int id_code;
+                /*!< group code = 5. */
+        char *linetype;
+                /*!< group code = 6\n
+                 * optional, defaults to \c BYLAYER. */
+        char *layer;
+                /*!< group code = 8. */
+        double x0;
+                /*!< group code = 10\n
+                 * base point. */
+        double y0;
+                /*!< group code = 20\n
+                 * base point. */
+        double z0;
+                /*!< group code = 30\n
+                 * base point. */
+        double x1;
+                /*!< group code = 11\n
+                 * alignment point. */
+        double y1;
+                /*!< group code = 21\n
+                 * alignment point. */
+        double z1;
+                /*!< group code = 31\n
+                 * alignment point. */
+        double x2;
+                /*!< group code = 12\n
+                 * alignment point. */
+        double y2;
+                /*!< group code = 22\n
+                 * alignment point. */
+        double z2;
+                /*!< group code = 32\n
+                 * alignment point. */
+        double x3;
+                /*!< group code = 13\n
+                 * alignment point. */
+        double y3;
+                /*!< group code = 23\n
+                 * alignment point. */
+        double z3;
+                /*!< group code = 33\n
+                 * alignment point. */
+        double thickness;
+                /*!< group code = 39\n
+                 * optional, defaults to 0.0. */
+        int color;
+                /*!< group code = 62\n
+                 * optional, defaults to \c BYLAYER. */
+        int paperspace;
+                /*!< group code = 67\n
+                 * optional, defaults to 0 (modelspace). */
+} DxfSolid, * DxfSolidPtr;
+
+/* EOF */
