60f420d1261ecbec17cc95287a30f99591c3eb6e
Bert Timmerman <bert.timmerman@xs4all.nl>
2009-09-30 10:19:16 +0200

    Bumped the year on the coyright notice.
    Reformatted the GPL notice.

--- src/color.h
+++ src/color.h
@@ -1,6 +1,6 @@
 /*!
  * \file color.h
- * \author Copyright (C) 2008 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008 ... 2009 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  * \brief Definition of DXF color values.
  *
  * These definitions contain the following:\n
@@ -11,20 +11,19 @@
  * <hr>
  * <h1><b>Copyright Notices.</b></h1>\n
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License,
- * or (at your option) any later version.\n\n
- * This program is distributed in the hope that it will be useful, but
- * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
- * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
- * See the GNU General Public License for more details.\n\n
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to:\n
- * Free Software Foundation, Inc.,\n
- * 59 Temple Place,\n
- * Suite 330,\n
- * Boston,\n
- * MA 02111 USA.\n
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.\n
+ * \n
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.\n
+ * \n
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.\n
  * \n
  * Drawing eXchange Format (DXF) is a defacto industry standard for the
  * exchange of drawing files between various Computer Aided Drafting
