562121f04112e91128ad0aa5bd80f7b3fb04a13e
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-10-16 21:49:18 +0200

    LWPOLYLINE: remove polygon leftover bloat.

--- src/lwpolyline.h
+++ src/lwpolyline.h
@@ -42,49 +42,6 @@
 
 /*!
  * \brief Definition of a DXF light weight polyline entity.
- *
- * A polyface mesh is represented in \c DXF as a variant of a Polyline
- * entity.\n
- * The Polyline header is identified as introducing a polyface mesh by the
- * presence of the 64 bit in the Polyline flags (70) group.\n
- * The 71 group specifies the number of vertices in the mesh, and the 72
- * group, the number of faces.\n
- * While these counts are correct for all meshes created with the \c PFACE
- * command, applications are not required to place correct values in these
- * fields, and AutoCAD actually never relies upon their accuracy.\n
- * \n
- * Following the Polyline header is a sequence of Vertex entities that
- * specify the vertex coordinates and faces that compose the mesh.\n
- * Vertices such as these are described in the following subsection on
- * Vertex.\n
- * \n
- * Applications might want to represent polygons with an arbitrarily large
- * number of sides in polyface meshes.\n
- * However, the AutoCAD entity structure imposes a limit on the number of
- * vertices that a given face entity can specify.\n
- * You can represent more complex polygons by decomposing them into
- * triangular wedges.\n
- * Their edges should be made invisible to prevent visible artifacts of this
- * subdivision from being drawn.\n
- * The \c PFACE command performs this subdivision automatically, but when
- * applications generate polyface meshes directly, the applications must do
- * this themselves.\n
- * \n
- * The number of vertices per face is the key parameter in this subdivision
- * process.\n
- * The \c PFACEVMAX system variable provides an application with the number
- * of vertices per face entity.\n
- * This value is read-only, and is set to 4.\n
- * \n
- * Polyface meshes created with the \c PFACE command are always generated
- * with all the vertex coordinate entities first, followed by the face
- * definition entities.\n
- * The code within AutoCAD that processes polyface meshes does not, at
- * present, require this ordering; it works even with interleaved vertex
- * coordinates and face definitions as long as no face specifies a vertex
- * with an index that appears after it in the database.\n
- * Programs that read polyface meshes from \c DXF would be wise to be as
- * tolerant of odd vertex and face ordering as AutoCAD is.\n
  */
 typedef struct
 dxf_lwpolyline
