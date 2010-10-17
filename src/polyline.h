a7ce53bbb20b8871b995387a067b88e80b6a720c
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-10-17 10:46:13 +0200

    POLYLINE: added forward declarations.

--- src/polyline.h
+++ src/polyline.h
@@ -164,9 +164,40 @@ dxf_polyline
 
 
 DxfPolyline *
+dxf_polyline_new ();
+DxfPolyline *
 dxf_polyline_init
 (
         DxfPolyline *dxf_polyline
+);
+int
+dxf_polyline_write_lowlevel
+(
+        FILE *fp,
+        int id_code,
+        char *linetype,
+        char *layer,
+        double x0,
+        double y0,
+        double z0,
+        double thickness,
+        double start_width,
+        double end_width,
+        int color,
+        int vertices_follow,
+        int paperspace,
+        int flag,
+        int polygon_mesh_M_vertex_count,
+        int polygon_mesh_N_vertex_count,
+        int smooth_M_surface_density,
+        int smooth_N_surface_density,
+        int surface_type
+);
+int
+dxf_polyline_write
+(
+        FILE *fp,
+        DxfPolyline dxf_polyline
 );
 
 
