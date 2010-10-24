e746f73632f2f332356ce71fe812e27883206324
Bert Timmerman <bert.timmerman@xs4all.nl>
2010-10-24 10:00:04 +0200

    3DFACE: added 3dface.[ch] with 3DFACE typedef and dxf_3dface_write().

---
+++ src/3dface.c
@@ -0,0 +1,107 @@
+/*!
+ * \file 3dface.c
+ * \author Copyright (C) 2010 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief Functions for a DXF 3D face entity (\c 3DFACE).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#include "3dface.h"
+
+
+/*!
+ * \brief Write DXF output to a file for a 3D face entity (\c 3DFACE).
+ *
+ * \return \c EXIT_SUCCESS when done, or \c EXIT_FAILURE when an error
+ * occurred.
+ */
+int
+dxf_3dface_write
+(
+        FILE *fp,
+                /*!< file pointer to output file (or device). */
+        Dxf3dface dxf_3dface
+                /*!< DXF 3D face entity. */
+)
+{
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Entering dxf_3dface_write () function.\n",
+                __FILE__, __LINE__);
+#endif
+        char *dxf_entity_name = strdup ("3DFACE");
+
+        if (strcmp (dxf_3dface.common.layer, "") == 0)
+        {
+                fprintf (stderr, "Warning in dxf_3dface_write () empty layer string for the %s entity with id-code: %x\n",
+                        dxf_entity_name, dxf_3dface.common.id_code);
+                fprintf (stderr, "    %s entity is relocated to layer 0",
+                        dxf_entity_name);
+                dxf_3dface.common.layer = strdup (DXF_DEFAULT_LAYER);
+        }
+        fprintf (fp, "  0\n%s\n", dxf_entity_name);
+        if (dxf_3dface.common.id_code != -1)
+        {
+                fprintf (fp, "  5\n%x\n", dxf_3dface.common.id_code);
+        }
+        if (strcmp (dxf_3dface.common.linetype, DXF_DEFAULT_LINETYPE) != 0)
+        {
+                fprintf (fp, "  6\n%s\n", dxf_3dface.common.linetype);
+        }
+        fprintf (fp, "  8\n%s\n", dxf_3dface.common.layer);
+        fprintf (fp, " 10\n%f\n", dxf_3dface.x0);
+        fprintf (fp, " 20\n%f\n", dxf_3dface.y0);
+        fprintf (fp, " 30\n%f\n", dxf_3dface.z0);
+        fprintf (fp, " 11\n%f\n", dxf_3dface.x1);
+        fprintf (fp, " 21\n%f\n", dxf_3dface.y1);
+        fprintf (fp, " 31\n%f\n", dxf_3dface.z1);
+        fprintf (fp, " 12\n%f\n", dxf_3dface.x2);
+        fprintf (fp, " 22\n%f\n", dxf_3dface.y2);
+        fprintf (fp, " 32\n%f\n", dxf_3dface.z2);
+        fprintf (fp, " 13\n%f\n", dxf_3dface.x3);
+        fprintf (fp, " 23\n%f\n", dxf_3dface.y3);
+        fprintf (fp, " 33\n%f\n", dxf_3dface.z3);
+        if (dxf_3dface.common.thickness != 0.0)
+        {
+                fprintf (fp, " 39\n%f\n", dxf_3dface.common.thickness);
+        }
+        if (dxf_3dface.common.color != DXF_COLOR_BYLAYER)
+        {
+                fprintf (fp, " 62\n%d\n", dxf_3dface.common.color);
+        }
+        if (dxf_3dface.common.paperspace == DXF_PAPERSPACE)
+        {
+                fprintf (fp, " 67\n%d\n", DXF_PAPERSPACE);
+        }
+#if DEBUG
+        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_3dface_write () function.\n",
+                __FILE__, __LINE__);
+#endif
+        return (EXIT_SUCCESS);
+}
+
+
+/* EOF */
---
+++ src/3dface.h
@@ -0,0 +1,107 @@
+/*!
+ * \file 3dface.h
+ * \author Copyright (C) 2010 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief Header file for a DXF 3D face entity (\c 3DFACE).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef _3DFACE_H
+#define _3DFACE_H
+
+
+#include "global.h"
+#include "entity.h"
+
+
+/*!
+ * \brief DXF definition of an AutoCAD 3D face entity (\c 3DFACE).
+ */
+typedef struct
+dxf_3dface
+{
+        DxfEntity common;
+                /*!< common properties for DXF entities. */
+        double x0;
+                /*!< group code = 10\n
+                 * base point. */
+        double y0;
+                /*!< group code = 20\n
+                 * base point. */
+        double z0;
+                /*!< group code = 30\n
+                 * base point. */
+        double x1;
+                /*!< group code = 11\n
+                 * alignment point. */
+        double y1;
+                /*!< group code = 21\n
+                 * alignment point. */
+        double z1;
+                /*!< group code = 31\n
+                 * alignment point. */
+        double x2;
+                /*!< group code = 12\n
+                 * alignment point. */
+        double y2;
+                /*!< group code = 22\n
+                 * alignment point. */
+        double z2;
+                /*!< group code = 32\n
+                 * alignment point. */
+        double x3;
+                /*!< group code = 13\n
+                 * alignment point. */
+        double y3;
+                /*!< group code = 23\n
+                 * alignment point. */
+        double z3;
+                /*!< group code = 33\n
+                 * alignment point. */
+        int flag;
+                /*!< group code = 70\n
+                 * optional, defaults to 0\n
+                 * bit coded:\n
+                 * 1 = First edge is invisible\n
+                 * 2 = Second edge is invisible\n
+                 * 4 = Third edge is invisible\n
+                 * 8 = Fourth edge is invisible\n */
+} Dxf3dface, * Dxf3dfacePtr;
+
+
+int
+dxf_3dface_write
+(
+        FILE *fp,
+        Dxf3dface dxf_3dface
+);
+
+
+#endif /* _3DFACE_H */
+
+
+/* EOF */
