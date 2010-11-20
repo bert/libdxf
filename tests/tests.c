e231f1fd68edefc37c1bcc0ffdb1dbd5e851ae94
lmatos <gass@otiliamatos.ath.cx>
2010-11-20 19:08:01 +0800

    TESTS: Added the copyright notice to tests.c

--- tests/tests.c
+++ tests/tests.c
@@ -1,3 +1,33 @@
+/*!
+ * \file tests.c
+ * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.\n
+ * \brief Tests for libdxf.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
 
 int main (void)
 {
