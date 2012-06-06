1b332f4a45a89a02e510534dac81408e50b57d3f
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-06-06 22:45:54 +0200

    COMMENT: changed function name dxf_write_comment() to dxf_comment_write().
    - added a header file.
    - changes in white space.
    
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

--- src/comment.h
+++ src/comment.h
@@ -1,7 +1,7 @@
 /*!
- * \file comment.c
- * \author Copyright (C) 2008..2012 by Bert Timmerman <bert.timmerman@xs4all.nl>.
- * \brief DXF comment entity (\c COMMENT).
+ * \file comment.h
+ * \author Copyright (C) 2008 ... 2012 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \brief Header file for a DXF comment entity (\c COMMENT).
  *
  * <hr>
  * <h1><b>Copyright Notices.</b></h1>\n
@@ -29,34 +29,19 @@
  * <hr>
  */
 
-#include "comment.h"
 
-/*!
- * \brief Write DXF output to fp for a comment.
- *
- * The 999 group code indicates that the following line is a comment string.\n
- * DXFOUT does not currently include such groups in a DXF output file, but
- * DXFIN honors them and ignores the comments.\n
- * Thus, you can use the 999 group to include comments in a DXF file you've
- * edited.
- */
+#ifndef _COMMENT_H
+#define _COMMENT_H
+
+
+#include "global.h"
+
+
 int
-dxf_comment_write
-(
-        FILE *fp,
-                /*!< file pointer to output file (or device). */
-        char *comment_string
-                /*!< comment string to be writen to fp. */
-)
-{
-#if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_comment_write () function.\n", __FILE__, __LINE__);
-#endif
-        fprintf (fp, "999\n%s\n", comment_string);
-#if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_comment_write () function.\n", __FILE__, __LINE__);
-#endif
-        return (EXIT_SUCCESS);
-}
+dxf_comment_write (FILE *fp, char *comment_string);
+
+
+#endif /* _COMMENT_H */
+
 
 /* EOF */
