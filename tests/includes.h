030f654a2e60f1b29aeaf809ee09900670ee0411
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-07-13 23:14:48 +0200

    TESTS: added an header file to include all src header files.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ tests/includes.h
@@ -0,0 +1,89 @@
+/*!
+ * \file includes.h
+ *
+ * \author Copyright (C) 2012 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Include all header files.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+
+#include "../src/entity.h"
+#include "../src/global.h"
+#include "../src/3dface.h"
+#include "../src/3dsolid.h"
+#include "../src/appid.h"
+#include "../src/arc.h"
+#include "../src/attdef.h"
+#include "../src/attrib.h"
+#include "../src/block.h"
+#include "../src/circle.h"
+#include "../src/class.h"
+#include "../src/color.h"
+#include "../src/comment.h"
+#include "../src/dim.h"
+#include "../src/donut.h"
+#include "../src/ellipse.h"
+#include "../src/entities.h"
+#include "../src/entity.h"
+#include "../src/file.h"
+#include "../src/global.h"
+#include "../src/hatch.h"
+#include "../src/header.h"
+#include "../src/insert.h"
+#include "../src/layer.h"
+#include "../src/line.h"
+#include "../src/ltype.h"
+#include "../src/lwpolyline.h"
+#include "../src/object.h"
+#include "../src/param.h"
+#include "../src/point.h"
+#include "../src/polyline.h"
+#include "../src/section.h"
+#include "../src/seqend.h"
+#include "../src/shape.h"
+#include "../src/solid.h"
+#include "../src/style.h"
+#include "../src/table.h"
+#include "../src/text.h"
+#include "../src/thumbnail.h"
+#include "../src/trace.h"
+#include "../src/ucs.h"
+#include "../src/util.h"
+#include "../src/vertex.h"
+#include "../src/view.h"
+#include "../src/viewport.h"
+#include "../src/vport.h"
+
+
+#endif /* _INCLUDES_H */
+
+
