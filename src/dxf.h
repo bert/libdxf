5b82a20800e1ad13a9f1ad2420fd6eaa197c167a
Bert Timmerman <bert.timmerman@xs4all.nl>
2012-12-22 14:48:07 +0100

    List of all dxf header files for convenient inclusion.
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ src/dxf.h
@@ -0,0 +1,90 @@
+/*!
+ * \file dxf.h
+ *
+ * \author Copyright (C) 2012 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief List of all dxf header files for convenient inclusion.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef DXF_H
+#define DXF_H
+
+
+#include "3dface.h"
+#include "3dsolid.h"
+#include "appid.h"
+#include "arc.h"
+#include "attdef.h"
+#include "attrib.h"
+#include "block.h"
+#include "circle.h"
+#include "class.h"
+#include "color.h"
+#include "comment.h"
+#include "dim.h"
+#include "dimstyle.h"
+#include "donut.h"
+#include "ellipse.h"
+#include "entities.h"
+#include "entity.h"
+#include "file.h"
+#include "global.h"
+#include "hatch.h"
+#include "header.h"
+#include "insert.h"
+#include "layer.h"
+#include "line.h"
+#include "ltype.h"
+#include "lwpolyline.h"
+#include "mtext.h"
+#include "object.h"
+#include "param.h"
+#include "point.h"
+#include "polyline.h"
+#include "section.h"
+#include "seqend.h"
+#include "shape.h"
+#include "solid.h"
+#include "style.h"
+#include "table.h"
+#include "text.h"
+#include "thumbnail.h"
+#include "trace.h"
+#include "ucs.h"
+#include "util.h"
+#include "vertex.h"
+#include "view.h"
+#include "viewport.h"
+#include "vport.h"
+
+
+#endif /* DXF_H */
+
+
+/* EOF */
