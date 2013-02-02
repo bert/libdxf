b19e013b39ad78458f366f1a38310c9baf21b1c2
Bert Timmerman <bert.timmerman@xs4all.nl>
2013-02-02 19:45:54 +0100

    HELIX: added helix.[ch].
    Signed-off-by: Bert Timmerman <bert.timmerman@xs4all.nl>

---
+++ src/helix.h
@@ -0,0 +1,144 @@
+/*!
+ * \file helix.h
+ *
+ * \author Copyright (C) 2013 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF helix entity (\c HELIX).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef _HELIX_H
+#define _HELIX_H
+
+
+#include "global.h"
+#include "entity.h"
+
+
+/*!
+ * \brief Definition of an AutoCAD helix entity (\c HELIX).
+ */
+typedef struct
+dxf_helix
+{
+        /* Members common for all DXF drawable entities. */
+        int id_code;
+                /*!< group code = 5\n
+                 * Identification number for the entity.\n
+                 * This is to be an unique (sequential) number in the DXF
+                 * file. */
+        char *linetype;
+                /*!< group code = 6\n
+                 * The linetype of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file. */
+        char *layer;
+                /*!< group code = 8\n
+                 * Layer on which the entity is drawn.\n
+                 * Defaults to layer "0" if no valid layername is given. */
+        double thickness;
+                /*!< group code = 39\n
+                 * Thickness of the arc in the local Z-direction.\n
+                 * Defaults to 0.0 if ommitted in the DXF file. */
+        int color;
+                /*!< group code = 62\n
+                 * Color of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Note that entities encapsulated in a block with the
+                 * color \c BYBLOCK are represented in the "native" color of
+                 * the \c BLOCK entity. */
+        int paperspace;
+                /*!< group code = 67\n
+                 * Entities are to be drawn on either \c PAPERSPACE or
+                 * \c MODELSPACE.\n
+                 * Optional, defaults to \c DXF_MODELSPACE (0). */
+        /* Specific members for a DXF helix. */
+        double x0;
+                /*!< group code = 10\n
+                 * X-value of the axis base point coordinate. */
+        double y0;
+                /*!< group code = 20\n
+                 * Y-value of the axis base point coordinate. */
+        double z0;
+                /*!< group code = 30\n
+                 * Z-value of the axis base point coordinate. */
+        double x1;
+                /*!< group code = 11\n
+                 * X-value of the start point coordinate. */
+        double y1;
+                /*!< group code = 21\n
+                 * Y-value of the start point coordinate. */
+        double z1;
+                /*!< group code = 31\n
+                 * Z-value of the start point coordinate. */
+        double x2;
+                /*!< group code = 12\n
+                 * X-value of the axis vector point coordinate. */
+        double y2;
+                /*!< group code = 22\n
+                 * Y-value of the axis vector point coordinate. */
+        double z2;
+                /*!< group code = 32\n
+                 * Z-value of the axis vector point coordinate. */
+        double radius;
+                /*!< group code = 40\n
+                 * Radius of the arc. */
+        double number_of_turns;
+                /*!< group code = 41\n
+                 * Number of turns. */
+        double turn_height;
+                /*!< group code = 42\n
+                 * Turn height. */
+        long major_release_number;
+                /*!< group code = 90\n
+                 * Major release number. */
+        long maintainance_release_number;
+                /*!< group code = 91\n
+                 * Maintainance release number. */
+        int constraint_type;
+                /*!< group code = 280\n
+                 * Constrain type\n
+                 * 0 = Constrain turn height.\n
+                 * 1 = Constrain turns.\n
+                 * 2 = Constrain height. */
+        int handedness;
+                /*!< group code = 290\n
+                 * Handedness; 0 = left, 1 = right. */
+        struct DxfHelix *next;
+                /*!< pointer to the next DxfHelix.\n
+                 * \c NULL in the last DxfHelix. */
+} DxfHelix, * DxfHelixPtr;
+
+
+DxfHelix *
+dxf_helix_new ();
+
+
+#endif /* _HELIX_H */
+
+
+/* EOF */
