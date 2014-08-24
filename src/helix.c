d818f942d78666787202b04d02fb5ded34d73525
bert <bert.timmerman@xs4all.nl>
2014-08-24 22:58:55 +0200

    HELIX: make strings translatable.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -1,7 +1,7 @@
 /*!
  * \file helix.c
  *
- * \author Copyright (C) 2013 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2013 ... 2014 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF helix entity (\c HELIX).
  *
@@ -47,8 +47,9 @@ DxfHelix *
 dxf_helix_new ()
 {
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_helix_new () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Entering %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         DxfHelix *dxf_helix = NULL;
         size_t size;
@@ -58,7 +59,9 @@ dxf_helix_new ()
         if (size == 0) size = 1;
         if ((dxf_helix = malloc (size)) == NULL)
         {
-                fprintf (stderr, "ERROR in dxf_helix_new () could not allocate memory for a DxfHelix struct.\n");
+                fprintf (stderr,
+                  (_("ERROR in %s () could not allocate memory for a DxfHelix struct.\n")),
+                  __FUNCTION__);
                 dxf_helix = NULL;
         }
         else
@@ -66,8 +69,9 @@ dxf_helix_new ()
                 memset (dxf_helix, 0, size);
         }
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_helix_new () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Leaving %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         return (dxf_helix);
 }
@@ -88,15 +92,18 @@ dxf_helix_init
 )
 {
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_helix_init () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Entering %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         int i;
 
         dxf_helix = dxf_helix_new ();
         if (dxf_helix == NULL)
         {
-              fprintf (stderr, "ERROR in dxf_helix_init () could not allocate memory for a DxfHelix struct.\n");
+              fprintf (stderr,
+                (_("ERROR in %s () could not allocate memory for a DxfHelix struct.\n")),
+                __FUNCTION__);
               return (NULL);
         }
         dxf_helix->id_code = 0;
@@ -140,8 +147,9 @@ dxf_helix_init
         dxf_spline_init (&dxf_helix->spline);
         dxf_helix->next = NULL;
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_helix_init () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Leaving %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         return (dxf_helix);
 }
@@ -169,8 +177,9 @@ dxf_helix_read
 )
 {
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_helix_read () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Entering %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         char *temp_string = NULL;
         int i;
@@ -194,8 +203,9 @@ dxf_helix_read
         {
                 if (ferror (fp->fp))
                 {
-                        fprintf (stderr, "Error in dxf_helix_read () while reading from: %s in line: %d.\n",
-                                fp->filename, fp->line_number);
+                        fprintf (stderr,
+                          (_("Error in %s () while reading from: %s in line: %d.\n")),
+                          __FUNCTION__, fp->filename, fp->line_number);
                         fclose (fp->fp);
                         return (EXIT_FAILURE);
                 }
@@ -470,8 +480,9 @@ dxf_helix_read
                         else if ((strcmp (temp_string, "AcDbEntity") != 0)
                                 && ((strcmp (temp_string, "AcDbHelix") != 0)))
                         {
-                                fprintf (stderr, "Error in dxf_helix_read () found a bad subclass marker in: %s in line: %d.\n",
-                                        fp->filename, fp->line_number);
+                                fprintf (stderr,
+                                  (_("Error in %s () found a bad subclass marker in: %s in line: %d.\n")),
+                                  __FUNCTION__, fp->filename, fp->line_number);
                         }
                 }
                 else if (strcmp (temp_string, "160") == 0)
@@ -574,13 +585,15 @@ dxf_helix_read
                 }
                 else
                 {
-                        fprintf (stderr, "Warning: in dxf_helix_read () unknown string tag found while reading from: %s in line: %d.\n",
-                                fp->filename, fp->line_number);
+                        fprintf (stderr,
+                          (_("Warning: in %s () unknown string tag found while reading from: %s in line: %d.\n")),
+                          __FUNCTION__, fp->filename, fp->line_number);
                 }
         }
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_helix_read () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Leaving %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         return (EXIT_SUCCESS);
 }
@@ -602,8 +615,9 @@ dxf_helix_write
 )
 {
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_helix_write () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Entering %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         char *dxf_entity_name = strdup ("HELIX");
         int i;
@@ -611,23 +625,28 @@ dxf_helix_write
         /* Do some basic checks. */
         if (dxf_helix == NULL)
         {
-                fprintf (stderr, "Error in dxf_helix_write () a NULL pointer was passed.\n");
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
                 return (EXIT_FAILURE);
         }
         if (dxf_helix->radius == 0.0)
         {
-                fprintf (stderr, "Error in dxf_helix_write () radius value equals 0.0 for the %s entity with id-code: %x.\n",
-                        dxf_entity_name, dxf_helix->id_code);
+                fprintf (stderr,
+                  (_("Error in %s () radius value equals 0.0 for the %s entity with id-code: %x.\n")),
+                  __FUNCTION__, dxf_entity_name, dxf_helix->id_code);
                 fprintf (stderr, "    skipping %s entity.\n",
                         dxf_entity_name);
                 return (EXIT_FAILURE);
         }
         if (strcmp (dxf_helix->layer, "") == 0)
         {
-                fprintf (stderr, "Warning in dxf_helix_write () empty layer string for the %s entity with id-code: %x.\n",
-                        dxf_entity_name, dxf_helix->id_code);
-                fprintf (stderr, "\t%s entity is relocated to default layer.\n",
-                        dxf_entity_name);
+                fprintf (stderr,
+                  (_("Warning in %s () empty layer string for the %s entity with id-code: %x.\n")),
+                  __FUNCTION__, dxf_entity_name, dxf_helix->id_code);
+                fprintf (stderr,
+                  (_("\t%s entity is relocated to default layer.\n")),
+                  dxf_entity_name);
                 dxf_helix->layer = DXF_DEFAULT_LAYER;
         }
         /* Start writing output. */
@@ -739,8 +758,9 @@ dxf_helix_write
         fprintf (fp->fp, "290\n%d\n", dxf_helix->handedness);
         fprintf (fp->fp, "280\n%d\n", dxf_helix->constraint_type);
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_helix_write () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Leaving %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         return (EXIT_SUCCESS);
 }
@@ -762,14 +782,17 @@ dxf_helix_free
 )
 {
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Entering dxf_helix_free () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Entering %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         int i;
 
         if (dxf_helix->next != NULL)
         {
-              fprintf (stderr, "ERROR in dxf_helix_free () pointer to next DxfHelix was not NULL.\n");
+              fprintf (stderr,
+                (_("ERROR in %s () pointer to next DxfHelix was not NULL.\n")),
+                __FUNCTION__);
               return (EXIT_FAILURE);
         }
         free (dxf_helix->linetype);
@@ -786,8 +809,9 @@ dxf_helix_free
         free (dxf_helix);
         dxf_helix = NULL;
 #if DEBUG
-        fprintf (stderr, "[File: %s: line: %d] Leaving dxf_helix_free () function.\n",
-                __FILE__, __LINE__);
+        fprintf (stderr,
+          (_("[File: %s: line: %d] Leaving %s () function.\n")),
+          __FILE__, __LINE__, __FUNCTION__);
 #endif
         return (EXIT_SUCCESS);
 }
