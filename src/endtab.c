e0c1e2fed054fb34ea791c5e006fa7960bdc6b4f
bert <bert.timmerman@xs4all.nl>
2014-09-16 21:37:08 +0200

    ENDTAB: added files endtab.[ch].
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/endtab.c
@@ -0,0 +1,69 @@
+/*!
+ * \file endtab.c
+ *
+ * \author Copyright (C) 2014 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Functions for a DXF end of table marker (\c ENDTAB).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#include "endtab.h"
+
+
+/*!
+ * \brief Write DXF output to a file for an end of table marker
+ * (\c ENDTAB).
+ *
+ * \return \c EXIT_SUCCESS when done, or \c EXIT_FAILURE when an error
+ * occurred.
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+int
+dxf_endtab_write
+(
+        DxfFile *fp
+                /*!< DXF file pointer to an output file (or device). */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        fprintf (fp->fp, "  0\nENDTAB\n");
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (EXIT_SUCCESS);
+}
+
+
+/* EOF */
