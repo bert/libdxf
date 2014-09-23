3e2e223b19a4ea56a805d0b2e0a7ef3116388e5c
bert <bert.timmerman@xs4all.nl>
2014-09-23 12:35:56 +0200

    global.h: correction of a Doxygen comment.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/global.h
+++ src/global.h
@@ -106,7 +106,7 @@
 #endif
 
  /*!
- * Verbose debugging output to \sdterr on/off toggle.
+ * Verbose debugging output to \c stderr on/off toggle.
  *
  * If not already defined as a compiler directive the default value is
  * 0 (off).
