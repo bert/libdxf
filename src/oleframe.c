dab94ced50710a9251a8caa1a03a20816baafc17
bert <bert.timmerman@xs4all.nl>
2014-11-17 23:11:27 +0100

    OLEFRAME: test for minimum DXF version.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/oleframe.c
+++ src/oleframe.c
@@ -368,6 +368,13 @@ dxf_oleframe_write
         int i;
 
         /* Do some basic checks. */
+        if (fp->acad_version_number < AutoCAD_13)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () illegal DXF version for this entity.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dxf_oleframe == NULL)
         {
                 fprintf (stderr,
