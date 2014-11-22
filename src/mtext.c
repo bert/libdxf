07228b359f5c4b428f17f05ac3a27adc378604dd
bert <bert.timmerman@xs4all.nl>
2014-11-22 13:17:54 +0100

    MTEXT: corrections in messages.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mtext.c
+++ src/mtext.c
@@ -98,7 +98,7 @@ dxf_mtext_init
         if (dxf_mtext == NULL)
         {
                 fprintf (stderr,
-                  (_("WARNING in %s () a NULL pointer was passed.\n")),
+                  (_("Warning in %s () a NULL pointer was passed.\n")),
                   __FUNCTION__);
                 dxf_mtext = dxf_mtext_new ();
         }
@@ -517,7 +517,7 @@ dxf_mtext_write
                   (_("Warning in %s () empty linetype string for the %s entity with id-code: %x\n")),
                   __FUNCTION__, dxf_entity_name, dxf_mtext->id_code);
                 fprintf (stderr,
-                  (_("    %s entity is reset to default linetype")),
+                  (_("\t%s entity is reset to default linetype")),
                   dxf_entity_name);
                 dxf_mtext->linetype = strdup (DXF_DEFAULT_LINETYPE);
         }
