6d1d7464c9fb6d61fe4d389ed2b3ecaa3531010a
bert <bert.timmerman@xs4all.nl>
2014-12-03 19:36:22 +0100

    SOLID: removed dxf_solid_write_lowlevel().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.h
+++ src/solid.h
@@ -159,29 +159,6 @@ dxf_solid_read
         DxfSolid *dxf_solid
 );
 int
-dxf_solid_write_lowlevel
-(
-        FILE *fp,
-        int id_code,
-        char *linetype,
-        char *layer,
-        double x0,
-        double y0,
-        double z0,
-        double x1,
-        double y1,
-        double z1,
-        double x2,
-        double y2,
-        double z2,
-        double x3,
-        double y3,
-        double z3,
-        double thickness,
-        int color,
-        int paperspace
-);
-int
 dxf_solid_write
 (
         FILE *fp,
