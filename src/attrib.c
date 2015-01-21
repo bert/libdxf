b628e81b313a5b2223a4bf60e8f4d9a3af35eed2
bert <bert.timmerman@xs4all.nl>
2015-01-21 22:24:27 +0100

    ATTRIB: check for NULL file pointers.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/attrib.c
+++ src/attrib.c
@@ -180,6 +180,13 @@ dxf_attrib_read
         char *temp_string = NULL;
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
         if (dxf_attrib == NULL)
         {
                 fprintf (stderr,
@@ -497,6 +504,13 @@ dxf_attrib_write
         char *dxf_entity_name = strdup ("ATTRIB");
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dxf_attrib == NULL)
         {
                 fprintf (stderr,
