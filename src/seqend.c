91232ddee644296f6bb1a0352d8d3290fc0a27aa
bert <bert.timmerman@xs4all.nl>
2015-01-22 23:00:34 +0100

    SEQEND: check for NULL file pointers.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/seqend.c
+++ src/seqend.c
@@ -1,7 +1,7 @@
 /*!
  * \file seqend.c
  *
- * \author Copyright (C) 2008 ... 2014 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF end of sequence marker (\c SEQEND).
  *
@@ -168,6 +168,13 @@ dxf_seqend_read
         char *temp_string = NULL;
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
         if (dxf_seqend == NULL)
         {
                 fprintf (stderr,
