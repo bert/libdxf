54e7709e384732e82aeed1cd8f5009626cadfa3d
bert <bert.timmerman@xs4all.nl>
2015-01-22 21:44:22 +0100

    LAYER: check for NULL file pointers.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/layer.c
+++ src/layer.c
@@ -154,6 +154,13 @@ dxf_layer_read
         char *temp_string = NULL;
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
         if (dxf_layer == NULL)
         {
                 fprintf (stderr,
@@ -314,6 +321,13 @@ dxf_layer_write
         char *dxf_entity_name = strdup ("LAYER");
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dxf_layer == NULL)
         {
                 fprintf (stderr,
