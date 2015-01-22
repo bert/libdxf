61a0d6967e7e34ba046fac974385d52d235aa052
bert <bert.timmerman@xs4all.nl>
2015-01-22 23:19:01 +0100

    VIEWPORT: check for NULL file pointers.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.c
+++ src/viewport.c
@@ -222,6 +222,13 @@ dxf_viewport_read
         char *temp_string = NULL;
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
         if (dxf_viewport == NULL)
         {
                 fprintf (stderr,
@@ -1030,6 +1037,13 @@ dxf_viewport_write
         char *dxf_entity_name = strdup ("VIEWPORT");
 
         /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL file pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dxf_viewport == NULL)
         {
                 fprintf (stderr,
