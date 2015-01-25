039b4da4f3f0ac960188f64416beb38dcbe1fb38
bert <bert.timmerman@xs4all.nl>
2015-01-25 12:17:29 +0100

    DONUT: converted dxf_donut_write_lowlevel() into dxf_donut_write().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -3,7 +3,9 @@
  * 
  * \author Copyright (C) 2008 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  * 
- * \brief Functions for a DXF donut entity (\c DONUT).
+ * \brief Functions for a libDXF donut entity.
+ *
+ * \warning This is not an official AutoCAD entity.
  *
  * <hr>
  * <h1><b>Copyright Notices.</b></h1>\n
@@ -36,7 +38,7 @@
 
 
 /*!
- * \brief Draw a donut.
+ * \brief Write DXF output to a file for a donut.
  *
  * A typical convenience function.\n
  * Draw a donut by means of two bulged vertices into a single polyline.
@@ -45,49 +47,25 @@
  * occurred.
  */
 int
-dxf_donut_write_lowlevel
+dxf_donut_write
 (
         DxfFile *fp,
                 /*!< DXF file pointer to an output file (or device). */
-        int id_code,
-                /*!< group code = 5. */
-        char *linetype,
-                /*!< group code = 6\n
-                 * optional, if omitted defaults to \c BYLAYER. */
-        char *layer,
-                /*!< group code = 8. */
-        double x0,
-                /*!< group code = 10\n
-                 * if omitted defaults to 0.0. */
-        double y0,
-                /*!< group code = 20\n
-                 * if omitted defaults to 0.0. */
-        double z0,
-                /*!< group code = 30\n
-                 * default elevation for vertices. */
-        double thickness,
-                /*!< group code = 39\n
-                 * optional, if omitted defaults to 0.0. */
-        double outside_diameter,
-                /*!< outside diameter. */
-        double inside_diameter,
-                /*!< inside diameter. */
-        int color,
-                /*!< group code = 62\n
-                 * optional, if omitted defaults to \c BYLAYER. */
-        int paperspace
-                /*!< group code = 67\n
-                 * optional, if omitted defaults to 0 (modelspace). */
+        DxfDonut *donut
+                /*!< Pointer to a donut struct. */
 )
 {
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
-        DxfPolyline *dxf_polyline = NULL;
-        DxfVertex *dxf_vertex = NULL;
+        DxfPolyline *polyline = NULL;
+        DxfVertex *vertex_1 = NULL;
+        DxfVertex *vertex_2 = NULL;
+        DxfSeqend *seqend = NULL;
         char *dxf_entity_name = strdup ("POLYLINE");
         double start_width;
         double end_width;
+        int id_code = -1;
 
         /* Do some basic checks. */
         if (fp == NULL)
@@ -97,16 +75,16 @@ dxf_donut_write_lowlevel
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
-        if (outside_diameter > inside_diameter)
+        if (donut->outside_diameter > donut->inside_diameter)
         {
                 fprintf (stderr,
                   (_("Error in %s () outside diameter is smaller than the inside diameter for the %s entity with id-code: %x\n")),
                   __FUNCTION__, dxf_entity_name, id_code);
                 return (EXIT_FAILURE);
         }
-        start_width = 0.5 * (outside_diameter - inside_diameter);
-        end_width = 0.5 * (outside_diameter - inside_diameter);
-        if (strcmp (layer, "") == 0)
+        start_width = 0.5 * (donut->outside_diameter - donut->inside_diameter);
+        end_width = 0.5 * (donut->outside_diameter - donut->inside_diameter);
+        if (strcmp (donut->layer, "") == 0)
         {
                 fprintf (stderr,
                   (_("Warning in %s () empty layer string for the %s entity with id-code: %x\n")),
@@ -114,61 +92,84 @@ dxf_donut_write_lowlevel
                 fprintf (stderr,
                   (_("\t%s entity is relocated to layer 0\n")),
                   dxf_entity_name);
-                layer = strdup (DXF_DEFAULT_LAYER);
+                donut->layer = strdup (DXF_DEFAULT_LAYER);
         }
         /* Draw a polyline primitive. */
-        dxf_polyline_new (dxf_polyline);
-        dxf_polyline = dxf_polyline_init (dxf_polyline);
-        dxf_polyline->id_code = id_code;
-        dxf_polyline->linetype = linetype;
-        dxf_polyline->layer = layer;
-        dxf_polyline->x0 = x0;
-        dxf_polyline->y0 = y0;
-        dxf_polyline->z0 = z0;
-        dxf_polyline->thickness = thickness;
-        dxf_polyline->start_width = 0.5 * start_width;
-        dxf_polyline->end_width = 0.5 * end_width;
-        dxf_polyline->color = color;
-        dxf_polyline->vertices_follow = 1;
-        dxf_polyline->paperspace = paperspace;
-        dxf_polyline->flag = 1;
-        dxf_polyline->polygon_mesh_M_vertex_count = 0;
-        dxf_polyline->polygon_mesh_N_vertex_count = 0;
-        dxf_polyline->smooth_M_surface_density = 0;
-        dxf_polyline->smooth_N_surface_density = 0;
-        dxf_polyline->surface_type = 0;
-        dxf_polyline_write (fp, dxf_polyline);
+        dxf_polyline_new (polyline);
+        polyline = dxf_polyline_init (polyline);
+        id_code = fp->last_id_code;
+        id_code++;
+        polyline->id_code = id_code;
+        polyline->linetype = donut->linetype;
+        polyline->layer = donut->layer;
+        polyline->x0 = donut->x0;
+        polyline->y0 = donut->y0;
+        polyline->z0 = donut->z0;
+        polyline->thickness = donut->thickness;
+        polyline->start_width = 0.5 * start_width;
+        polyline->end_width = 0.5 * end_width;
+        polyline->color = donut->color;
+        polyline->vertices_follow = 1;
+        polyline->paperspace = donut->paperspace;
+        polyline->flag = 1;
+        polyline->polygon_mesh_M_vertex_count = 0;
+        polyline->polygon_mesh_N_vertex_count = 0;
+        polyline->smooth_M_surface_density = 0;
+        polyline->smooth_N_surface_density = 0;
+        polyline->surface_type = 0;
+        dxf_polyline_write (fp, polyline);
         /*! \todo Hook up this polyline to the list of polylines. */
+        /* Write the first vertex. */
+        vertex_1 = dxf_vertex_new ();
+        vertex_1 = dxf_vertex_init (vertex_1);
+        id_code = fp->last_id_code;
         id_code++;
-        /* Write first XY-coordinate. */
-        dxf_vertex = dxf_vertex_new ();
-        dxf_vertex = dxf_vertex_init (dxf_vertex);
-        dxf_vertex->id_code = id_code;
-        dxf_vertex->layer = layer;
-        dxf_vertex->linetype = linetype;
-        dxf_vertex->x0 = x0 - (0.25 * (outside_diameter + inside_diameter));
-        dxf_vertex->y0 = y0;
-        dxf_vertex->z0 = z0;
-        dxf_vertex->thickness = thickness;
-        dxf_vertex->start_width = 0.5 * start_width;
-        dxf_vertex->end_width = 0.5 * end_width;
-        dxf_vertex->bulge = 1;
-        dxf_vertex->curve_fit_tangent_direction = 0.0;
-        dxf_vertex->color = color;
-        dxf_vertex->paperspace = paperspace;
-        dxf_vertex->flag = 0;
-        dxf_vertex_write (fp, dxf_vertex);
+        vertex_1->id_code = id_code;
+        vertex_1->layer = donut->layer;
+        vertex_1->linetype = donut->linetype;
+        vertex_1->x0 = donut->x0 - (0.25 * (donut->outside_diameter + donut->inside_diameter));
+        vertex_1->y0 = donut->y0;
+        vertex_1->z0 = donut->z0;
+        vertex_1->thickness = donut->thickness;
+        vertex_1->start_width = 0.5 * start_width;
+        vertex_1->end_width = 0.5 * end_width;
+        vertex_1->bulge = 1;
+        vertex_1->curve_fit_tangent_direction = 0.0;
+        vertex_1->color = donut->color;
+        vertex_1->paperspace = donut->paperspace;
+        vertex_1->flag = 0;
+        dxf_vertex_write (fp, vertex_1);
         /*! \todo Hook up this vertex to the list of vertices. */
+        /* Write the second vertex. */
+        vertex_2 = dxf_vertex_new ();
+        vertex_2 = dxf_vertex_init (vertex_2);
+        id_code = fp->last_id_code;
         id_code++;
-        /* Write second XY-coordinate. */
-        dxf_vertex->x0 = x0 + (0.25 * (outside_diameter + inside_diameter));
-        dxf_vertex_write (fp, dxf_vertex);
+        vertex_2->id_code = id_code;
+        vertex_2->layer = donut->layer;
+        vertex_2->linetype = donut->linetype;
+        vertex_2->x0 = donut->x0 + (0.25 * (donut->outside_diameter + donut->inside_diameter));
+        vertex_2->y0 = donut->y0;
+        vertex_2->z0 = donut->z0;
+        vertex_2->thickness = donut->thickness;
+        vertex_2->start_width = 0.5 * start_width;
+        vertex_2->end_width = 0.5 * end_width;
+        vertex_2->bulge = 1;
+        vertex_2->curve_fit_tangent_direction = 0.0;
+        vertex_2->color = donut->color;
+        vertex_2->paperspace = donut->paperspace;
+        vertex_2->flag = 0;
+        dxf_vertex_write (fp, vertex_2);
         /*! \todo Hook up this vertex to the list of vertices. */
-        id_code++;
         /* Write the end of polyline sequence marker. */
-        /*! \todo FIX ME:
-        dxf_seqend_write (fp, dxf_seqend);
-        */
+        dxf_seqend_new (seqend);
+        seqend = dxf_seqend_init (seqend);
+        id_code = fp->last_id_code;
+        id_code++;
+        seqend->id_code = id_code;
+        seqend->layer = donut->layer;
+        seqend->linetype = donut->linetype;
+        dxf_seqend_write (fp, seqend);
 #if DEBUG
         DXF_DEBUG_END
 #endif
--- src/donut.h
+++ src/donut.h
@@ -1,9 +1,11 @@
 /*!
  * \file donut.h
  *
- * \author Copyright (C) 2008 ... 2014 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
- * \brief Header file for a DXF donut entity (\c DONUT).
+ * \brief Header file for a libDXF donut entity.
+ *
+ * \warning This is not an official AutoCAD entity.
  *
  * <hr>
  * <h1><b>Copyright Notices.</b></h1>\n
@@ -42,21 +44,98 @@
 #include "seqend.h"
 
 
+/*!
+ * \brief Definition of a libDXF donut entity.
+ *
+ * A libDXF donut is a typical convenience struct.
+ *
+ * \warning This is not an official AutoCAD entity.
+ */
+typedef struct
+dxf_donut
+{
+        /* Members common for all DXF drawable entities. */
+        int id_code;
+                /*!< Identification number for the entity.\n
+                 * This is to be an unique (sequential) number in the DXF
+                 * file.\n
+                 * Group code = 5. */
+        char *linetype;
+                /*!< The linetype of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Group code = 6. */
+        char *layer;
+                /*!< Layer on which the entity is drawn.\n
+                 * Defaults to layer "0" if no valid layername is given.\n
+                 * Group code = 8. */
+        double elevation;
+                /*!< Elevation of the arc in the local Z-direction.\n
+                 * Defaults to 0.0 if omitted in the DXF file, or prior
+                 * to DXF version R12, or DXF_FLATLAND equals 0 (default).\n
+                 * Group code = 38. */
+        double thickness;
+                /*!< Thickness of the arc in the local Z-direction.\n
+                 * Defaults to 0.0 if ommitted in the DXF file.\n
+                 * Group code = 39. */
+        double linetype_scale;
+                /*!< Linetype scale (optional).\n
+                 * Group code = 48. */
+        int16_t visibility;
+                /*!< Object visibility (optional):\n
+                 * <ol>
+                 * <li value = "0"> Visible</li>
+                 * <li value = "1"> Invisible</li>
+                 * </ol>
+                 * Group code = 60. */
+        int color;
+                /*!< Color of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Note that entities encapsulated in a block with the
+                 * color \c BYBLOCK are represented in the "native" color of
+                 * the \c BLOCK entity.\n
+                 * Group code = 62. */
+        int paperspace;
+                /*!< Entities are to be drawn on either \c PAPERSPACE or
+                 * \c MODELSPACE.\n
+                 * Optional, defaults to \c DXF_MODELSPACE (0).\n
+                 * Group code = 67. */
+        char *dictionary_owner_soft;
+                /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
+                 * Group code = 330. */
+        char *dictionary_owner_hard;
+                /*!< Hard owner ID/handle to owner dictionary (optional).\n
+                 * Group code = 360. */
+        /* Specific members for a libDXF donut. */
+        double x0;
+                /*!< X value of the insertion point for the center of
+                 * the donut.\n
+                 * If omitted defaults to 0.0.\n
+                 * Group code = 10. */
+        double y0;
+                /*!< Y value of the insertion point for the center of
+                 * the donut.\n
+                 * If omitted defaults to 0.0.\n
+                 * Group code = 20. */
+        double z0;
+                /*!< Z value of the insertion point for the center of
+                 * the donut.\n
+                 * If omitted defaults to 0.0.\n
+                 * Group code = 30. */
+        double outside_diameter;
+                /*!< Outside diameter. */
+        double inside_diameter;
+                /*!< Inside diameter. */
+        struct DxfDonut *next;
+                /*!< Pointer to the next DxfDonut.\n
+                 * \c NULL in the last DxfDonut. */
+} DxfDonut;
+
+
 int
-dxf_donut_write_lowlevel
+dxf_donut_write
 (
         DxfFile *fp,
-        int id_code,
-        char *linetype,
-        char *layer,
-        double x0,
-        double y0,
-        double z0,
-        double thickness,
-        double outside_diameter,
-        double inside_diameter,
-        int color,
-        int paperspace
+        DxfDonut *donut
 );
 
 
