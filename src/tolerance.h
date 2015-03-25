4b4684e2862f01dc077c46ab46fc065146e8f773
bert <bert.timmerman@xs4all.nl>
2015-03-25 21:24:01 +0100

    TOLERANCE: added tolerance.h.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/tolerance.h
@@ -0,0 +1,148 @@
+/*!
+ * \file tolerance.h
+ *
+ * \author Copyright (C) 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF tolerance entity (\c TOLERANCE).
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef LIBDXF_SRC_TOLERANCE_H
+#define LIBDXF_SRC_TOLERANCE_H
+
+
+#include "global.h"
+
+
+/*!
+ * \brief DXF definition of an AutoCAD tolerance entity (\c TOLERANCE).
+ *
+ * \version According to DXF R10 (backward compatibility).
+ * \version According to DXF R11 (backward compatibility).
+ * \version According to DXF R12 (backward compatibility).
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+typedef struct
+dxf_tolerance
+{
+        /* Members common for all DXF drawable entities. */
+        int id_code;
+                /*!< Identification number for the entity.\n
+                 * This is to be an unique (sequential) number in the DXF
+                 * file.\n
+                 * Group code = 5. */
+        char *linetype;
+                /*!< The linetype of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Group code = 6. */
+        char *layer;
+                /*!< Layer on which the entity is drawn.\n
+                 * Defaults to layer "0" if no valid layername is given.\n
+                 * Group code = 8. */
+        double elevation;
+                /*!< Elevation of the arc in the local Z-direction.\n
+                 * Defaults to 0.0 if omitted in the DXF file, or prior
+                 * to DXF version R12, or DXF_FLATLAND equals 0 (default).\n
+                 * Group code = 38. */
+        double thickness;
+                /*!< Thickness of the arc in the local Z-direction.\n
+                 * Defaults to 0.0 if ommitted in the DXF file.\n
+                 * Group code = 39. */
+        double linetype_scale;
+                /*!< Linetype scale (optional).\n
+                 * Group code = 48. */
+        int16_t visibility;
+                /*!< Object visibility (optional):\n
+                 * <ol>
+                 * <li value = "0"> Visible</li>
+                 * <li value = "1"> Invisible</li>
+                 * </ol>
+                 * Group code = 60. */
+        int color;
+                /*!< Color of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Note that entities encapsulated in a block with the
+                 * color \c BYBLOCK are represented in the "native" color of
+                 * the \c BLOCK entity.\n
+                 * Group code = 62. */
+        int paperspace;
+                /*!< Entities are to be drawn on either \c PAPERSPACE or
+                 * \c MODELSPACE.\n
+                 * Optional, defaults to \c DXF_MODELSPACE (0).\n
+                 * Group code = 67. */
+        char *dictionary_owner_soft;
+                /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
+                 * Group code = 330. */
+        char *dictionary_owner_hard;
+                /*!< Hard owner ID/handle to owner dictionary (optional).\n
+                 * Group code = 360. */
+        /* Specific members for a DXF arc. */
+        char *dimstyle_name;
+                /*!< Dimension style name.\n
+                 * Group code = 3.  */
+        double x0;
+                /*!< X-value of the insertion point coordinate.\n
+                 * Group code = 10. */
+        double y0;
+                /*!< Y-value of the insertion point coordinate.\n
+                 * Group code = 20. */
+        double z0;
+                /*!< Z-value of the insertion point coordinate.\n
+                 * Group code = 30. */
+        double x1;
+                /*!< X-value of the direction vector.\n
+                 * Group code = 11. */
+        double y1;
+                /*!< Y-value of the direction vector.\n
+                 * Group code = 21. */
+        double z1;
+                /*!< Z-value of the direction vector.\n
+                 * Group code = 31. */
+        double extr_x0;
+                /*!< X-value of the extrusion vector.\n
+                 * Defaults to 0.0 if omitted in the DXF file.\n
+                 * Group code = 210. */
+        double extr_y0;
+                /*!< Y-value of the extrusion vector.\n
+                 * Defaults to 0.0 if omitted in the DXF file.\n
+                 * Group code = 220. */
+        double extr_z0;
+                /*!< Z-value of the extrusion vector.\n
+                 * Defaults to 1.0 if omitted in the DXF file.\n
+                 * Group code = 230. */
+        struct DxfTolerance *next;
+                /*!< Pointer to the next DxfTolerance.\n
+                 * \c NULL in the last DxfTolerance. */
+} DxfTolerance;
+
+
+#endif /* LIBDXF_SRC_TOLERANCE_H */
+
+
+/* EOF */
