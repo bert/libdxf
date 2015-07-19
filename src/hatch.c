56595865f8b17b45cd9dfb64455a8983cbe88d44
bert <bert.timmerman@xs4all.nl>
2015-07-19 19:29:07 +0200

    HATCH: do some basic checks in dxf_hatch_boundary_path_arc_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -3608,6 +3608,14 @@ dxf_hatch_boundary_path_edge_arc_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (arc == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (arc->next != NULL)
         {
                 fprintf (stderr,
