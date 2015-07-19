ea0513e8d1f7b7f4ef6f14a83e6704c185eefd1a
bert <bert.timmerman@xs4all.nl>
2015-07-19 16:25:27 +0200

    HATCH: do some basic checks in dxf_hatch_pattern_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -3305,6 +3305,14 @@ dxf_hatch_pattern_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (pattern == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (pattern->next != NULL)
         {
                 fprintf (stderr,
