6f1469b51bc58cb09e70021b1a9663241bc42a36
bert <bert.timmerman@xs4all.nl>
2015-07-19 19:27:46 +0200

    HATCH: do some basic checks in dxf_hatch_boundary_path_edge_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -3562,6 +3562,14 @@ dxf_hatch_boundary_path_edge_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (edge == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (edge->next != NULL)
         {
                 fprintf (stderr,
