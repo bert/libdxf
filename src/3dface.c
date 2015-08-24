5d72b601c015ec217104e81c5dbf36ab089ebe32
bert <bert.timmerman@xs4all.nl>
2015-08-24 23:58:21 +0200

    3DFACE: added dxf_3dface_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -1246,6 +1246,57 @@ dxf_3dface_get_visibility
 
 
 /*!
+ * \brief Set the visibility for this DXF \c 3DFACE entity.
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+Dxf3dface *
+dxf_3dface_set_visibility
+(
+        Dxf3dface *face,
+                /*!< a pointer to a DXF \c 3DFACE entity. */
+        int16_t visibility
+                /*!< the visibility to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (face == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (visibility < 0)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a negative visibility value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (visibility > 1)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () an out of range visibility value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        face->visibility = visibility;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (face);
+}
+
+
+/*!
  * \brief Test if the first edge of the DXF \c 3DFACE is invisible.
  *
  * \return \c TRUE when the edge is invisible, or \c FALSE when the edge is
--- src/3dface.h
+++ src/3dface.h
@@ -255,6 +255,12 @@ dxf_3dface_get_visibility
 (
         Dxf3dface *face
 );
+Dxf3dface *
+dxf_3dface_set_visibility
+(
+        Dxf3dface *face,
+        int16_t visibility
+);
 int
 dxf_3dface_is_first_edge_invisible
 (
