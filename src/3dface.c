e2b12dc5362d42971fb132b2df465d11dfc7805a
bert <bert.timmerman@xs4all.nl>
2015-08-25 23:58:12 +0200

    3DFACE: added dxf_3dface_get_dictionary_owner_hard().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -1538,6 +1538,55 @@ dxf_3dface_get_dictionary_owner_soft
 
 
 /*!
+ * \brief Get the hard pointer to the dictionary owner from this DXF 
+ * \c 3DFACE entity.
+ *
+ * \return hard pointer to the dictionary owner.
+ *
+ * \warning No checks are performed on the returned pointer (string).
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+char *
+dxf_3dface_get_dictionary_owner_hard
+(
+        Dxf3dface *face
+                /*!< a pointer to a DXF \c 3DFACE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        char *result;
+
+        /* Do some basic checks. */
+        if (face == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (face->dictionary_owner_hard ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_hard member.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        result = strdup (face->dictionary_owner_hard);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (result);
+}
+
+
+/*!
  * \brief Test if the first edge of the DXF \c 3DFACE is invisible.
  *
  * \return \c TRUE when the edge is invisible, or \c FALSE when the edge is
--- src/3dface.h
+++ src/3dface.h
@@ -288,6 +288,11 @@ dxf_3dface_get_dictionary_owner_soft
 (
         Dxf3dface *face
 );
+char *
+dxf_3dface_get_dictionary_owner_hard
+(
+        Dxf3dface *face
+);
 int
 dxf_3dface_is_first_edge_invisible
 (
