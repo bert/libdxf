e8d06e9dea0d2e85b57862268cbb695a63993974
bert <bert.timmerman@xs4all.nl>
2015-08-31 23:58:05 +0200

    3DSOLID: added dxf_3dsolid_set_paperspace().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dsolid.c
+++ src/3dsolid.c
@@ -1432,4 +1432,55 @@ dxf_3dsolid_get_paperspace
 }
 
 
+/*!
+ * \brief Set the paperspace flag for this DXF \c 3DSOLID entity.
+ *
+ * \version According to DXF R10 (backward compatibility).
+ * \version According to DXF R11 (backward compatibility).
+ * \version According to DXF R12 (backward compatibility).
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+Dxf3dsolid *
+dxf_3dsolid_set_paperspace
+(
+        Dxf3dsolid *solid,
+                /*!< a pointer to a DXF \c 3DSOLID entity. */
+        int paperspace
+                /*!< the paperspace flag value to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (solid == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (paperspace < 0)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a negative paperspace value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (paperspace > 1)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () an out of range paperspace value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        solid->paperspace = paperspace;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (solid);
+}
+
+
 /* EOF */
--- src/3dsolid.h
+++ src/3dsolid.h
@@ -248,6 +248,12 @@ dxf_3dsolid_get_paperspace
 (
         Dxf3dsolid *solid
 );
+Dxf3dsolid *
+dxf_3dsolid_set_paperspace
+(
+        Dxf3dsolid *solid,
+        int paperspace
+);
 
 
 #endif /* LIBDXF_SRC_3DSOLID_H */
