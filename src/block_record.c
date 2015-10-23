b3c53e2afda0fde25577d8c62ad12a1b1613f7e4
bert <bert.timmerman@xs4all.nl>
2015-10-23 21:28:25 +0200

    BLOCK_RECORD: added dxf_block_record_set_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.c
+++ src/block_record.c
@@ -905,4 +905,50 @@ dxf_block_record_get_dictionary_owner_soft
 }
 
 
+/*!
+ * \brief Set the pointer to the dictionary_owner_soft for a DXF
+ * \c BLOCK_RECORD entity.
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ */
+DxfBlockRecord *
+dxf_block_record_set_dictionary_owner_soft
+(
+        DxfBlockRecord *block_record,
+                /*!< a pointer to a DXF \c BLOCK_RECORD entity. */
+        char *dictionary_owner_soft
+                /*!< a string containing the pointer to the
+                 * dictionary_owner_soft for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (block_record == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (dictionary_owner_soft == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        block_record->dictionary_owner_soft = strdup (dictionary_owner_soft);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (block_record);
+}
+
+
 /* EOF*/
--- src/block_record.h
+++ src/block_record.h
@@ -178,6 +178,12 @@ dxf_block_record_get_dictionary_owner_soft
 (
         DxfBlockRecord *block_record
 );
+DxfBlockRecord *
+dxf_block_record_set_dictionary_owner_soft
+(
+        DxfBlockRecord *block_record,
+        char *dictionary_owner_soft
+);
 
 
 #endif /* LIBDXF_SRC_BLOCK_RECORD_H */
