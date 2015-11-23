b91f1f2a22037f439a9367fa6549b6a1d5462d5a
bert <bert.timmerman@xs4all.nl>
2015-11-23 22:08:21 +0100

    DIMSTYLE: added dxf_dimstyle_set_dimscale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimstyle.h
+++ src/dimstyle.h
@@ -411,6 +411,12 @@ dxf_dimstyle_get_dimscale
 (
         DxfDimStyle *dimstyle
 );
+DxfDimStyle *
+dxf_dimstyle_set_dimscale
+(
+        DxfDimStyle *dimstyle,
+        double dimscale
+);
 
 
 #endif /* LIBDXF_SRC_DIMSTYLE_H */
