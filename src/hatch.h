73b84987914a2790070c6051e57c1fce68281722
bert <bert.timmerman@xs4all.nl>
2016-01-07 07:38:24 +0100

    HATCH: added dxf_hatch_boundary_path_edge_spline_get_rational().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.h
+++ src/hatch.h
@@ -1157,6 +1157,11 @@ dxf_hatch_boundary_path_edge_spline_set_degree
         int degree
 );
 int
+dxf_hatch_boundary_path_edge_spline_get_rational
+(
+        DxfHatchBoundaryPathEdgeSpline *spline
+);
+int
 dxf_hatch_boundary_path_edge_spline_append_control_point
 (
         DxfHatchBoundaryPathEdgeSpline *spline,
