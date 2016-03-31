44047710a87da7b555346ac1ca3929837ad6ab9f
bert <bert.timmerman@xs4all.nl>
2016-03-31 21:12:16 +0200

    Removed cruft from the build script.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 make distclean
-#autoreconf
 ./autogen.sh
 ./configure
 make
 make -C po update-po
 make -C po libdxf.pot
-#../cppcheck/cppcheck -I ./src -f --enable=all ./ 2>libdxf_error.txt
+
