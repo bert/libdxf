ed959b5893ce1a95c57db68d0b50b75216d2bb3d
bert <bert.timmerman@xs4all.nl>
2016-04-08 23:52:26 +0200

    Added proprietary_data.[ch].
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/proprietary_data.h
@@ -0,0 +1,64 @@
+/*!
+ * \file proprietary_data.h
+ *
+ * \author Copyright (C) 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF proprietary data entity.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef LIBDXF_SRC_PROPRIETARY_DATA_H
+#define LIBDXF_SRC_PROPRIETARY_DATA_H
+
+
+#include "global.h"
+
+
+/*!
+ * \brief DXF definition of an AutoCAD Proprietary data entity.
+ */
+typedef struct
+dxf_proprietary_data_struct
+{
+        int i;
+                /*!< Incremental counter for storing order of data lines. */
+        char *data_line;
+                /*!< Proprietary data.\n
+                 * Multiple lines of 256 characters maximum per line
+                 * (optional).\n
+                 * Group code = 1. */
+        struct DxfProprietaryData *next;
+                /*!< Pointer to the next DxfProprietaryData.\n
+                 * \c NULL if the last DxfProprietaryData. */
+} DxfProprietaryData;
+
+
+#endif /* LIBDXF_SRC_PROPRIETARY_DATA_H */
+
+
+/* EOF */
