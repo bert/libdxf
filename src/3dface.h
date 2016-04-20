f19347cc131a039cf4b54b661cb5b4085e912b69
bert <bert.timmerman@xs4all.nl>
2016-04-20 06:31:04 +0200

    3DFACE: added version information to struct members.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.h
+++ src/3dface.h
@@ -71,21 +71,24 @@ dxf_3dface_struct
                 /*!< Elevation of the arc in the local Z-direction.\n
                  * Defaults to 0.0 if omitted in the DXF file, or prior
                  * to DXF version R12, or DXF_FLATLAND equals 0 (default).\n
-                 * Group code = 38. */
+                 * Group code = 38.\n
+                 * \deprecated As of version R11. */
         double thickness;
                 /*!< Thickness of the arc in the local Z-direction.\n
                  * Defaults to 0.0 if ommitted in the DXF file.\n
                  * Group code = 39. */
         double linetype_scale;
                 /*!< Linetype scale (optional).\n
-                 * Group code = 48. */
+                 * Group code = 48.\n
+                 * \since Introduced in version R13. */
         int16_t visibility;
                 /*!< Object visibility (optional):\n
                  * <ol>
                  * <li value = "0"> Visible</li>
                  * <li value = "1"> Invisible</li>
                  * </ol>
-                 * Group code = 60. */
+                 * Group code = 60.\n
+                 * \since Introduced in version R13. */
         int color;
                 /*!< Color of the entity.\n
                  * Defaults to \c BYLAYER if ommitted in the DXF file.\n
@@ -102,8 +105,8 @@ dxf_3dface_struct
                 /*!< Number of bytes in the proxy entity graphics
                  * represented in the sub-sequent 310 groups, which are
                  * binary chunk records (optional).\n
-                 * Group code = 92.
-                 *
+                 * Group code = 92.\n
+                 * \since Introduced in version R2000.\n
                  * \warning On some 64 bit workstations output is
                  * generated with group code "160", thus omitting group
                  * code "92".
@@ -116,29 +119,36 @@ dxf_3dface_struct
                  * <li value = "2"> Receives shadows.</li>
                  * <li value = "3"> Ignores shadows.</li>
                  * </ol>\n
-                 * Group code = 284. */
+                 * Group code = 284.\n
+                 * \since Introduced in version R2009. */
         DxfBinaryGraphicsData *binary_graphics_data;
                 /*!< Proxy entity graphics data.\n
                  * Multiple lines of 256 characters maximum per line
                  * (optional).\n
-                 * Group code = 310. */
+                 * Group code = 310.\n
+                 * \since Introduced in version R2000. */
         char *dictionary_owner_soft;
                 /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
-                 * Group code = 330. */
+                 * Group code = 330.\n
+                 * \since Introduced in version R14. */
         char *material;
                 /*!< Hard-pointer ID/handle to material object (present if
                  * not BYLAYER).\n
-                 * Group code = 347. */
+                 * Group code = 347.\n
+                 * \since Introduced in version R2008. */
         char *dictionary_owner_hard;
                 /*!< Hard owner ID/handle to owner dictionary (optional).\n
-                 * Group code = 360. */
+                 * Group code = 360.\n
+                 * \since Introduced in version R14. */
         int16_t lineweight;
                 /*!< Lineweight enum value.\n
                  * Stored and moved around as a 16-bit integer.\n
-                 * Group code = 370. */
+                 * Group code = 370.\n
+                 * \since Introduced in version R2002. */
         char *plot_style_name;
                 /*!< Hard pointer ID / handle of PlotStyleName object.\n
-                 * Group code = 390. */
+                 * Group code = 390.\n
+                 * \since Introduced in version R2009. */
         long color_value;
                 /*!< A 24-bit color value that should be dealt with in
                  * terms of bytes with values of 0 to 255.\n
@@ -149,21 +159,24 @@ dxf_3dface_struct
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 420. */
+                 * Group code = 420.\n
+                 * \since Introduced in version R2004. */
         char *color_name;
                 /*!< Color name.\n
                  * The group code cannot be used by custom entities for
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 430. */
+                 * Group code = 430.\n
+                 * \since Introduced in version R2004. */
         long transparency;
                 /*!< Transparency value.\n
                  * The group code cannot be used by custom entities for
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 440. */
+                 * Group code = 440.\n
+                 * \since Introduced in version R2004. */
         /* Specific members for a DXF 3D face. */
         DxfPoint *p0;
                 /*!< Base point.\n
