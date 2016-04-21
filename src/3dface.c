4b75898ea3cdaf72abb8aaf8127992e23c253e26
bert <bert.timmerman@xs4all.nl>
2016-04-21 20:00:09 +0200

    3DFACE: updated dxf_3dface_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -696,6 +696,7 @@ dxf_3dface_free
         }
         free (face->linetype);
         free (face->layer);
+        dxf_binary_graphics_data_free_chain (face->binary_graphics_data);
         free (face->dictionary_owner_soft);
         free (face->material);
         free (face->dictionary_owner_hard);
