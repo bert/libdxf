18ad42cd5392b97fcccbfec06f80cfdf460af478
bert <bert.timmerman@xs4all.nl>
2016-04-30 12:44:58 +0200

    ACAD_PROXY_ENTITY: maintain order.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/acad_proxy_entity.h
+++ src/acad_proxy_entity.h
@@ -370,6 +370,12 @@ dxf_acad_proxy_entity_get_dictionary_owner_soft
 (
         DxfAcadProxyEntity *acad_proxy_entity
 );
+DxfAcadProxyEntity *
+dxf_acad_proxy_entity_set_dictionary_owner_soft
+(
+        DxfAcadProxyEntity *acad_proxy_entity,
+        char *dictionary_owner_soft
+);
 char *
 dxf_acad_proxy_entity_get_material
 (
@@ -380,12 +386,6 @@ dxf_acad_proxy_entity_set_material
 (
         DxfAcadProxyEntity *acad_proxy_entity,
         char *material
-);
-DxfAcadProxyEntity *
-dxf_acad_proxy_entity_set_dictionary_owner_soft
-(
-        DxfAcadProxyEntity *acad_proxy_entity,
-        char *dictionary_owner_soft
 );
 char *
 dxf_acad_proxy_entity_get_dictionary_owner_hard
