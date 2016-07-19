f7dff06f000271a5d4ef871bf18bec2433fd0262
bert <bert.timmerman@xs4all.nl>
2016-07-19 07:54:13 +0200

    DICTIONARY: moved version info from doxygen comments.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dictionary.h
+++ src/dictionary.h
@@ -7,6 +7,12 @@
  *
  * \version The \c DICTIONARY object was introduced in DXF R13.
  *
+ * \version According to DXF R10 (backward compatibility).
+ * \version According to DXF R11 (backward compatibility).
+ * \version According to DXF R12 (backward compatibility).
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ *
  * <hr>
  * <h1><b>Copyright Notices.</b></h1>\n
  * This program is free software; you can redistribute it and/or modify
@@ -43,12 +49,6 @@
 
 /*!
  * \brief DXF definition of an AutoCAD dictionary object (\c DICTIONARY).
- *
- * \version According to DXF R10 (backward compatibility).
- * \version According to DXF R11 (backward compatibility).
- * \version According to DXF R12 (backward compatibility).
- * \version According to DXF R13.
- * \version According to DXF R14.
  */
 typedef struct
 dxf_dictionary
