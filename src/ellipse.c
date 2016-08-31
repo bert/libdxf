5636b4c7780779e6551b16384ed5b9c1d030e42b
bert <bert.timmerman@xs4all.nl>
2016-08-31 21:47:29 +0200

    ELLIPSE: correction in dxf_ellipse_get_layer().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -822,8 +822,6 @@ dxf_ellipse_get_layer
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
-        char *result = NULL;
-
         /* Do some basic checks. */
         if (ellipse == NULL)
         {
@@ -839,11 +837,10 @@ dxf_ellipse_get_layer
                   __FUNCTION__);
                 return (NULL);
         }
-        result = strdup (ellipse->layer);
 #if DEBUG
         DXF_DEBUG_END
 #endif
-        return (result);
+        return (strdup (ellipse->layer));
 }
 
 
