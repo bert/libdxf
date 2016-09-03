06449db2a0273ed0bca32ddfd2b7dfcf6a3abe48
bert <bert.timmerman@xs4all.nl>
2016-09-03 10:17:08 +0200

    ELLIPSE: added dxf_ellipse_get_extr_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -3125,6 +3125,38 @@ dxf_ellipse_set_extr_x0
 
 
 /*!
+ * \brief Get the Y-value of the extrusion vector of a DXF \c ELLIPSE
+ * entity.
+ *
+ * \return the Y-value of the extrusion vector.
+ */
+double
+dxf_ellipse_get_extr_y0
+(
+        DxfEllipse *ellipse
+                /*!< a pointer to a DXF \c ELLIPSE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (ellipse == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ellipse->extr_y0);
+}
+
+
+/*!
  * \brief Get the extrusion vector as a DXF \c POINT entity from a DXF
  * \c ELLIPSE entity.
  *
