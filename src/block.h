4403547d7c3e2a1ae95ea032fba2ca8de11e07cc
bert <bert.timmerman@xs4all.nl>
2016-09-26 15:14:37 +0200

    BLOCK: added dxf_block_set_extr_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block.h
+++ src/block.h
@@ -336,6 +336,12 @@ dxf_block_get_extr_y0
 (
         DxfBlock *block
 );
+DxfBlock *
+dxf_block_set_extr_y0
+(
+        DxfBlock *block,
+        double extr_y0
+);
 DxfPoint *
 dxf_block_get_extrusion_vector_as_point
 (
