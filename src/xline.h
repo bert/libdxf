c487f4b08177e89fad2878b9bfcdb5eb5d6f9c28
bert <bert.timmerman@xs4all.nl>
2016-09-30 20:13:11 +0200

    XLINE: prevent function name mangling in C++.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/xline.h
+++ src/xline.h
@@ -1,7 +1,7 @@
 /*!
  * \file xline.h
  *
- * \author Copyright (C) 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF xline entity (\c XLINE).
  *
@@ -37,6 +37,11 @@
 
 
 #include "global.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 
 /*!
@@ -153,6 +158,11 @@ dxf_xline_free_chain
 (
         DxfXLine *xlines
 );
+
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* LIBDXF_SRC_XLINE_H */
