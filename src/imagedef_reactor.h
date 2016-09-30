4c62fcde58cfb14b5b16b597dbbb37d4f11a2150
bert <bert.timmerman@xs4all.nl>
2016-09-30 18:29:01 +0200

    IMAGEDEF_REACTOR: prevent function name mangling in C++.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef_reactor.h
+++ src/imagedef_reactor.h
@@ -1,7 +1,7 @@
 /*!
  * \file imagedef_reactor.h
  *
- * \author Copyright (C) 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF imagedef_reactor object (\c IMAGEDEF_REACTOR).
  *
@@ -39,6 +39,11 @@
 
 
 #include "global.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 
 /*!
@@ -109,6 +114,11 @@ dxf_imagedef_reactor_free_chain
 (
         DxfImagedefReactor *imagedef_reactors
 );
+
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif /* LIBDXF_SRC_IMAGEDEF_REACTOR_H */
