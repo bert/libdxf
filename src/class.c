8c29f6d395b44c11f210fbff003b3113a8f7f6a9
bert <bert.timmerman@xs4all.nl>
2016-10-07 20:25:52 +0200

    CLASS: use setter functions in dxf_class_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/class.c
+++ src/class.c
@@ -1,7 +1,7 @@
 /*!
  * \file class.c
  *
- * \author Copyright (C) 2009 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2009 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF class entity (\c CLASS).
  *
@@ -104,14 +104,14 @@ dxf_class_init
                 __FUNCTION__);
               return (NULL);
         }
-        class->record_type = strdup ("");
-        class->record_name = strdup ("");
-        class->class_name = strdup ("");
-        class->app_name = strdup ("");
-        class->proxy_cap_flag = 0;
-        class->was_a_proxy_flag = 0;
-        class->is_an_entity_flag = 0;
-        class->next = NULL;
+        dxf_class_set_record_type (class, strdup (""));
+        dxf_class_set_record_name (class, strdup (""));
+        dxf_class_set_class_name (class, strdup (""));
+        dxf_class_set_app_name (class, strdup (""));
+        dxf_class_set_proxy_cap_flag (class, 0);
+        dxf_class_set_was_a_proxy_flag (class, 0);
+        dxf_class_set_is_an_entity_flag (class, 0);
+        dxf_class_set_next (class, NULL);
 #if DEBUG
         DXF_DEBUG_END
 #endif
