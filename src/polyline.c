95882e57b50e059e82828359840b4cbaa3092378
bert <bert.timmerman@xs4all.nl>
2016-11-26 23:15:32 +0100

    POLYLINE: added dxf_polyline_get_polygon_mesh_N_vertex_count().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/polyline.c
+++ src/polyline.c
@@ -2999,4 +2999,40 @@ dxf_polyline_set_polygon_mesh_M_vertex_count
 }
 
 
+/*!
+ * \brief Get the \c polygon_mesh_N_vertex_count from a DXF \c POLYLINE entity.
+ *
+ * \return \c polygon_mesh_N_vertex_count.
+ */
+int
+dxf_polyline_get_polygon_mesh_N_vertex_count
+(
+        DxfPolyline *polyline
+                /*!< a pointer to a DXF \c POLYLINE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (polyline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (polyline->polygon_mesh_N_vertex_count < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found in the polygon_mesh_N_vertex_count member.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (polyline->polygon_mesh_N_vertex_count);
+}
+
+
 /* EOF */
--- src/polyline.h
+++ src/polyline.h
@@ -654,6 +654,11 @@ dxf_polyline_set_polygon_mesh_M_vertex_count
         DxfPolyline *polyline,
         int polygon_mesh_M_vertex_count
 );
+int
+dxf_polyline_get_polygon_mesh_N_vertex_count
+(
+        DxfPolyline *polyline
+);
 
 
 #ifdef __cplusplus
