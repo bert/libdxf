ae59344b26c9fdd5e7db91a0bf955ef066a498ff
bert <bert.timmerman@xs4all.nl>
2016-12-12 21:02:39 +0100

    VERTEX: renamed dxf_vertex to dxf_vertex_struct.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vertex.h
+++ src/vertex.h
@@ -75,7 +75,7 @@ extern "C" {
  * \version According to DXF R14.
  */
 typedef struct
-dxf_vertex
+dxf_vertex_struct
 {
         /* Members common for all DXF drawable entities. */
         int id_code;
