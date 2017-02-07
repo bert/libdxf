3983108385ffe0da7cf2ec0f5110eb2fc1ebf2f8
bert <bert.timmerman@xs4all.nl>
2017-02-07 21:42:56 +0100

    3DFACE: whitespace correction.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -1,7 +1,7 @@
 /*!
  * \file 3dface.c
  *
- * \author Copyright (C) 2010, 2012, 2013, 2014,2015, 2016, 2017 by Bert
+ * \author Copyright (C) 2010, 2012, 2013, 2014, 2015, 2016, 2017 by Bert
  * Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF 3D face entity (\c 3DFACE).
