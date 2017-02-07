a3f55ea43c46ec9cee566622e7725544cfb3b755
bert <bert.timmerman@xs4all.nl>
2017-02-07 21:41:34 +0100

    ARC: bumped the copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -1,7 +1,8 @@
 /*!
  * \file arc.c
  *
- * \author Copyright (C) 2008 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2010, 2012, 2013, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF arc entity (\c ARC).
  *
