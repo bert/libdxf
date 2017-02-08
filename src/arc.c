828aa11adaa55b4043445dc7f40386ab0c3d0081
bert <bert.timmerman@xs4all.nl>
2017-02-08 07:24:51 +0100

    ARC: standardise one more error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -3171,7 +3171,7 @@ dxf_arc_get_next
         if (arc->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
