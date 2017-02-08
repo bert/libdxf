29e87da7fb7f521133a5cea718ccfa964c0bcb5b
bert <bert.timmerman@xs4all.nl>
2017-02-08 18:39:16 +0100

    INSERT: bumped the copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/insert.h
+++ src/insert.h
@@ -1,7 +1,8 @@
 /*!
  * \file insert.h
  *
- * \author Copyright (C) 2008 ... 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF insert entity (\c INSERT).
  *
