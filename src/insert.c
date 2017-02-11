803840aff0397ce97f86b6f3468a0e8c18fa0639
bert <bert.timmerman@xs4all.nl>
2017-02-11 21:40:16 +0100

    INSERT: corrections in dxf_insert_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/insert.c
+++ src/insert.c
@@ -249,21 +249,21 @@ dxf_insert_read
                         /* Now follows a string containing the
                          * X-coordinate of the center point. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%lf\n", &insert->x0);
+                        fscanf (fp->fp, "%lf\n", &insert->p0->x0);
                 }
                 else if (strcmp (temp_string, "20") == 0)
                 {
                         /* Now follows a string containing the
                          * Y-coordinate of the center point. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%lf\n", &insert->y0);
+                        fscanf (fp->fp, "%lf\n", &insert->p0->y0);
                 }
                 else if (strcmp (temp_string, "30") == 0)
                 {
                         /* Now follows a string containing the
                          * Z-coordinate of the center point. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%lf\n", &insert->z0);
+                        fscanf (fp->fp, "%lf\n", &insert->p0->z0);
                 }
                 else if ((fp->acad_version_number <= AutoCAD_11)
                         && (strcmp (temp_string, "38") == 0)
