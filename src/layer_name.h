2fdebcfb4a287e8f83dcd3cdc2e21b9d70075ce9
bert <bert.timmerman@xs4all.nl>
2017-03-14 23:30:37 +0100

    LAYER_NAME: added dxf_layer_name_struct.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/layer_name.h
@@ -0,0 +1,74 @@
+/*!
+ * \file layer_name.h
+ *
+ * \author Copyright (C) 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF layer name object.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef LIBDXF_SRC_LAYER_NAME_H
+#define LIBDXF_SRC_LAYER_NAME_H
+
+
+#include "global.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*!
+ * \brief DXF definition of an AutoCAD layer name object.
+ */
+typedef struct
+dxf_layer_name_struct
+{
+        char *name;
+                /*!< Layer name.\n
+                 * Multiple lines of 256 characters maximum per line
+                 * (optional).\n
+                 * Group code = 8. */
+        int length;
+                /*!< Length of the \c name member. */
+        struct DxfLayerName *next;
+                /*!< Pointer to the next DxfLayerName.\n
+                 * \c NULL if the last DxfLayerName. */
+} DxfLayerName;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* LIBDXF_SRC_LAYER_NAME_H */
+
+
+/* EOF */
