fa6952ac29b477c5295efa4a5c929ff324d28c0c
bert <bert.timmerman@xs4all.nl>
2017-04-17 23:58:16 +0200

    MLINE: added dxf_mline_set_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mline.c
+++ src/mline.c
@@ -1831,4 +1831,44 @@ dxf_mline_get_dictionary_owner_soft
 }
 
 
+/*!
+ * \brief Set the pointer to the dictionary_owner_soft for a DXF
+ * \c MLINE entity.
+ */
+DxfMline *
+dxf_mline_set_dictionary_owner_soft
+(
+        DxfMline *mline,
+                /*!< a pointer to a DXF \c MLINE entity. */
+        char *dictionary_owner_soft
+                /*!< a string containing the pointer to the
+                 * dictionary_owner_soft for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (dictionary_owner_soft == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        mline->dictionary_owner_soft = strdup (dictionary_owner_soft);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mline);
+}
+
+
 /* EOF */
