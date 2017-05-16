489f77670ef2d4df63d0430cc7effbacc93dc161
bert <bert.timmerman@xs4all.nl>
2017-05-16 22:27:06 +0200

    MTEXT: better copyrights.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mtext.c
+++ src/mtext.c
@@ -1,7 +1,8 @@
 /*!
  * \file mtext.c
  *
- * \author Copyright (C) 2012 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2012, 2013, 2014, 2015, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF mtext entity (\c MTEXT).
  *
