3e69dc48ed71e24798545d2ad67a8e0adc88b2a1
bert <bert.timmerman@xs4all.nl>
2017-07-02 00:02:08 +0200

    BLOCK: better copyright notices.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block.h
+++ src/block.h
@@ -1,7 +1,10 @@
 /*!
  * \file block.h
  *
- * \author Copyright (C) 2008 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2009, 2010, 2012, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
  * \brief Header file for a DXF block entity (\c BLOCK).
  *
