e08275c89de3e71bc358f667bf184d544c72f3c1
bert <bert.timmerman@xs4all.nl>
2017-07-02 00:20:21 +0200

    BLOCK_RECORD: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.h
+++ src/block_record.h
@@ -1,7 +1,7 @@
 /*!
  * \file block_record.h
  *
- * \author Copyright (C) 2015 .. 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF block record symbol table entry
  * (\c BLOCK_RECORD).
