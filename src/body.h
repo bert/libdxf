d2e1a700d557d04587c0fa54acd7054075d48d41
bert <bert.timmerman@xs4all.nl>
2017-07-02 16:25:11 +0200

    BODY: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/body.h
+++ src/body.h
@@ -1,7 +1,8 @@
 /*!
  * \file body.h
  *
- * \author Copyright (C) 2013 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2013, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF body entity (\c BODY).
  *
