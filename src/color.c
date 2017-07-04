2d794116efdaa4ee5130a148a60711ee27504362
bert <bert.timmerman@xs4all.nl>
2017-07-05 00:44:26 +0200

    COLOR: better copyright notices.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/color.c
+++ src/color.c
@@ -1,7 +1,8 @@
 /*!
  * \file color.c
  *
- * \author Copyright (C) 2012 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2012, 2014, 2015, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for DXF colors.
  *
