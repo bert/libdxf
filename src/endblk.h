fa080e2e7d6ba3741cd51a459e52b2dcb0c4c81d
bert <bert.timmerman@xs4all.nl>
2017-07-09 01:39:11 +0200

    ENDBLK: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/endblk.h
+++ src/endblk.h
@@ -1,7 +1,8 @@
 /*!
  * \file endblk.h
  *
- * \author Copyright (C) 2014 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF end of block marker entity (\c ENDBLK).
  *
