3ad901c41cc5e2db8fb4367395ec333db3c405b2
bert <bert.timmerman@xs4all.nl>
2017-07-10 22:26:33 +0200

    HEADER: better copyright notices.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/header.h
+++ src/header.h
@@ -1,7 +1,10 @@
 /*!
  * \file header.h
  *
- * \author Copyright (C) 2008 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.\n
+ * \author Copyright (C) 2008, 2009, 2010, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.\n
+ *
+ * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
  * \brief Definition of a DXF header section.
  *
