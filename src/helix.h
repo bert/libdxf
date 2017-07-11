4e8f16cc02c91aa894d689b54f1503417166f592
bert <bert.timmerman@xs4all.nl>
2017-07-11 07:35:52 +0200

    HELIX: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.h
+++ src/helix.h
@@ -1,7 +1,8 @@
 /*!
  * \file helix.h
  *
- * \author Copyright (C) 2013 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2013, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF helix entity (\c HELIX).
  *
