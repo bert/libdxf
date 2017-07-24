e71c995f2947a754bf16b60109530f10893f1cbe
bert <bert.timmerman@xs4all.nl>
2017-07-24 21:42:09 +0200

    LWPOLYLINE: better copyright notices.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/lwpolyline.c
+++ src/lwpolyline.c
@@ -4,6 +4,8 @@
  * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
+ * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
+ *
  * \brief Functions for a DXF light weight polyline entity (\c LWPOLYLINE).
  *
  * \warning This entity requires AutoCAD version R14 or higher.
