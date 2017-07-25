773fdb0abeca6e18356a61679d4c0888d96b2568
bert <bert.timmerman@xs4all.nl>
2017-07-25 08:40:17 +0200

    OLE2FRAME: added dxf_ole2frame_get_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ole2frame.c
+++ src/ole2frame.c
@@ -742,4 +742,41 @@ dxf_ole2frame_set_id_code
 }
 
 
+/*!
+ * \brief Get the \c linetype from a DXF \c OLE2FRAME entity.
+ *
+ * \return \c linetype when sucessful, \c NULL when an error occurred.
+ */
+char *
+dxf_ole2frame_get_linetype
+(
+        DxfOle2Frame *ole2frame
+                /*!< a pointer to a DXF \c OLE2FRAME entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ole2frame == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (ole2frame->linetype ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (ole2frame->linetype));
+}
+
+
 /* EOF */
--- src/ole2frame.h
+++ src/ole2frame.h
@@ -167,6 +167,7 @@ int dxf_ole2frame_free (DxfOle2Frame *ole2frame);
 void dxf_ole2frame_free_chain (DxfOle2Frame *ole2frames);
 int dxf_ole2frame_get_id_code (DxfOle2Frame *ole2frame);
 DxfOle2Frame *dxf_ole2frame_set_id_code (DxfOle2Frame *ole2frame, int id_code);
+char *dxf_ole2frame_get_linetype (DxfOle2Frame *ole2frame);
 
 
 #ifdef __cplusplus
