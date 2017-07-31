4374f54c234c5666fe5ee061a4ffa25ce9e9b2be
bert <bert.timmerman@xs4all.nl>
2017-07-31 23:29:12 +0200

    OLE2FRAME: added dxf_ole2frame_set_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ole2frame.c
+++ src/ole2frame.c
@@ -1645,4 +1645,44 @@ dxf_ole2frame_get_dictionary_owner_soft
 }
 
 
+/*!
+ * \brief Set the pointer to the dictionary_owner_soft for a DXF
+ * \c OLE2FRAME entity.
+ */
+DxfOle2Frame *
+dxf_ole2frame_set_dictionary_owner_soft
+(
+        DxfOle2Frame *ole2frame,
+                /*!< a pointer to a DXF \c OLE2FRAME entity. */
+        char *dictionary_owner_soft
+                /*!< a string containing the pointer to the
+                 * dictionary_owner_soft for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ole2frame == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (dictionary_owner_soft == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        ole2frame->dictionary_owner_soft = strdup (dictionary_owner_soft);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ole2frame);
+}
+
+
 /* EOF */
--- src/ole2frame.h
+++ src/ole2frame.h
@@ -258,6 +258,7 @@ DxfOle2Frame *dxf_ole2frame_set_shadow_mode (DxfOle2Frame *ole2frame, int16_t sh
 DxfBinaryGraphicsData *dxf_ole2frame_get_binary_graphics_data (DxfOle2Frame *ole2frame);
 DxfOle2Frame *dxf_ole2frame_set_binary_graphics_data (DxfOle2Frame *ole2frame, DxfBinaryGraphicsData *data);
 char *dxf_ole2frame_get_dictionary_owner_soft (DxfOle2Frame *ole2frame);
+DxfOle2Frame *dxf_ole2frame_set_dictionary_owner_soft (DxfOle2Frame *ole2frame, char *dictionary_owner_soft);
 
 
 #ifdef __cplusplus
