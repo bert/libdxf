2264a9bd8397dd4f9a9dc7fba9293706cf00c23e
bert <bert.timmerman@xs4all.nl>
2017-08-01 23:58:39 +0200

    OLE2FRAME: added dxf_ole2frame_get_transparency().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ole2frame.h
+++ src/ole2frame.h
@@ -271,6 +271,7 @@ long dxf_ole2frame_get_color_value (DxfOle2Frame *ole2frame);
 DxfOle2Frame *dxf_ole2frame_set_color_value (DxfOle2Frame *ole2frame, long color_value);
 char *dxf_ole2frame_get_color_name (DxfOle2Frame *ole2frame);
 DxfOle2Frame *dxf_ole2frame_set_color_name (DxfOle2Frame *ole2frame, char *color_name);
+long dxf_ole2frame_get_transparency (DxfOle2Frame *ole2frame);
 
 
 #ifdef __cplusplus
