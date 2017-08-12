932b2b69406fd8a10fcf05e9d7f7f7128231bb3e
bert <bert.timmerman@xs4all.nl>
2017-08-12 23:45:22 +0200

    DIMSTYLE: added a test for a NULL pointer in dxf_dimstyle_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimstyle.c
+++ src/dimstyle.c
@@ -900,6 +900,14 @@ dxf_dimstyle_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (dimstyle == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dimstyle->next != NULL)
         {
               fprintf (stderr,
