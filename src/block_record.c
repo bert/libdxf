742e6d49d0f4f5b1f6372f704ee13f70739453f6
bert <bert.timmerman@xs4all.nl>
2017-08-12 23:32:56 +0200

    BLOCK_RECORD: added a test for a NULL pointer in dxf_block_record_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.c
+++ src/block_record.c
@@ -362,6 +362,13 @@ dxf_block_record_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        if (block_record == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (block_record->next != NULL)
         {
               fprintf (stderr,
