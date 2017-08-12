3534b02eb779ba97a754be5b2c13b7429fe03e80
bert <bert.timmerman@xs4all.nl>
2017-08-12 23:43:20 +0200

    DIMENSION: added a test for a NULL pointer in dxf_dimension_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimension.c
+++ src/dimension.c
@@ -925,6 +925,14 @@ dxf_dimension_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (dimension == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (dimension->next != NULL)
         {
               fprintf (stderr,
