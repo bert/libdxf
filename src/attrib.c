7d154b285fc0b908a7ab094d5c91bd522aa28fa4
bert <bert.timmerman@xs4all.nl>
2017-08-12 12:05:21 +0200

    ATTRIB: added a test for a NULL pointer in dxf_attrib_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/attrib.c
+++ src/attrib.c
@@ -771,6 +771,13 @@ dxf_attrib_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        if (attrib == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (attrib->next != NULL)
         {
               fprintf (stderr,
