715ef8c849bf9402f8be962226ac1aaad73ea6a7
bert <bert.timmerman@xs4all.nl>
2017-08-12 23:37:13 +0200

    CLASS: added a test for a NULL pointer in dxf_class_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/class.c
+++ src/class.c
@@ -435,6 +435,13 @@ dxf_class_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        if (class == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (class->next != NULL)
         {
               fprintf (stderr,
