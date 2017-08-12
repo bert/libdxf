c910653b83c53ed4b404b88e6c777ac6aef2e3fd
bert <bert.timmerman@xs4all.nl>
2017-08-13 00:31:52 +0200

    IMAGE: added a test for a NULL pointer in dxf_image_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -708,6 +708,14 @@ dxf_image_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (image == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (image->next != NULL)
         {
               fprintf (stderr,
