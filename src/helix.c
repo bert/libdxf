6bc979066d37f59dffa5cf337a54da2f89cc41f3
bert <bert.timmerman@xs4all.nl>
2017-08-12 23:52:10 +0200

    HELIX: added a test for a NULL pointer in dxf_helix_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -725,6 +725,14 @@ dxf_helix_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (helix == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (helix->next != NULL)
         {
               fprintf (stderr,
