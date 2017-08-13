bb0cfe0cc6cddd62e29476e4ede25a28c98dfb16
bert <bert.timmerman@xs4all.nl>
2017-08-13 22:51:35 +0200

    SOLID: added a test for a NULL pointer in dxf_solid_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.c
+++ src/solid.c
@@ -1,7 +1,7 @@
 /*!
  * \file solid.c
  *
- * \author Copyright (C) 2008 ... 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008 ... 2015, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF solid entity (\c SOLID).
  *
@@ -631,6 +631,14 @@ dxf_solid_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (solid == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (solid->next != NULL)
         {
                 fprintf (stderr,
