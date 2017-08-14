7dfd4ebe968e093ba8ba0ac5ea06216068010f4b
bert <bert.timmerman@xs4all.nl>
2017-08-15 00:22:23 +0200

    UCS: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ucs.c
+++ src/ucs.c
@@ -492,7 +492,7 @@ dxf_ucs_free
         if (ucs->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfUcs was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
