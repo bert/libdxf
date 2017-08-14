40b847087d87bb990824acf1b0a3cc414c0aee82
bert <bert.timmerman@xs4all.nl>
2017-08-15 00:38:02 +0200

    VIEW: added a test for a NULL pointer in dxf_view_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/view.c
+++ src/view.c
@@ -1,7 +1,7 @@
 /*!
  * \file view.c
  *
- * \author Copyright (C) 2015 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF View symbol table entry (\c VIEW).
  *
@@ -531,6 +531,14 @@ dxf_view_free
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
+        /* Do some basic checks. */
+        if (view == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
         if (view->next != NULL)
         {
                 fprintf (stderr,
