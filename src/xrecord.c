7163c2c0456c2d47f48e9e7b6a07f762000d925b
bert <bert.timmerman@xs4all.nl>
2017-08-15 19:41:33 +0200

    XRECORD: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/xrecord.c
+++ src/xrecord.c
@@ -505,7 +505,7 @@ dxf_xrecord_free
         if (xrecord->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfXrecord was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
