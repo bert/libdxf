b68fea3b68095cf10d5f6a2d63e8194835607c89
bert <bert.timmerman@xs4all.nl>
2017-08-16 22:58:28 +0200

    SORTENTSTABLE: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sortentstable.c
+++ src/sortentstable.c
@@ -449,7 +449,7 @@ dxf_sortentstable_free
         if (sortentstable->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfSortentsTable was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
