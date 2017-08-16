d2b9108e0a05a1af8e76d39709aa7edc951bdd26
bert <bert.timmerman@xs4all.nl>
2017-08-16 22:32:04 +0200

    SPATIAL_FILTER: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_filter.c
+++ src/spatial_filter.c
@@ -598,7 +598,7 @@ dxf_spatial_filter_free
         if (spatial_filter->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfSpatialFilter was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
