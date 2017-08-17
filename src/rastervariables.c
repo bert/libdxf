9bbd837d79fb17bf9f9abb0af7b7c7c03baaaa00
bert <bert.timmerman@xs4all.nl>
2017-08-17 23:04:39 +0200

    RASTERVARIABLES: correction in an error messag.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rastervariables.c
+++ src/rastervariables.c
@@ -422,7 +422,7 @@ dxf_rastervariables_free
         if (rastervariables->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfRasterVariables was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
