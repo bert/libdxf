34920af2fe40df8389cdf47416c6588e87e4ec00
bert <bert.timmerman@xs4all.nl>
2017-08-17 23:07:11 +0200

    PROPRIETARY_DATA: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/proprietary_data.c
+++ src/proprietary_data.c
@@ -140,7 +140,7 @@ dxf_proprietary_data_free
         if (data->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfProprietaryData was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
