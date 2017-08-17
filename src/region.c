aa2929f03dadc838ef7b5f86cda9a86961d9e902
bert <bert.timmerman@xs4all.nl>
2017-08-17 22:34:26 +0200

    REGION: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -560,7 +560,7 @@ dxf_region_free
         if (region->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfRegion was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
