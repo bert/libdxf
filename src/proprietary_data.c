d91b084e5650f6a3607c2b207e7608c025d9bff0
bert <bert.timmerman@xs4all.nl>
2017-08-17 23:07:57 +0200

    PROPRIETARY_DATA: correction in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/proprietary_data.c
+++ src/proprietary_data.c
@@ -139,10 +139,10 @@ dxf_proprietary_data_free
         }
         if (data->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (data->line);
         free (data);
