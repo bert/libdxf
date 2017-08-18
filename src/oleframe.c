fff986f71d22843b2ec6b53b43899094624db598
bert <bert.timmerman@xs4all.nl>
2017-08-18 22:46:54 +0200

    OLEFRAME: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/oleframe.c
+++ src/oleframe.c
@@ -545,10 +545,10 @@ dxf_oleframe_free
         }
         if (oleframe->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (oleframe->linetype);
         free (oleframe->layer);
