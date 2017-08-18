8334cd93d389158323d91361532fc7a070efb112
bert <bert.timmerman@xs4all.nl>
2017-08-18 22:46:14 +0200

    OLEFRAME: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/oleframe.c
+++ src/oleframe.c
@@ -546,7 +546,7 @@ dxf_oleframe_free
         if (oleframe->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfOleFrame was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
