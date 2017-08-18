3cf5b5e5c8f20d42a6b0b7694ea1be865d259800
bert <bert.timmerman@xs4all.nl>
2017-08-18 22:53:57 +0200

    OBJECT_ID: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/object_id.c
+++ src/object_id.c
@@ -140,7 +140,7 @@ dxf_object_id_free
         if (object_id->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfObject was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
