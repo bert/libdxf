79f0a46831a7b7914613bcdeee8ea586927ee2a2
bert <bert.timmerman@xs4all.nl>
2017-08-18 14:17:45 +0200

    POINT: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/point.c
+++ src/point.c
@@ -560,7 +560,7 @@ dxf_point_free
         if (point->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfPoint was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
