247eee4e42f31de64a2643c047bec6f72a44b31e
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:52:04 +0200

    IMAGEDEF_REACTOR: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef_reactor.c
+++ src/imagedef_reactor.c
@@ -393,7 +393,7 @@ dxf_imagedef_reactor_free
         if (imagedef_reactor->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfImagedefReactor was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
