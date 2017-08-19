1af999b20da9d2fe145d2ab18f72d05082bff0f6
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:11:10 +0200

    MLINESTYLE: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mlinestyle.c
+++ src/mlinestyle.c
@@ -483,10 +483,10 @@ dxf_mlinestyle_free
         }
         if (mlinestyle->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (dxf_mlinestyle_get_dictionary_owner_soft (mlinestyle));
         free (dxf_mlinestyle_get_dictionary_owner_hard (mlinestyle));
