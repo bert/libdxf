566cc7e3efb2f74569cc16e759cd07c02e850aae
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:47:42 +0200

    LINE: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -698,7 +698,7 @@ dxf_line_free
         if (line->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfLine was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
