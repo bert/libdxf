7ae056cd3ae5e4b9b7234c9666b2fe0a38a59acc
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:12:07 +0200

    MLINE: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mline.c
+++ src/mline.c
@@ -911,7 +911,7 @@ dxf_mline_free
         if (mline->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfMline was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
