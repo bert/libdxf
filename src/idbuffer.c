0472ae3135faeba927a849783d0d170be90650d2
bert <bert.timmerman@xs4all.nl>
2017-08-19 21:42:41 +0200

    IDBUFFER: corrections in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/idbuffer.c
+++ src/idbuffer.c
@@ -382,7 +382,7 @@ dxf_idbuffer_free
         if (idbuffer->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfIdbuffer was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
