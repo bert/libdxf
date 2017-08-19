fe1608bbc7cb391fc17fed87a5b96974ad2599af
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:23:33 +0200

    LEADER: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/leader.c
+++ src/leader.c
@@ -788,10 +788,10 @@ dxf_leader_free
         }
         if (leader->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (leader->linetype);
         free (leader->layer);
