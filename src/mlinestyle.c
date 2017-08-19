5af596b9b320656d5668bf1b8343b3162449b5f7
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:10:28 +0200

    MLINESTYLE: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mlinestyle.c
+++ src/mlinestyle.c
@@ -484,7 +484,7 @@ dxf_mlinestyle_free
         if (mlinestyle->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfMlinestyle was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
