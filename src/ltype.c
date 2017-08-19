b8ce9355578fddb1aa0917ba986990a674e64b92
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:46:04 +0200

    LTYPE: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ltype.c
+++ src/ltype.c
@@ -586,7 +586,7 @@ dxf_ltype_free
         if (ltype->next != NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () pointer to next DxfLType was not NULL.\n")),
+                (_("Error in %s () pointer to next was not NULL.\n")),
                 __FUNCTION__);
               return (EXIT_FAILURE);
         }
