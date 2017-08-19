587637bf953e98734e853a691c3dfb69628667c6
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:51:05 +0200

    IMAGEDEF: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef.c
+++ src/imagedef.c
@@ -472,10 +472,10 @@ dxf_imagedef_free
         }
         if (imagedef->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (imagedef->dictionary_owner_soft);
         free (imagedef->dictionary_owner_hard);
