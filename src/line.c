b86a63fc50bcd3f5887b29e6d21e367706bcbe6b
bert <bert.timmerman@xs4all.nl>
2017-08-19 08:48:20 +0200

    LINE: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -697,10 +697,10 @@ dxf_line_free
         }
         if (line->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (dxf_line_get_linetype (line));
         free (dxf_line_get_layer (line));
