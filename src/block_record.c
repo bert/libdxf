d576e27a828ac8cc862161b9b33e81c2f0a79b51
bert <bert.timmerman@xs4all.nl>
2017-08-20 08:47:16 +0200

    BLOCK_RECORD: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.c
+++ src/block_record.c
@@ -371,10 +371,10 @@ dxf_block_record_free
         }
         if (block_record->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (block_record->block_name);
         free (block_record->dictionary_owner_soft);
