4a852df61fa10a23917ca59fcd9b187d22d8bb45
bert <bert.timmerman@xs4all.nl>
2017-08-22 00:27:19 +0200

    ELLIPSE: corrections in indentation.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -627,10 +627,10 @@ dxf_ellipse_free
         }
         if (ellipse->next != NULL)
         {
-              fprintf (stderr,
-                (_("Error in %s () pointer to next was not NULL.\n")),
-                __FUNCTION__);
-              return (EXIT_FAILURE);
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
         }
         free (ellipse->linetype);
         free (ellipse->layer);
