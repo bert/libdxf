0665afac0dbbaea203bd481e41b862f4cfd598b5
bert <bert.timmerman@xs4all.nl>
2017-08-22 23:47:06 +0200

    HATCH: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -11389,7 +11389,7 @@ dxf_hatch_boundary_path_edge_spline_free
         if (spline->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfHatchBoundaryPathEdgeSpline was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
