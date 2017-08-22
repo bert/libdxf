16a50d56d607f043565d09727face001bc2fa8b5
bert <bert.timmerman@xs4all.nl>
2017-08-22 23:49:32 +0200

    HATCH: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -6145,7 +6145,7 @@ dxf_hatch_boundary_path_polyline_free
         if (polyline->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfHatchBoundaryPathPolyline was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
