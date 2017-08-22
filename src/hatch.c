b4bac125b840805740d4ea0b7f5b6f19f8f20be9
bert <bert.timmerman@xs4all.nl>
2017-08-22 23:52:09 +0200

    HATCH: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -3661,7 +3661,7 @@ dxf_hatch_pattern_def_line_dash_free
         if (dash->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfHatchPatternDefLineDash was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
