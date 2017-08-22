859b9bfb4f3582aa251f0cf05a1deb5eab4fc85d
bert <bert.timmerman@xs4all.nl>
2017-08-22 23:43:25 +0200

    HATCH: correction in an error message.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -7763,7 +7763,7 @@ dxf_hatch_boundary_path_edge_free
         if (edge->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next DxfHatchBoundaryPathEdge was not NULL.\n")),
+                  (_("Error in %s () pointer to next was not NULL.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
