2705aaafc98bed81f2e2ece4589855ab8aa91389
bert <bert.timmerman@xs4all.nl>
2017-08-29 07:05:39 +0200

    OLEFRAME: added dxf_oleframe_get_material().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/oleframe.c
+++ src/oleframe.c
@@ -1623,4 +1623,44 @@ dxf_oleframe_set_dictionary_owner_soft
 }
 
 
+/*!
+ * \brief Get the pointer to the \c material from a DXF \c OLEFRAME entity.
+ *
+ * \return a pointer to \c material when successful, or \c NULL when an
+ * error occurred.
+ *
+ * \warning No checks are performed on the returned pointer.
+ */
+char *
+dxf_oleframe_get_material
+(
+        DxfOleFrame *oleframe
+                /*!< a pointer to a DXF \c OLEFRAME entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (oleframe == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (oleframe->material ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (oleframe->material));
+}
+
+
 /* EOF */
