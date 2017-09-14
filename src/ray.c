12b41cb5c3a978b6c9170b9efaa434f9e28c38d1
bert <bert.timmerman@xs4all.nl>
2017-09-14 22:21:05 +0200

    RAY: added dxf_ray_get_elevation().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ray.c
+++ src/ray.c
@@ -858,4 +858,34 @@ dxf_ray_set_layer
 }
 
 
+/*!
+ * \brief Get the \c elevation from a DXF \c RAY entity.
+ *
+ * \return \c elevation.
+ */
+double
+dxf_ray_get_elevation
+(
+        DxfRay *ray
+                /*!< a pointer to a DXF \c RAY entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ray == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ray->elevation);
+}
+
+
 /* EOF */
