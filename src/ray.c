c4be07bab106dde2b71909c1b9a0687530c8d563
bert <bert.timmerman@xs4all.nl>
2017-09-14 07:25:34 +0200

    RAY: added dxf_ray_get_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ray.c
+++ src/ray.c
@@ -706,4 +706,41 @@ dxf_ray_set_id_code
 }
 
 
+/*!
+ * \brief Get the \c linetype from a DXF \c RAY entity.
+ *
+ * \return \c linetype when sucessful, \c NULL when an error occurred.
+ */
+char *
+dxf_ray_get_linetype
+(
+        DxfRay *ray
+                /*!< a pointer to a DXF \c RAY entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ray == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (ray->linetype ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (ray->linetype));
+}
+
+
 /* EOF */
