3ba10efcacf41bd9e4207d15e1cb3d0389832dc1
bert <bert.timmerman@xs4all.nl>
2017-09-17 09:53:00 +0200

    RAY: added dxf_ray_set_lineweight().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ray.c
+++ src/ray.c
@@ -1856,4 +1856,38 @@ dxf_ray_get_lineweight
 }
 
 
+/*!
+ * \brief Set the \c lineweight for a DXF \c RAY entity.
+ *
+ * \return a pointer to \c ray when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfRay *
+dxf_ray_set_lineweight
+(
+        DxfRay *ray,
+                /*!< a pointer to a DXF \c RAY entity. */
+        int16_t lineweight
+                /*!< the \c lineweight to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ray == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        ray->lineweight = lineweight;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ray);
+}
+
+
 /* EOF */
