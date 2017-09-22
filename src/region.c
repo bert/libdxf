4f1c874ab6e8abc939f53b875ed937aa2fcd7311
bert <bert.timmerman@xs4all.nl>
2017-09-22 19:34:36 +0200

    REGION: added dxf_region_get_thickness().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -881,4 +881,41 @@ dxf_region_set_elevation
 }
 
 
+/*!
+ * \brief Get the \c thickness from a DXF \c REGION entity.
+ *
+ * \return \c thickness.
+ */
+double
+dxf_region_get_thickness
+(
+        DxfRegion *region
+                /*!< a pointer to a DXF \c REGION entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (region == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (region->thickness < 0.0)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (region->thickness);
+}
+
+
 /* EOF */
--- src/region.h
+++ src/region.h
@@ -218,6 +218,7 @@ char *dxf_region_get_layer (DxfRegion *region);
 DxfRegion *dxf_region_set_layer (DxfRegion *region, char *layer);
 double dxf_region_get_elevation (DxfRegion *region);
 DxfRegion *dxf_region_set_elevation (DxfRegion *region, double elevation);
+double dxf_region_get_thickness (DxfRegion *region);
 
 
 #ifdef __cplusplus
