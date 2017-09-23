246274624c4b0829441d2a71ba8211b9c7c8c060
bert <bert.timmerman@xs4all.nl>
2017-09-23 23:55:43 +0200

    REGION: added dxf_region_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -1075,4 +1075,49 @@ dxf_region_get_visibility
 }
 
 
+/*!
+ * \brief Set the \c visibility for a DXF \c REGION entity.
+ */
+DxfRegion *
+dxf_region_set_visibility
+(
+        DxfRegion *region,
+                /*!< a pointer to a DXF \c REGION entity. */
+        int16_t visibility
+                /*!< the \c visibility to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (region == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (visibility < 0)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (visibility > 1)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () an out of range value was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        region->visibility = visibility;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (region);
+}
+
+
 /* EOF */
