1ebbf764eaf15faff2cccdb727e92a1ef9e34759
bert <bert.timmerman@xs4all.nl>
2017-09-24 22:27:13 +0200

    REGION: added dxf_region_get_graphics_data_size().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -1281,4 +1281,48 @@ dxf_region_set_paperspace
 }
 
 
+/*!
+ * \brief Get the \c graphics_data_size value from a DXF \c REGION
+ * entity.
+ *
+ * \return \c graphics_data_size value when successful, or
+ * \c EXIT_FAILURE when an error occurred.
+ */
+int
+dxf_region_get_graphics_data_size
+(
+        DxfRegion *region
+                /*!< a pointer to a DXF \c REGION entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (region == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (region->graphics_data_size < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (region->graphics_data_size == 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a zero value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (region->graphics_data_size);
+}
+
+
 /* EOF */
--- src/region.h
+++ src/region.h
@@ -228,6 +228,7 @@ int dxf_region_get_color (DxfRegion *region);
 DxfRegion *dxf_region_set_color (DxfRegion *region, int color);
 int dxf_region_get_paperspace (DxfRegion *region);
 DxfRegion *dxf_region_set_paperspace (DxfRegion *region, int paperspace);
+int dxf_region_get_graphics_data_size (DxfRegion *region);
 
 
 #ifdef __cplusplus
