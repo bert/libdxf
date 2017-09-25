94d3a351bdc942fc8ef423ffc67a8e722fbab157
bert <bert.timmerman@xs4all.nl>
2017-09-25 23:14:09 +0200

    REGION: added dxf_region_get_color_value().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -1933,4 +1933,35 @@ dxf_region_set_plot_style_name
 }
 
 
+/*!
+ * \brief Get the \c color_value from a DXF \c REGION entity.
+ *
+ * \return \c color_value when successful, or \c EXIT_FAILURE when an
+ * error occurred.
+ */
+long
+dxf_region_get_color_value
+(
+        DxfRegion *region
+                /*!< a pointer to a DXF \c REGION entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (region == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (region->color_value);
+}
+
+
 /* EOF */
--- src/region.h
+++ src/region.h
@@ -244,6 +244,7 @@ int16_t dxf_region_get_lineweight (DxfRegion *region);
 DxfRegion *dxf_region_set_lineweight (DxfRegion *region, int16_t lineweight);
 char *dxf_region_get_plot_style_name (DxfRegion *region);
 DxfRegion *dxf_region_set_plot_style_name (DxfRegion *region, char *plot_style_name);
+long dxf_region_get_color_value (DxfRegion *region);
 
 
 #ifdef __cplusplus
