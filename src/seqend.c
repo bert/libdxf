6486e93b44743e6a12364d9ae8ef3ab099678dff
bert <bert.timmerman@xs4all.nl>
2017-09-28 07:48:03 +0200

    SEQEND: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/seqend.c
+++ src/seqend.c
@@ -1,7 +1,10 @@
 /*!
  * \file seqend.c
  *
- * \author Copyright (C) 2008 ... 2015, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2010, 2013, 2014, 2015, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
  * \brief Functions for a DXF end of sequence marker (\c SEQEND).
  *
