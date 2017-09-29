df0d564549cec1402ff719751e8bb619d538d712
bert <bert.timmerman@xs4all.nl>
2017-09-30 01:09:30 +0200

    SEQEND: added dxf_seqend_get_lineweight().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/seqend.c
+++ src/seqend.c
@@ -1687,4 +1687,35 @@ dxf_seqend_set_dictionary_owner_hard
 }
 
 
+/*!
+ * \brief Get the \c lineweight from a DXF \c SEQEND entity.
+ *
+ * \return \c lineweight when successful, or \c EXIT_FAILURE when an
+ * error occurred.
+ */
+int16_t
+dxf_seqend_get_lineweight
+(
+        DxfSeqend *seqend
+                /*!< a pointer to a DXF \c SEQEND entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (seqend == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (seqend->lineweight);
+}
+
+
 /* EOF */
--- src/seqend.h
+++ src/seqend.h
@@ -231,6 +231,7 @@ char *dxf_seqend_get_material (DxfSeqend *seqend);
 DxfSeqend *dxf_seqend_set_material (DxfSeqend *seqend, char *material);
 char *dxf_seqend_get_dictionary_owner_hard (DxfSeqend *seqend);
 DxfSeqend *dxf_seqend_set_dictionary_owner_hard (DxfSeqend *seqend, char *dictionary_owner_hard);
+int16_t dxf_seqend_get_lineweight (DxfSeqend *seqend);
 
 
 #ifdef __cplusplus
