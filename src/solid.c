e88d4bbdce0b9af972412c394dd5d07980c081c5
bert <bert.timmerman@xs4all.nl>
2017-10-21 23:34:08 +0200

    SOLID: added dxf_solid_set_material().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.c
+++ src/solid.c
@@ -1922,4 +1922,46 @@ dxf_solid_get_material
 }
 
 
+/*!
+ * \brief Set the pointer to the \c material for a DXF \c SOLID entity.
+ *
+ * \return a pointer to \c solid when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfSolid *
+dxf_solid_set_material
+(
+        DxfSolid *solid,
+                /*!< a pointer to a DXF \c SOLID entity. */
+        char *material
+                /*!< a string containing the pointer to the \c
+                 * material for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (solid == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (material == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        solid->material = strdup (material);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (solid);
+}
+
+
 /* EOF */
