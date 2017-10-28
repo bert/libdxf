cfc4431bdd174421e55c6b5ed516bcc65faa8f76
bert <bert.timmerman@xs4all.nl>
2017-10-28 08:10:10 +0200

    SOLID: added dxf_solid_get_extrusion_vector_as_point().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.c
+++ src/solid.c
@@ -3694,6 +3694,52 @@ dxf_solid_set_z3
 
 
 /*!
+ * \brief Get the extrusion vector as a DXF \c POINT entity from a DXF
+ * \c SOLID entity.
+ *
+ * \return a DXF \c POINT containing the extrusion coordinates.
+ *
+ * \warning No other members are copied into the DXF \c POINT.
+ */
+DxfPoint *
+dxf_solid_get_extrusion_vector_as_point
+(
+        DxfSolid *solid
+                /*!< a pointer to a DXF \c SOLID entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        DxfPoint *point = NULL;
+
+        /* Do some basic checks. */
+        if (solid == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        point = dxf_point_init (point);
+        if (point == NULL)
+        {
+              fprintf (stderr,
+                  (_("Error in %s () could not allocate memory.\n")),
+                __FUNCTION__);
+              return (NULL);
+        }
+        point->x0 = solid->extr_x0;
+        point->y0 = solid->extr_y0;
+        point->z0 = solid->extr_z0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (point);
+}
+
+
+/*!
  * \brief Get the X-value of the extrusion vector \c extr_x0 of a DXF
  * \c SOLID entity.
  *
--- src/solid.h
+++ src/solid.h
@@ -339,6 +339,7 @@ double dxf_solid_get_y3 (DxfSolid *solid);
 DxfSolid *dxf_solid_set_y3 (DxfSolid *solid, double y3);
 double dxf_solid_get_z3 (DxfSolid *solid);
 DxfSolid *dxf_solid_set_z3 (DxfSolid *solid, double z3);
+DxfPoint *dxf_solid_get_extrusion_vector_as_point (DxfSolid *solid);
 double dxf_solid_get_extr_x0 (DxfSolid *solid);
 DxfSolid *dxf_solid_set_extr_x0 (DxfSolid *solid, double extr_x0);
 double dxf_solid_get_extr_y0 (DxfSolid *solid);
