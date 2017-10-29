8d1493fa746e831b3500cf2e0cb06ea57427003d
bert <bert.timmerman@xs4all.nl>
2017-10-29 09:54:32 +0100

    SPATIAL_FILTER: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_filter.h
+++ src/spatial_filter.h
@@ -1,7 +1,7 @@
 /*!
  * \file spatial_filter.h
  *
- * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF spatial_filter object (\c SPATIAL_FILTER).
  *
