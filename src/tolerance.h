62b2455088d896042efce7a474dd0c38fa784395
bert <bert.timmerman@xs4all.nl>
2017-10-29 11:38:46 +0100

    TOLERANCE: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/tolerance.h
+++ src/tolerance.h
@@ -1,7 +1,7 @@
 /*!
  * \file tolerance.h
  *
- * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF tolerance entity (\c TOLERANCE).
  *
