33bbca8f7eb97da5fd921f06e143ec28f048fa59
bert <bert.timmerman@xs4all.nl>
2017-10-29 09:57:39 +0100

    SPATIAL_INDEX: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_index.h
+++ src/spatial_index.h
@@ -1,7 +1,7 @@
 /*!
  * \file spatial_index.h
  *
- * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF spatial_index object (\c SPATIAL_INDEX).
  *
