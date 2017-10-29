82ec7296beee55d32eefae279341a6b0cab0bf57
bert <bert.timmerman@xs4all.nl>
2017-10-29 10:02:18 +0100

    SPLINE: better copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -1,7 +1,8 @@
 /*!
  * \file spline.c
  * 
- * \author Copyright (C) 2013 ... 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2013, 2014, 2015, 2016, 2017
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  * 
  * \brief Functions for a DXF spline entity (\c SPLINE).
  *
