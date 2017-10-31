74f7510c33ea9f8bf953ad57e920866de49ab2f3
bert <bert.timmerman@xs4all.nl>
2017-10-31 21:15:43 +0100

    SPATIAL_FILTER: added dxf_spatial_filter_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_filter.c
+++ src/spatial_filter.c
@@ -621,4 +621,40 @@ dxf_spatial_filter_free_chain
 }
 
 
+/*!
+ * \brief Get the \c id_code from a DXF \c SPATIAL_FILTER object.
+ *
+ * \return \c id_code.
+ */
+int
+dxf_spatial_filter_get_id_code
+(
+        DxfSpatialFilter *spatial_filter
+                /*!< a pointer to a DXF \c SPATIAL_FILTER object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spatial_filter == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (spatial_filter->id_code < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spatial_filter->id_code);
+}
+
+
 /* EOF*/
--- src/spatial_filter.h
+++ src/spatial_filter.h
@@ -168,6 +168,7 @@ DxfSpatialFilter *dxf_spatial_filter_read (DxfFile *fp, DxfSpatialFilter *spatia
 int dxf_spatial_filter_write (DxfFile *fp, DxfSpatialFilter *spatial_filter);
 int dxf_spatial_filter_free (DxfSpatialFilter *spatial_filter);
 void dxf_spatial_filter_free_chain (DxfSpatialFilter *spatial_filters);
+int dxf_spatial_filter_get_id_code (DxfSpatialFilter *spatial_filter);
 
 
 #ifdef __cplusplus
