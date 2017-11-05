c67c3421638a1ae6ca45fce32d8952e24cd2bc72
bert <bert.timmerman@xs4all.nl>
2017-11-05 21:07:58 +0100

    SPATIAL_FILTER: added dxf_spatial_filter_set_x1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_filter.c
+++ src/spatial_filter.c
@@ -1306,4 +1306,49 @@ dxf_spatial_filter_get_x1
 }
 
 
+/*!
+ * \brief Set the X-value of the origin used to define the local
+ * coordinate system of the clip boundary \c x1 of a DXF
+ * \c SPATIAL_FILTER object.
+ *
+ * \return a pointer to \c spatial_filter when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfSpatialFilter *
+dxf_spatial_filter_set_x1
+(
+        DxfSpatialFilter *spatial_filter,
+                /*!< a pointer to a DXF \c SPATIAL_FILTER object. */
+        double x1
+                /*!< the X-value of the origin used to define the local
+                 * coordinate system of the clip boundary \c x0 of a DXF
+                 * \c SPATIAL_FILTER object. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spatial_filter == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (spatial_filter->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        spatial_filter->p1->x0 = x1;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spatial_filter);
+}
+
+
 /* EOF*/
--- src/spatial_filter.h
+++ src/spatial_filter.h
@@ -189,6 +189,7 @@ DxfPoint *dxf_spatial_filter_set_p0_next (DxfPoint *p0, DxfPoint *next);
 DxfPoint *dxf_spatial_filter_get_p1 (DxfSpatialFilter *spatial_filter);
 DxfSpatialFilter *dxf_spatial_filter_set_p1 (DxfSpatialFilter *spatial_filter, DxfPoint *p1);
 double dxf_spatial_filter_get_x1 (DxfSpatialFilter *spatial_filter);
+DxfSpatialFilter *dxf_spatial_filter_set_x1 (DxfSpatialFilter *spatial_filter, double x1);
 
 
 #ifdef __cplusplus
