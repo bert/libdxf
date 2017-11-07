c8cad8fac57694403e043ade8e4d0850edb900ba
bert <bert.timmerman@xs4all.nl>
2017-11-07 21:42:24 +0100

    SPATIAL_FILTER: added dxf_spatial_filter_get_back_clipping_plane_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_filter.c
+++ src/spatial_filter.c
@@ -1931,4 +1931,47 @@ dxf_spatial_filter_set_front_clipping_plane_flag
 }
 
 
+/*!
+ * \brief Get the \c back_clipping_plane_flag from a DXF
+ * \c SPATIAL_FILTER object.
+ *
+ * \return \c back_clipping_plane_flag.
+ */
+int
+dxf_spatial_filter_get_back_clipping_plane_flag
+(
+        DxfSpatialFilter *spatial_filter
+                /*!< a pointer to a DXF \c SPATIAL_FILTER object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spatial_filter == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (spatial_filter->back_clipping_plane_flag < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (spatial_filter->back_clipping_plane_flag > 1)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spatial_filter->back_clipping_plane_flag);
+}
+
+
 /* EOF*/
--- src/spatial_filter.h
+++ src/spatial_filter.h
@@ -204,6 +204,7 @@ int dxf_spatial_filter_get_clip_boundary_display_flag (DxfSpatialFilter *spatial
 DxfSpatialFilter *dxf_spatial_filter_set_clip_boundary_display_flag (DxfSpatialFilter *spatial_filter, int clip_boundary_display_flag);
 int dxf_spatial_filter_get_front_clipping_plane_flag (DxfSpatialFilter *spatial_filter);
 DxfSpatialFilter *dxf_spatial_filter_set_front_clipping_plane_flag (DxfSpatialFilter *spatial_filter, int front_clipping_plane_flag);
+int dxf_spatial_filter_get_back_clipping_plane_flag (DxfSpatialFilter *spatial_filter);
 
 
 #ifdef __cplusplus
