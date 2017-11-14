800a2190892360c12b86423b2428c384cacd871c
bert <bert.timmerman@xs4all.nl>
2017-11-14 19:13:59 +0100

    SPLINE: added dxf_spline_get_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -2901,6 +2901,45 @@ dxf_spline_set_y0
 
 
 /*!
+ * \brief Get the Z-value \c z0 of the first control point \c p0 of a
+ * DXF \c SPLINE entity.
+ *
+ * \return the Z-value \c z0 of the first control point \c p0.
+ */
+double
+dxf_spline_get_z0
+(
+        DxfSpline *spline
+                /*!< a pointer to a DXF \c ARC entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (spline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (spline->p0 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spline->p0->z0);
+}
+
+
+/*!
  * \brief Get the first fit point \c p1 from a DXF \c SPLINE entity.
  *
  * \c p1 is the first fit point in a single linked list.
