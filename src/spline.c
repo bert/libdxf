1b94c5606e5c533f57ad6193e5ba440b50d56876
bert <bert.timmerman@xs4all.nl>
2017-11-14 07:12:18 +0100

    SPLINE: added dxf_spline_set_x0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -2776,6 +2776,49 @@ dxf_spline_get_x0
 
 
 /*!
+ * \brief Set the X-value \c x0 of the first control point \c p0 of a
+ * DXF \c SPLINE entity.
+ *
+ * \return a pointer to \c spline when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfSpline *
+dxf_spline_set_x0
+(
+        DxfSpline *spline,
+                /*!< a pointer to a DXF \c SPLINE entity. */
+        double x0
+                /*!< the X-value \c x0 of the first control point \c p0
+                 * to be set for a DXF \c SPLINE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (spline->p0 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        spline->p0->x0 = x0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spline);
+}
+
+
+/*!
  * \brief Get the first fit point \c p1 from a DXF \c SPLINE entity.
  *
  * \c p1 is the first fit point in a single linked list.
--- src/spline.h
+++ src/spline.h
@@ -290,6 +290,7 @@ DxfSpline *dxf_spline_set_transparency (DxfSpline *spline, long transparency);
 DxfPoint *dxf_spline_get_p0 (DxfSpline *spline);
 DxfSpline *dxf_spline_set_p0 (DxfSpline *spline, DxfPoint *p0);
 double dxf_spline_get_x0 (DxfSpline *spline);
+DxfSpline *dxf_spline_set_x0 (DxfSpline *spline, double x0);
 DxfPoint *dxf_spline_get_p1 (DxfSpline *spline);
 DxfSpline *dxf_spline_set_p1 (DxfSpline *spline, DxfPoint *p1);
 
