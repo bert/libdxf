35df1b451d89a9861d7d93150b69627f294a03b2
bert <bert.timmerman@xs4all.nl>
2017-11-16 22:20:04 +0100

    SPLINE: added dxf_spline_set_p3().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -3675,4 +3675,45 @@ dxf_spline_get_p3
 }
 
 
+/*!
+ * \brief Set the end tangent point \c p3 for a DXF \c SPLINE entity.
+ *
+ * \return a pointer to \c spline when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfSpline *
+dxf_spline_set_p3
+(
+        DxfSpline *spline,
+                /*!< a pointer to a DXF \c SPLINE entity. */
+        DxfPoint *p3
+                /*!< a end tangent point \c p3 for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (p3 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        spline->p3 = (DxfPoint *) p3;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spline);
+}
+
+
 /* EOF */
--- src/spline.h
+++ src/spline.h
@@ -312,6 +312,7 @@ DxfSpline *dxf_spline_set_y2 (DxfSpline *spline, double y2);
 double dxf_spline_get_z2 (DxfSpline *spline);
 DxfSpline *dxf_spline_set_z2 (DxfSpline *spline, double z2);
 DxfPoint *dxf_spline_get_p3 (DxfSpline *spline);
+DxfSpline *dxf_spline_set_p3 (DxfSpline *spline, DxfPoint *p3);
 
 
 #ifdef __cplusplus
