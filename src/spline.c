122c193596cc7ee9a305b5075398a62c1ba436ab
bert <bert.timmerman@xs4all.nl>
2017-11-21 19:58:39 +0100

    SPLINE: added dxf_spline_set_extrusion_vector_from_point().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -4610,6 +4610,47 @@ dxf_spline_get_extrusion_vector_as_point
 
 
 /*!
+ * \brief Set the extrusion vector from a DXF \c POINT for a DXF
+ * \c SPLINE entity.
+ */
+DxfSpline *
+dxf_spline_set_extrusion_vector_from_point
+(
+        DxfSpline *spline,
+                /*!< a pointer to a DXF \c SPLINE entity. */
+        DxfPoint *point
+                /*!< a pointer to a DXF \c POINT entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (spline == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (point == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        spline->extr_x0 = (double) point->x0;
+        spline->extr_y0 = (double) point->y0;
+        spline->extr_z0 = (double) point->z0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (spline);
+}
+
+
+/*!
  * \brief Get the X-value of the extrusion vector \c extr_x0 of a DXF
  * \c SPLINE entity.
  *
