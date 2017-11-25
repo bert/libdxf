4830f373bbc5c56b484ef0cb8b60388366587bf6
bert <bert.timmerman@xs4all.nl>
2017-11-25 22:15:10 +0100

    STYLE: added dxf_style_set_oblique_angle().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/style.c
+++ src/style.c
@@ -1028,6 +1028,38 @@ dxf_style_get_oblique_angle
 
 
 /*!
+ * \brief Set the \c oblique_angle for a DXF \c STYLE symbol table
+ * entries.
+ */
+DxfStyle *
+dxf_style_set_oblique_angle
+(
+        DxfStyle *style,
+                /*!< a pointer to a DXF \c STYLE symbol table entries. */
+        double oblique_angle
+                /*!< the \c oblique_angle to be set for the entries. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (style == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        style->oblique_angle = oblique_angle;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (style);
+}
+
+
+/*!
  * \brief Test if a shape file should be loaded.
  *
  * \return \c TRUE when a shape file should be loaded,
