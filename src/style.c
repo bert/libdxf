34316f87ae4f1665def217a1c08afade0873d156
bert <bert.timmerman@xs4all.nl>
2017-11-25 22:18:36 +0100

    STYLE: added dxf_style_get_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/style.c
+++ src/style.c
@@ -1060,6 +1060,49 @@ dxf_style_set_oblique_angle
 
 
 /*!
+ * \brief Get the \c flag from a DXF \c STYLE symbol table entries.
+ *
+ * \return \c flag.
+ */
+int
+dxf_style_get_flag
+(
+        DxfStyle *style
+                /*!< a pointer to a DXF \c STYLE symbol table
+                 * entries. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (style == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (style->flag < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (style->flag > 127)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (style->flag);
+}
+
+
+/*!
  * \brief Test if a shape file should be loaded.
  *
  * \return \c TRUE when a shape file should be loaded,
--- src/style.h
+++ src/style.h
@@ -152,6 +152,7 @@ double dxf_style_get_last_height (DxfStyle *style);
 DxfStyle *dxf_style_set_last_height (DxfStyle *style, double last_height);
 double dxf_style_get_oblique_angle (DxfStyle *style);
 DxfStyle *dxf_style_set_oblique_angle (DxfStyle *style, double oblique_angle);
+int dxf_style_get_flag (DxfStyle *style);
 int dxf_style_is_shape_file (DxfStyle *style);
 int dxf_style_is_text_vertical (DxfStyle *style);
 int dxf_style_is_xreferenced (DxfStyle *style);
