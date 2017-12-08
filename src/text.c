0355322c62dcacb34aebaec3e4fbd3b89a26e79f
bert <bert.timmerman@xs4all.nl>
2017-12-08 23:18:19 +0100

    TEXT: added dxf_text_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.c
+++ src/text.c
@@ -769,4 +769,43 @@ dxf_text_get_id_code
 }
 
 
+/*!
+ * \brief Set the \c id_code for a DXF \c TEXT entity.
+ */
+DxfText *
+dxf_text_set_id_code
+(
+        DxfText *text,
+                /*!< a pointer to a DXF \c TEXT entity. */
+        int id_code
+                /*!< the \c id_code to be set for the entity.\n
+                 * This is to be an unique (sequential) number in the
+                 * DXF file. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (text == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (id_code < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        text->id_code = id_code;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (text);
+}
+
+
 /* EOF */
