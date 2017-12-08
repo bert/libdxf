07fd1d0474b4f654fdb124197a7ce75f5a9d4e06
bert <bert.timmerman@xs4all.nl>
2017-12-08 23:26:11 +0100

    TEXT: added dxf_text_get_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.c
+++ src/text.c
@@ -808,4 +808,41 @@ dxf_text_set_id_code
 }
 
 
+/*!
+ * \brief Get the \c linetype from a DXF \c TEXT entity.
+ *
+ * \return \c linetype when sucessful, \c NULL when an error occurred.
+ */
+char *
+dxf_text_get_linetype
+(
+        DxfText *text
+                /*!< a pointer to a DXF \c TEXT entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (text == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (text->linetype ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (text->linetype));
+}
+
+
 /* EOF */
--- src/text.h
+++ src/text.h
@@ -306,6 +306,7 @@ int dxf_text_free (DxfText *text);
 void dxf_text_free_chain (DxfText *texts);
 int dxf_text_get_id_code (DxfText *text);
 DxfText *dxf_text_set_id_code (DxfText *text, int id_code);
+char *dxf_text_get_linetype (DxfText *text);
 
 
 #ifdef __cplusplus
