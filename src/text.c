9fe66b8834b6b96ce6b9faff25198bab3770d371
bert <bert.timmerman@xs4all.nl>
2017-12-13 22:15:44 +0100

    TEXT: added dxf_text_get_color_value().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.c
+++ src/text.c
@@ -2057,4 +2057,35 @@ dxf_text_set_plot_style_name
 }
 
 
+/*!
+ * \brief Get the \c color_value from a DXF \c TEXT entity.
+ *
+ * \return \c color_value when successful, or \c EXIT_FAILURE when an
+ * error occurred.
+ */
+long
+dxf_text_get_color_value
+(
+        DxfText *text
+                /*!< a pointer to a DXF \c TEXT entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (text == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (text->color_value);
+}
+
+
 /* EOF */
