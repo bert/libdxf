f67925b844f5cd3f69c8ef5bc0801acb3bbd3c48
bert <bert.timmerman@xs4all.nl>
2017-12-16 18:18:04 +0100

    TEXT: added dxf_text_get_z1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.h
+++ src/text.h
@@ -362,6 +362,7 @@ double dxf_text_get_x1 (DxfText *text);
 DxfText *dxf_text_set_x1 (DxfText *text, double x1);
 double dxf_text_get_y1 (DxfText *text);
 DxfText *dxf_text_set_y1 (DxfText *text, double y1);
+double dxf_text_get_z1 (DxfText *text);
 
 
 #ifdef __cplusplus
