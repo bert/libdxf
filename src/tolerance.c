2d9147599469444eaec7e8ac41cf54714f5ca113
bert <bert.timmerman@xs4all.nl>
2018-01-03 13:13:08 +0100

    TOLERANCE: added dxf_tolerance_get_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/tolerance.c
+++ src/tolerance.c
@@ -2740,4 +2740,43 @@ dxf_tolerance_set_x1
 }
 
 
+/*!
+ * \brief Get the Y-value of the direction vector \c y1 of a DXF
+ * \c TOLERANCE entity.
+ *
+ * \return the Y-value of the direction vector \c y1.
+ */
+double
+dxf_tolerance_get_y1
+(
+        DxfTolerance *tolerance
+                /*!< a pointer to a DXF \c TOLERANCE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (tolerance == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (tolerance->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (tolerance->p1->y0);
+}
+
+
 /* EOF*/
--- src/tolerance.h
+++ src/tolerance.h
@@ -289,6 +289,7 @@ DxfPoint *dxf_tolerance_get_p1 (DxfTolerance *tolerance);
 DxfTolerance *dxf_tolerance_set_p1 (DxfTolerance *tolerance, DxfPoint *p1);
 double dxf_tolerance_get_x1 (DxfTolerance *tolerance);
 DxfTolerance *dxf_tolerance_set_x1 (DxfTolerance *tolerance, double x1);
+double dxf_tolerance_get_y1 (DxfTolerance *tolerance);
 
 
 #ifdef __cplusplus
