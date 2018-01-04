11cf804f573e16e79ecf7378694b93bc95506a4a
bert <bert.timmerman@xs4all.nl>
2018-01-04 21:04:04 +0100

    TOLERANCE: added dxf_tolerance_set_extr_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/tolerance.c
+++ src/tolerance.c
@@ -3118,4 +3118,40 @@ dxf_tolerance_get_extr_z0
 }
 
 
+/*!
+ * \brief Set the Z-value of the extrusion vector \c extr_z0 of a DXF
+ * \c TOLERANCE entity.
+ *
+ * \return a pointer to \c tolerance when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfTolerance *
+dxf_tolerance_set_extr_z0
+(
+        DxfTolerance *tolerance,
+                /*!< a pointer to a DXF \c TOLERANCE entity. */
+        double extr_z0
+                /*!< the Z-value of the extrusion vector \c extr_z0 of a
+                 * DXF \c TOLERANCE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (tolerance == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        tolerance->extr_z0 = extr_z0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (tolerance);
+}
+
+
 /* EOF*/
--- src/tolerance.h
+++ src/tolerance.h
@@ -299,6 +299,7 @@ DxfTolerance *dxf_tolerance_set_extr_x0 (DxfTolerance *tolerance, double extr_x0
 double dxf_tolerance_get_extr_y0 (DxfTolerance *tolerance);
 DxfTolerance *dxf_tolerance_set_extr_y0 (DxfTolerance *tolerance, double extr_y0);
 double dxf_tolerance_get_extr_z0 (DxfTolerance *tolerance);
+DxfTolerance *dxf_tolerance_set_extr_z0 (DxfTolerance *tolerance, double extr_z0);
 
 
 #ifdef __cplusplus
