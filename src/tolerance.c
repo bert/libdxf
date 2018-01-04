be2a9e260b3097e54f9a2c9d4b884c18b94d5113
bert <bert.timmerman@xs4all.nl>
2018-01-04 20:15:10 +0100

    TOLERANCE: added dxf_tolerance_get_extr_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/tolerance.c
+++ src/tolerance.c
@@ -3018,4 +3018,36 @@ dxf_tolerance_set_extr_x0
 }
 
 
+/*!
+ * \brief Get the Y-value of the extrusion vector \c extr_y0 of a DXF
+ * \c TOLERANCE entity.
+ *
+ * \return the Y-value of the extrusion vector \c extr_y0.
+ */
+double
+dxf_tolerance_get_extr_y0
+(
+        DxfTolerance *tolerance
+                /*!< a pointer to a DXF \c TOLERANCE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (tolerance == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (tolerance->extr_y0);
+}
+
+
 /* EOF*/
