131e637c937edec42c4dd945ebbf3401f57333d5
bert <bert.timmerman@xs4all.nl>
2018-01-15 22:36:20 +0100

    TRACE: added dxf_trace_get_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/trace.c
+++ src/trace.c
@@ -2813,4 +2813,43 @@ dxf_trace_set_x1
 }
 
 
+/*!
+ * \brief Get the Y-value of the alignment point \c y1 of a DXF \c TRACE
+ * entity.
+ *
+ * \return the Y-value of the alignment point \c y1.
+ */
+double
+dxf_trace_get_y1
+(
+        DxfTrace *trace
+                /*!< a pointer to a DXF \c TRACE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (trace == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (trace->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (trace->p1->y0);
+}
+
+
 /* EOF */
--- src/trace.h
+++ src/trace.h
@@ -272,6 +272,7 @@ DxfPoint *dxf_trace_get_p1 (DxfTrace *trace);
 DxfTrace *dxf_trace_set_p1 (DxfTrace *trace, DxfPoint *p1);
 double dxf_trace_get_x1 (DxfTrace *trace);
 DxfTrace *dxf_trace_set_x1 (DxfTrace *trace, double x1);
+double dxf_trace_get_y1 (DxfTrace *trace);
 
 
 #ifdef __cplusplus
