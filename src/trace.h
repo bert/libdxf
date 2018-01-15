ead7eae9dc1232df4354f8d7c7ddbc0201345f63
bert <bert.timmerman@xs4all.nl>
2018-01-15 22:10:28 +0100

    TRACE: added dxf_trace_set_x1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/trace.h
+++ src/trace.h
@@ -271,6 +271,7 @@ DxfTrace *dxf_trace_set_z0 (DxfTrace *trace, double z0);
 DxfPoint *dxf_trace_get_p1 (DxfTrace *trace);
 DxfTrace *dxf_trace_set_p1 (DxfTrace *trace, DxfPoint *p1);
 double dxf_trace_get_x1 (DxfTrace *trace);
+DxfTrace *dxf_trace_set_x1 (DxfTrace *trace, double x1);
 
 
 #ifdef __cplusplus
