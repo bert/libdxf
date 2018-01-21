8a7ac8f7dad596c9156ff782e7e0b4e25b34e924
bert <bert.timmerman@xs4all.nl>
2018-01-21 22:04:51 +0100

    UCS: added dxf_ucs_get_origin_z().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ucs.c
+++ src/ucs.c
@@ -911,4 +911,43 @@ dxf_ucs_set_origin_y
 }
 
 
+/*!
+ * \brief Get the Z-value of the base point \c origin of a DXF \c UCS
+ * symbol table entry.
+ *
+ * \return the Z-value of the base point \c origin.
+ */
+double
+dxf_ucs_get_origin_z
+(
+        DxfUcs *ucs
+                /*!< a pointer to a DXF \c UCS symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (ucs == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (ucs->origin == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ucs->origin->z0);
+}
+
+
 /* EOF */
--- src/ucs.h
+++ src/ucs.h
@@ -206,6 +206,7 @@ double dxf_ucs_get_origin_x (DxfUcs *ucs);
 DxfUcs *dxf_ucs_set_origin_x (DxfUcs *ucs, double x);
 double dxf_ucs_get_origin_y (DxfUcs *ucs);
 DxfUcs *dxf_ucs_set_origin_y (DxfUcs *ucs, double y);
+double dxf_ucs_get_origin_z (DxfUcs *ucs);
 
 
 #ifdef __cplusplus
