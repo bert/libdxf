f5e68297a2170fede387a957ec0da70074c38171
bert <bert.timmerman@xs4all.nl>
2018-01-21 19:38:08 +0100

    UCS: added dxf_ucs_set_origin().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ucs.c
+++ src/ucs.c
@@ -706,4 +706,45 @@ dxf_ucs_get_origin
 }
 
 
+/*!
+ * \brief Set the base point \c origin of a DXF \c UCS symbol table
+ * entry.
+ *
+ * \return a pointer to a DXF \c UCS symbol table entry.
+ */
+DxfUcs *
+dxf_ucs_set_origin
+(
+        DxfUcs *ucs,
+                /*!< a pointer to a DXF \c UCS symbol table entry. */
+        DxfPoint *origin
+                /*!< a pointer to a DXF \c POINT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (ucs == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (origin == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        ucs->origin = origin;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (ucs);
+}
+
+
 /* EOF */
