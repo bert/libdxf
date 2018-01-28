e47dca0c0f78320d6d21892bdcc2b3531c2cbca5
bert <bert.timmerman@xs4all.nl>
2018-01-28 10:25:09 +0100

    Use libtoolize.
    Reported-by: Mathias-Fuchs <https://github.com/Mathias-Fuchs>
    
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- autogen.sh
+++ autogen.sh
@@ -49,9 +49,9 @@ fi
 }
 
 (grep "^AM_PROG_LIBTOOL" $srcdir/configure.ac >/dev/null) && {
-  (libtool --version) < /dev/null > /dev/null 2>&1 || {
+  (libtoolize --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`libtool' installed."
+    echo "**Error**: You must have \`libtoolize' installed."
     echo "You can get it from: ftp://ftp.gnu.org/pub/gnu/"
     DIE=1
   }
