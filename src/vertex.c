687d47c1ea0dc06a383ffefe05e43305627c410d
bert <bert.timmerman@xs4all.nl>
2018-02-09 20:36:12 +0100

    VERTEX: added dxf_vertex_get_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vertex.c
+++ src/vertex.c
@@ -2769,4 +2769,46 @@ dxf_vertex_set_curve_fit_tangent_direction
 }
 
 
+/*!
+ * \brief Get the \c flag from a DXF \c VERTEX entity.
+ *
+ * \return \c flag.
+ */
+int
+dxf_vertex_get_flag
+(
+        DxfVertex *vertex
+                /*!< a pointer to a DXF \c VERTEX entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (vertex == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (vertex->flag < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (vertex->flag > 0x11111111)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (vertex->flag);
+}
+
+
 /* EOF */
--- src/vertex.h
+++ src/vertex.h
@@ -339,6 +339,7 @@ double dxf_vertex_get_bulge (DxfVertex *vertex);
 DxfVertex *dxf_vertex_set_bulge (DxfVertex *vertex, double bulge);
 double dxf_vertex_get_curve_fit_tangent_direction (DxfVertex *vertex);
 DxfVertex *dxf_vertex_set_curve_fit_tangent_direction (DxfVertex *vertex, double curve_fit_tangent_direction);
+int dxf_vertex_get_flag (DxfVertex *vertex);
 
 
 #ifdef __cplusplus
