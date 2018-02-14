2d9d1c4436b5d7dabae26e537978f5f80d6f95a6
bert <bert.timmerman@xs4all.nl>
2018-02-14 23:23:52 +0100

    VIEW: added dxf_view_set_view_center_x0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/view.c
+++ src/view.c
@@ -836,4 +836,48 @@ dxf_view_get_view_center_x0
 }
 
 
+/*!
+ * \brief Set the X-value of the view center point \c view_center_x0 of
+ * a DXF \c VIEW symbol table entry.
+ *
+ * \return a pointer to \c view when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfView *
+dxf_view_set_view_center_x0
+(
+        DxfView *view,
+                /*!< a pointer to a DXF \c VIEW symbol table entry. */
+        double view_center_x0
+                /*!< the X-value of the view center point
+                 * \c view_center_x0 of a DXF \c VIEW symbol table
+                 * entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (view == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (view->view_center == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        view->view_center->x0 = view_center_x0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (view);
+}
+
+
 /* EOF */
