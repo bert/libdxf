777fa89b93ae1f5ad3338a994a41b7f6680a44f6
bert <bert.timmerman@xs4all.nl>
2018-02-17 22:27:11 +0100

    VIEW: added dxf_view_get_x1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/view.c
+++ src/view.c
@@ -1040,4 +1040,43 @@ dxf_view_set_p1
 }
 
 
+/*!
+ * \brief Get the X-value of the view direction from target \c x1 of a
+ * DXF \c VIEW symbol table entry.
+ *
+ * \return the X-value of the view direction from target \c x1.
+ */
+double
+dxf_view_get_x1
+(
+        DxfView *view
+                /*!< a pointer to a DXF \c VIEW symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (view == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (view->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (view->p1->x0);
+}
+
+
 /* EOF */
