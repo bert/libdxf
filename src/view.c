a53cc7ece84976387dacaa88747c7e9813ee765b
bert <bert.timmerman@xs4all.nl>
2018-02-20 07:54:50 +0100

    VIEW: added dxf_view_set_y2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/view.c
+++ src/view.c
@@ -1486,4 +1486,47 @@ dxf_view_get_y2
 }
 
 
+/*!
+ * \brief Set the Y-value of the view target point \c y2 of a DXF
+ * \c VIEW symbol table entry.
+ *
+ * \return a pointer to \c view when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfView *
+dxf_view_set_y2
+(
+        DxfView *view,
+                /*!< a pointer to a DXF \c VIEW symbol table entry. */
+        double y2
+                /*!< the Y-value of the view target point \c y2 of a DXF
+                 * \c VIEW symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (view == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (view->p2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        view->p2->y0 = y2;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (view);
+}
+
+
 /* EOF */
