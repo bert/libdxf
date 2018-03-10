7a5ffa9be3a99f036a1a1d56b1d2c2cfc7bba5f9
bert <bert.timmerman@xs4all.nl>
2018-03-10 23:50:11 +0100

    VIEWPORT: added dxf_viewport_set_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.c
+++ src/viewport.c
@@ -3892,6 +3892,49 @@ dxf_viewport_get_y1
 
 
 /*!
+ * \brief Set the Y-value of the target point \c y1 of a DXF \c VIEWPORT
+ * entity.
+ *
+ * \return a pointer to \c viewport when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfViewport *
+dxf_viewport_set_y1
+(
+        DxfViewport *viewport,
+                /*!< a pointer to a DXF \c VIEWPORT entity. */
+        double y1
+                /*!< the Y-value of the target point \c y1 of a DXF
+                 * \c VIEWPORT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (viewport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (viewport->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        viewport->p1->y0 = y1;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (viewport);
+}
+
+
+/*!
  * \brief Get the \c perspective_lens_length from a DXF \c VIEWPORT
  * entity.
  *
