3d916dc5619cb0946858722f7cfc216ad9b44d90
bert <bert.timmerman@xs4all.nl>
2018-03-12 19:01:33 +0100

    VIEWPORT: added dxf_viewport_get_x2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.c
+++ src/viewport.c
@@ -4102,6 +4102,45 @@ dxf_viewport_set_p2
 
 
 /*!
+ * \brief Get the X-value of the direction from target point \c x2 of a
+ * DXF \c VIEWPORT entity.
+ *
+ * \return the X-value of the direction from target point \c x2.
+ */
+double
+dxf_viewport_get_x2
+(
+        DxfViewport *viewport
+                /*!< a pointer to a DXF \c VIEWPORT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (viewport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (viewport->p2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (viewport->p2->x0);
+}
+
+
+/*!
  * \brief Get the \c perspective_lens_length from a DXF \c VIEWPORT
  * entity.
  *
