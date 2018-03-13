93d802f2c33a2f71e391733a93092c737dc3901c
bert <bert.timmerman@xs4all.nl>
2018-03-13 20:36:55 +0100

    VIEWPORT: added dxf_viewport_set_z2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.c
+++ src/viewport.c
@@ -4305,6 +4305,49 @@ dxf_viewport_get_z2
 
 
 /*!
+ * \brief Set the Z-value of the direction from target point \c z2 of a
+ * DXF \c VIEWPORT entity.
+ *
+ * \return a pointer to \c viewport when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfViewport *
+dxf_viewport_set_z2
+(
+        DxfViewport *viewport,
+                /*!< a pointer to a DXF \c VIEWPORT entity. */
+        double z2
+                /*!< the Z-value of the direction from target point
+                 * \c z2 of a DXF \c VIEWPORT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (viewport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (viewport->p2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        viewport->p2->z0 = z2;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (viewport);
+}
+
+
+/*!
  * \brief Get the \c perspective_lens_length from a DXF \c VIEWPORT
  * entity.
  *
