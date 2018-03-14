651b78e376544ab749d74ec1cc3fb8631150b768
bert <bert.timmerman@xs4all.nl>
2018-03-14 21:40:27 +0100

    VIEWPORT: added dxf_viewport_get_x3().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.c
+++ src/viewport.c
@@ -4551,6 +4551,45 @@ dxf_viewport_set_p3
 
 
 /*!
+ * \brief Get the X-value of the view center point \c x3 of a DXF
+ * \c VIEWPORT entity.
+ *
+ * \return the X-value of the view center point \c x3.
+ */
+double
+dxf_viewport_get_x3
+(
+        DxfViewport *viewport
+                /*!< a pointer to a DXF \c VIEWPORT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (viewport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (viewport->p3 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (viewport->p3->x0);
+}
+
+
+/*!
  * \brief Get the \c perspective_lens_length from a DXF \c VIEWPORT
  * entity.
  *
--- src/viewport.h
+++ src/viewport.h
@@ -445,6 +445,7 @@ double dxf_viewport_get_view_height (DxfViewport *viewport);
 DxfViewport *dxf_viewport_set_view_height (DxfViewport *viewport, double view_height);
 DxfPoint *dxf_viewport_get_p3 (DxfViewport *viewport);
 DxfViewport *dxf_viewport_set_p3 (DxfViewport *viewport, DxfPoint *p3);
+double dxf_viewport_get_x3 (DxfViewport *viewport);
 double dxf_viewport_get_perspective_lens_length (DxfViewport *viewport);
 DxfViewport *dxf_viewport_set_perspective_lens_length (DxfViewport *viewport, double perspective_lens_length);
 
