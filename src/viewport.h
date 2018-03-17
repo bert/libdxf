3c26c4e2f003f1f2f9df18e87e9544378f4f190b
bert <bert.timmerman@xs4all.nl>
2018-03-17 21:49:33 +0100

    VIEWPORT: added dxf_viewport_get_UCSICON_setting().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/viewport.h
+++ src/viewport.h
@@ -455,6 +455,7 @@ int dxf_viewport_get_circle_zoom_percent (DxfViewport *viewport);
 DxfViewport *dxf_viewport_set_circle_zoom_percent (DxfViewport *viewport, int circle_zoom_percent);
 int dxf_viewport_get_fast_zoom_setting (DxfViewport *viewport);
 DxfViewport *dxf_viewport_set_fast_zoom_setting (DxfViewport *viewport, int fast_zoom_setting);
+int dxf_viewport_get_UCSICON_setting (DxfViewport *viewport);
 
 
 #ifdef __cplusplus
