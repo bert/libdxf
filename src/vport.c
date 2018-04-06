a92914d661f43938caa581078f89bb0a15b5649c
bert <bert.timmerman@xs4all.nl>
2018-04-06 23:02:45 +0200

    VPORT: added dxf_vport_get_max_x().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vport.c
+++ src/vport.c
@@ -1209,4 +1209,43 @@ dxf_vport_set_max
 }
 
 
+/*!
+ * \brief Get the X-value of the upper-right corner \c max_x of a DXF
+ * \c VPORT symbol table entry.
+ *
+ * \return the X-value of the upper-right corner \c max_x.
+ */
+double
+dxf_vport_get_max_x
+(
+        DxfVPort *vport
+                /*!< a pointer to a DXF \c VPORT symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (vport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (vport->max == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (vport->max->x0);
+}
+
+
 /* EOF*/
--- src/vport.h
+++ src/vport.h
@@ -234,6 +234,7 @@ double dxf_vport_get_min_y (DxfVPort *vport);
 DxfVPort *dxf_vport_set_min_y (DxfVPort *vport, double min_y);
 DxfPoint *dxf_vport_get_max (DxfVPort *vport);
 DxfVPort *dxf_vport_set_max (DxfVPort *vport, DxfPoint *max);
+double dxf_vport_get_max_x (DxfVPort *vport);
 
 
 #ifdef __cplusplus
