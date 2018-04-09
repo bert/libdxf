0f6a4cddaf2843344df823d7124cdc871cf2dd1b
bert <bert.timmerman@xs4all.nl>
2018-04-09 23:00:34 +0200

    VPORT: test for a valid pointer to min in dxf_vport_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vport.c
+++ src/vport.c
@@ -212,6 +212,14 @@ dxf_vport_read
                 vport = dxf_vport_new ();
                 vport = dxf_vport_init (vport);
         }
+        if (vport->min == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                vport->min = dxf_point_new ();
+                vport->min = dxf_point_init (vport->min);
+        }
         (fp->line_number)++;
         fscanf (fp->fp, "%[^\n]", temp_string);
         while (strcmp (temp_string, "0") != 0)
