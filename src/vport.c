c6d437dab2dfc7d06fd60d3ec57b77bfd06802ab
bert <bert.timmerman@xs4all.nl>
2018-04-13 23:07:29 +0200

    VPORT: added dxf_vport_get_snap_spacing_y().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vport.c
+++ src/vport.c
@@ -2104,4 +2104,43 @@ dxf_vport_set_snap_spacing_x
 }
 
 
+/*!
+ * \brief Get the Y-value of the snap spacing \c snap_spacing_y of a DXF
+ * \c VPORT symbol table entry.
+ *
+ * \return the Y-value of the snap spacing \c snap_spacing_y.
+ */
+double
+dxf_vport_get_snap_spacing_y
+(
+        DxfVPort *vport
+                /*!< a pointer to a DXF \c VPORT symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (vport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (vport->snap_spacing == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (vport->snap_spacing->y0);
+}
+
+
 /* EOF*/
--- src/vport.h
+++ src/vport.h
@@ -237,6 +237,7 @@ DxfPoint *dxf_vport_get_snap_spacing (DxfVPort *vport);
 DxfVPort *dxf_vport_set_snap_spacing (DxfVPort *vport, DxfPoint *snap_spacing);
 double dxf_vport_get_snap_spacing_x (DxfVPort *vport);
 DxfVPort *dxf_vport_set_snap_spacing_x (DxfVPort *vport, double snap_spacing_x);
+double dxf_vport_get_snap_spacing_y (DxfVPort *vport);
 
 
 #ifdef __cplusplus
