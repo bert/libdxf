f7f64d5700495114eb7d92f0b7060cb712d0ddad
bert <bert.timmerman@xs4all.nl>
2018-04-18 22:22:01 +0200

    VPORT: added dxf_vport_get_target_y().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vport.c
+++ src/vport.c
@@ -2976,4 +2976,43 @@ dxf_vport_set_target_x
 }
 
 
+/*!
+ * \brief Get the Y-value of the target point \c target_y of a DXF
+ * \c VPORT symbol table entry.
+ *
+ * \return the Y-value of the target point \c target_y.
+ */
+double
+dxf_vport_get_target_y
+(
+        DxfVPort *vport
+                /*!< a pointer to a DXF \c VPORT symbol table entry. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (vport == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (vport->target == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (vport->target->y0);
+}
+
+
 /* EOF*/
--- src/vport.h
+++ src/vport.h
@@ -245,6 +245,7 @@ DxfPoint *dxf_vport_get_target (DxfVPort *vport);
 DxfVPort *dxf_vport_set_target (DxfVPort *vport, DxfPoint *target);
 double dxf_vport_get_target_x (DxfVPort *vport);
 DxfVPort *dxf_vport_set_target_x (DxfVPort *vport, double target_x);
+double dxf_vport_get_target_y (DxfVPort *vport);
 
 
 #ifdef __cplusplus
