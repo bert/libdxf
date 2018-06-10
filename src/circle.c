b64020daca7c0bf68a6903887bad27532605e49e
bert <bert.timmerman@xs4all.nl>
2018-06-10 19:58:38 +0200

    CIRCLE: updated dxf_circle_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/circle.c
+++ src/circle.c
@@ -174,6 +174,8 @@ dxf_circle_read
         DXF_DEBUG_BEGIN
 #endif
         char *temp_string = NULL;
+        DxfBinaryGraphicsData *iter310 = NULL;
+        int iter330;
 
         /* Do some basic checks. */
         if (fp == NULL)
@@ -193,6 +195,8 @@ dxf_circle_read
                 circle = dxf_circle_new ();
                 circle = dxf_circle_init (circle);
         }
+        iter310 = (DxfBinaryGraphicsData *) circle->binary_graphics_data;
+        iter330 = 0;
         (fp->line_number)++;
         fscanf (fp->fp, "%[^\n]", temp_string);
         while (strcmp (temp_string, "0") != 0)
@@ -299,6 +303,13 @@ dxf_circle_read
                         (fp->line_number)++;
                         fscanf (fp->fp, "%d\n", &circle->paperspace);
                 }
+                else if (strcmp (temp_string, "92") == 0)
+                {
+                        /* Now follows a string containing the
+                         * graphics data size value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%d\n", &circle->graphics_data_size);
+                }
                 else if ((fp->acad_version_number >= AutoCAD_13)
                         && (strcmp (temp_string, "100") == 0))
                 {
@@ -313,6 +324,20 @@ dxf_circle_read
                                   (_("Warning in %s () found a bad subclass marker in: %s in line: %d.\n")),
                                   __FUNCTION__, fp->filename, fp->line_number);
                         }
+                }
+                else if (strcmp (temp_string, "160") == 0)
+                {
+                        /* Now follows a string containing the
+                         * graphics data size value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%d\n", &circle->graphics_data_size);
+                }
+                else if (strcmp (temp_string, "284") == 0)
+                {
+                        /* Now follows a string containing the shadow
+                         * mode value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%hd\n", &circle->shadow_mode);
                 }
                 else if (strcmp (temp_string, "210") == 0)
                 {
@@ -335,12 +360,39 @@ dxf_circle_read
                         (fp->line_number)++;
                         fscanf (fp->fp, "%lf\n", &circle->extr_z0);
                 }
+                else if (strcmp (temp_string, "310") == 0)
+                {
+                        /* Now follows a string containing binary
+                         * graphics data. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%s\n", iter310->data_line);
+                        dxf_binary_graphics_data_init ((DxfBinaryGraphicsData *) iter310->next);
+                        iter310 = (DxfBinaryGraphicsData *) iter310->next;
+                }
                 else if (strcmp (temp_string, "330") == 0)
                 {
-                        /* Now follows a string containing Soft-pointer
-                         * ID/handle to owner dictionary. */
+                        if (iter330 == 0)
+                        {
+                                /* Now follows a string containing a soft-pointer
+                                 * ID/handle to owner dictionary. */
+                                (fp->line_number)++;
+                                fscanf (fp->fp, "%s\n", circle->dictionary_owner_soft);
+                        }
+                        if (iter330 == 1)
+                        {
+                                /* Now follows a string containing a soft-pointer
+                                 * ID/handle to owner object. */
+                                (fp->line_number)++;
+                                fscanf (fp->fp, "%s\n", circle->object_owner_soft);
+                        }
+                        iter330++;
+                }
+                else if (strcmp (temp_string, "347") == 0)
+                {
+                        /* Now follows a string containing a
+                         * hard-pointer ID/handle to material object. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->dictionary_owner_soft);
+                        fscanf (fp->fp, "%s\n", circle->material);
                 }
                 else if (strcmp (temp_string, "360") == 0)
                 {
@@ -348,6 +400,40 @@ dxf_circle_read
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
                         fscanf (fp->fp, "%s\n", circle->dictionary_owner_hard);
+                }
+                else if (strcmp (temp_string, "370") == 0)
+                {
+                        /* Now follows a string containing the lineweight
+                         * value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%hd\n", &circle->lineweight);
+                }
+                else if (strcmp (temp_string, "390") == 0)
+                {
+                        /* Now follows a string containing a plot style
+                         * name value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%s\n", circle->plot_style_name);
+                }
+                else if (strcmp (temp_string, "420") == 0)
+                {
+                        /* Now follows a string containing a color value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%ld\n", &circle->color_value);
+                }
+                else if (strcmp (temp_string, "430") == 0)
+                {
+                        /* Now follows a string containing a color
+                         * name value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%s\n", circle->color_name);
+                }
+                else if (strcmp (temp_string, "440") == 0)
+                {
+                        /* Now follows a string containing a transparency
+                         * value. */
+                        (fp->line_number)++;
+                        fscanf (fp->fp, "%ld\n", &circle->transparency);
                 }
                 else if (strcmp (temp_string, "999") == 0)
                 {
