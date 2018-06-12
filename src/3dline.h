fbd21c9f1cfd5bda4b4799801ac1724218131546
bert <bert.timmerman@xs4all.nl>
2018-06-12 23:23:40 +0200

    3DLINE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dline.h
+++ src/3dline.h
@@ -1,7 +1,8 @@
 /*!
  * \file 3dline.h
  *
- * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017, 2018
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF 3D line entity (\c 3DLINE).
  *
