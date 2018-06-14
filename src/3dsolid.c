09ab7979b68e29a6f3dc7a28be1118ced9d853e2
bert <bert.timmerman@xs4all.nl>
2018-06-14 07:31:50 +0200

    3DSOLID: updated dxf_3dsolid_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dsolid.c
+++ src/3dsolid.c
@@ -131,6 +131,7 @@ dxf_3dsolid_init
         solid->shadow_mode = 0;
         solid->binary_graphics_data = (DxfBinaryGraphicsData *) dxf_binary_graphics_data_init (solid->binary_graphics_data);
         solid->dictionary_owner_soft = strdup ("");
+        solid->object_owner_soft = strdup ("");
         solid->material = strdup ("");
         solid->dictionary_owner_hard = strdup ("");
         solid->lineweight = 0;
