ba09f9dc7e20132583815a562dab255a0ca950fd
bert <bert.timmerman@xs4all.nl>
2018-06-20 06:42:19 +0200

    ARC: updated dxf_arc_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -177,6 +177,7 @@ dxf_arc_read
 #endif
         char *temp_string = NULL;
         DxfBinaryGraphicsData *iter310 = NULL;
+        int iter330;
 
         /* Do some basic checks. */
         if (fp == NULL)
@@ -197,6 +198,7 @@ dxf_arc_read
                 arc = dxf_arc_init (arc);
         }
         iter310 = (DxfBinaryGraphicsData *) arc->binary_graphics_data;
+        iter330 = 0;
         (fp->line_number)++;
         fscanf (fp->fp, "%[^\n]", temp_string);
         while (strcmp (temp_string, "0") != 0)
@@ -384,10 +386,21 @@ dxf_arc_read
                 }
                 else if (strcmp (temp_string, "330") == 0)
                 {
-                        /* Now follows a string containing Soft-pointer
-                         * ID/handle to owner dictionary. */
-                        (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", arc->dictionary_owner_soft);
+                        if (iter330 == 0)
+                        {
+                                /* Now follows a string containing a soft-pointer
+                                 * ID/handle to owner dictionary. */
+                                (fp->line_number)++;
+                                fscanf (fp->fp, "%s\n", arc->dictionary_owner_soft);
+                        }
+                        if (iter330 == 1)
+                        {
+                                /* Now follows a string containing a soft-pointer
+                                 * ID/handle to owner object. */
+                                (fp->line_number)++;
+                                fscanf (fp->fp, "%s\n", arc->object_owner_soft);
+                        }
+                        iter330++;
                 }
                 else if (strcmp (temp_string, "347") == 0)
                 {
