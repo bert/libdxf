7797ee4af32af8619f48c9006896f0d998ac3739
bert <bert.timmerman@xs4all.nl>
2018-07-01 21:45:17 +0200

    DIMSTYLE: correction of an error message in dxf_dimstyle_set_dimpost().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimstyle.c
+++ src/dimstyle.c
@@ -1129,7 +1129,7 @@ dxf_dimstyle_set_dimpost
         if (dimpost == NULL)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () the string name contained a NULL pointer.\n")),
+                  (_("Error in %s () a NULL pointer was passed.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
