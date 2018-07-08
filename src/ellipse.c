f6affafb64ec4d96d65e0cffd507ad7e94606c8b
bert <bert.timmerman@xs4all.nl>
2018-07-08 09:49:22 +0200

    ELLIPSE: correction in an error message in dxf_ellipse_get_linetype_scale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -1206,9 +1206,8 @@ dxf_ellipse_get_linetype_scale
         if (ellipse->linetype_scale < 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the linetype scale member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
