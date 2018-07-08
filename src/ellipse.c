6792d5c279bdbff2902cb5e1e7b12d624111747e
bert <bert.timmerman@xs4all.nl>
2018-07-08 11:30:31 +0200

    ELLIPSE: corrections in error messages in dxf_ellipse_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -1322,16 +1322,14 @@ dxf_ellipse_set_visibility
         if (visibility < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative visibility value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         if (visibility > 1)
         {
                 fprintf (stderr,
-                  (_("Error in %s () an out of range visibility value was passed.\n")),
+                  (_("Warning in %s () an out of range value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         ellipse->visibility = visibility;
 #if DEBUG
