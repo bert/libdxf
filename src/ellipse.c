2afbdd752937282519db1196d43fb91a350b51a8
bert <bert.timmerman@xs4all.nl>
2018-07-10 22:18:24 +0200

    ELLIPSE: correction in an error message in dxf_ellipse_get_dictionary_owner_hard().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -1951,7 +1951,7 @@ dxf_ellipse_get_dictionary_owner_hard
         if (ellipse->dictionary_owner_hard ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_hard member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
