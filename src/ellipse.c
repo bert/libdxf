3e91072be6b8a341075f36a1a3722d6ce00a1736
bert <bert.timmerman@xs4all.nl>
2018-07-10 22:22:41 +0200

    ELLIPSE: corrections in error messages in dxf_ellipse_get_ratio().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -3022,16 +3022,14 @@ dxf_ellipse_get_ratio
         if (ellipse->ratio < 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the ratio member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
         if (ellipse->ratio == 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a value of zero was found in the ratio member.\n")),
+                  (_("Warning in %s () a value of zero was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
