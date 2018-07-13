7c0d3644a32f8c5019856b04b4565d9afa544da4
bert <bert.timmerman@xs4all.nl>
2018-07-13 22:19:34 +0200

    ENDBLK: correction in an error message in dxf_endblk_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/endblk.c
+++ src/endblk.c
@@ -391,9 +391,8 @@ dxf_endblk_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         endblk->id_code = id_code;
 #if DEBUG
