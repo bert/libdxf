4ba9fd6c7903e9dd3410224c4933f978d5c8c78f
bert <bert.timmerman@xs4all.nl>
2018-07-15 09:29:08 +0200

    ENDSEC: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/endsec.h
+++ src/endsec.h
@@ -1,7 +1,8 @@
 /*!
  * \file endsec.h
  *
- * \author Copyright (C) 2015 ... 2016 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2018
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF end of section marker (\c ENDSEC).
  *
