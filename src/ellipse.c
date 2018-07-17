07af0e8cefaa218f13a6750063a53580ec5eaf52
bert <bert.timmerman@xs4all.nl>
2018-07-17 23:44:56 +0200

    ELLIPSE: correction in an error message in dxf_ellipse_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -887,7 +887,7 @@ dxf_ellipse_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
         }
         ellipse->id_code = id_code;
