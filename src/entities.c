d9f42f97d652250815fed2913542aa1b54c94305
bert <bert.timmerman@xs4all.nl>
2018-07-17 23:50:24 +0200

    ENTITIES: bumped the copyright years.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/entities.c
+++ src/entities.c
@@ -1,7 +1,8 @@
 /*!
  * \file entities.c
  *
- * \author Copyright (C) 2012 ... 2014 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2012, 2014, 2017, 2018
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF entities section (/c ENTITIES).
  *
