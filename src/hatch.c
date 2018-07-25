3c6b935cbe90dbd9690e34f186ef173c3e9ee541
bert <bert.timmerman@xs4all.nl>
2018-07-25 14:04:15 +0200

    HATCH: added dxf_hatch_get_plot_style_name().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -1708,6 +1708,44 @@ dxf_hatch_set_lineweight
 
 
 /*!
+ * \brief Get the \c plot_style_name from a DXF \c HATCH entity.
+ *
+ * \return a pointer to \c plot_style_name when sucessful, or \c NULL
+ * when an error occurred.
+ */
+char *
+dxf_hatch_get_plot_style_name
+(
+        DxfHatch *hatch
+                /*!< a pointer to a DXF \c HATCH entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (hatch == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (hatch->plot_style_name ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (hatch->plot_style_name));
+}
+
+
+/*!
  * \brief Get the pattern name from a DXF \c HATCH entity.
  *
  * \return pattern name when sucessful, \c NULL when an error occurred.
--- src/hatch.h
+++ src/hatch.h
@@ -682,6 +682,7 @@ char *dxf_hatch_get_dictionary_owner_hard (DxfHatch *hatch);
 DxfHatch *dxf_hatch_set_dictionary_owner_hard (DxfHatch *hatch, char *dictionary_owner_hard);
 int16_t dxf_hatch_get_lineweight (DxfHatch *hatch);
 DxfHatch *dxf_hatch_set_lineweight (DxfHatch *hatch, int16_t lineweight);
+char *dxf_hatch_get_plot_style_name (DxfHatch *hatch);
 char *dxf_hatch_get_pattern_name (DxfHatch *hatch);
 DxfHatch *dxf_hatch_set_pattern_name (DxfHatch *hatch, char *pattern_name);
 double dxf_hatch_get_x0 (DxfHatch *hatch);
