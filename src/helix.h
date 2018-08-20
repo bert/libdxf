cd5a03ebced4e24e84b9b71c68d7ea5531398a81
bert <bert.timmerman@xs4all.nl>
2018-08-20 22:30:46 +0200

    HELIX: updated dxf_helix_struct.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.h
+++ src/helix.h
@@ -74,14 +74,16 @@ dxf_helix_struct
                  * Group code = 39. */
         double linetype_scale;
                 /*!< Linetype scale (optional).\n
-                 * Group code = 48. */
+                 * Group code = 48.\n
+                 * \since Introduced in version R13. */
         int16_t visibility;
                 /*!< Object visibility (optional):\n
                  * <ol>
                  * <li value = "0"> Visible</li>
                  * <li value = "1"> Invisible</li>
                  * </ol>
-                 * Group code = 60. */
+                 * Group code = 60.\n
+                 * \since Introduced in version R13. */
         int color;
                 /*!< Color of the entity.\n
                  * Defaults to \c BYLAYER if ommitted in the DXF file.\n
@@ -93,17 +95,17 @@ dxf_helix_struct
                 /*!< Entities are to be drawn on either \c PAPERSPACE or
                  * \c MODELSPACE.\n
                  * Optional, defaults to \c DXF_MODELSPACE (0).\n
-                 * Group code = 67. */
+                 * Group code = 67.\n
+                 * \since Introduced in version R13. */
         int graphics_data_size;
                 /*!< Number of bytes in the proxy entity graphics
                  * represented in the sub-sequent 310 groups, which are
                  * binary chunk records (optional).\n
-                 * Group code = 92.
-                 *
+                 * Group code = 92.\n
+                 * \since Introduced in version R2000.\n
                  * \warning On some 64 bit workstations output is
                  * generated with group code "160", thus omitting group
-                 * code "92".
-                 */
+                 * code "92". */
         int16_t shadow_mode;
                 /*!< Shadow mode:\n
                  * <ol>
@@ -112,29 +114,40 @@ dxf_helix_struct
                  * <li value = "2"> Receives shadows.</li>
                  * <li value = "3"> Ignores shadows.</li>
                  * </ol>\n
-                 * Group code = 284. */
+                 * Group code = 284.\n
+                 * \since Introduced in version R2009. */
         DxfBinaryGraphicsData *binary_graphics_data;
                 /*!< Proxy entity graphics data.\n
                  * Multiple lines of 256 characters maximum per line
                  * (optional).\n
-                 * Group code = 310. */
+                 * Group code = 310.\n
+                 * \since Introduced in version R2000. */
         char *dictionary_owner_soft;
                 /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
-                 * Group code = 330. */
+                 * Group code = 330.\n
+                 * \since Introduced in version R14. */
+        char *object_owner_soft;
+                /*!< Soft-pointer ID/handle  to owner BLOCK_RECORD object.\n
+                 * Group code = 330.\n
+                 * \since Introduced in version R2000. */
         char *material;
                 /*!< Hard-pointer ID/handle to material object (present if
                  * not BYLAYER).\n
-                 * Group code = 347. */
+                 * Group code = 347.\n
+                 * \since Introduced in version R2008. */
         char *dictionary_owner_hard;
                 /*!< Hard owner ID/handle to owner dictionary (optional).\n
-                 * Group code = 360. */
+                 * Group code = 360.\n
+                 * \since Introduced in version R14. */
         int16_t lineweight;
                 /*!< Lineweight enum value.\n
                  * Stored and moved around as a 16-bit integer.\n
-                 * Group code = 370. */
+                 * Group code = 370.\n
+                 * \since Introduced in version R2002. */
         char *plot_style_name;
                 /*!< Hard pointer ID / handle of PlotStyleName object.\n
-                 * Group code = 390. */
+                 * Group code = 390.\n
+                 * \since Introduced in version R2009. */
         long color_value;
                 /*!< A 24-bit color value that should be dealt with in
                  * terms of bytes with values of 0 to 255.\n
@@ -145,21 +158,24 @@ dxf_helix_struct
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 420. */
+                 * Group code = 420.\n
+                 * \since Introduced in version R2004. */
         char *color_name;
                 /*!< Color name.\n
                  * The group code cannot be used by custom entities for
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 430. */
+                 * Group code = 430.\n
+                 * \since Introduced in version R2004. */
         long transparency;
                 /*!< Transparency value.\n
                  * The group code cannot be used by custom entities for
                  * their own data because the group code is reserved for
                  * AcDbEntity, class-level color data and AcDbEntity,
                  * class-level transparency data.\n
-                 * Group code = 440. */
+                 * Group code = 440.\n
+                 * \since Introduced in version R2004. */
         /* Specific members for a DXF helix. */
         DxfPoint *p0;
                 /*!< Axis base point.\n
