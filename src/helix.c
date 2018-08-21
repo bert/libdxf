5ce964c3a53037b3dc510ad13233ac7d23d90d59
bert <bert.timmerman@xs4all.nl>
2018-08-21 23:29:02 +0200

    HELIX: corrections of error messages in dxf_helix_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -1258,16 +1258,14 @@ dxf_helix_set_visibility
         if (visibility < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative visibility value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         if (visibility > 1)
         {
                 fprintf (stderr,
-                  (_("Error in %s () an out of range visibility value was passed.\n")),
+                  (_("Warning in %s () an out of range value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         helix->visibility = visibility;
 #if DEBUG
