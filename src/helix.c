750bffaf1a7c4fd35e94edf451647225ad77ebf1
bert <bert.timmerman@xs4all.nl>
2018-08-22 22:10:53 +0200

    HELIX: correction of an error in dxf_helix_get_material().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -1899,7 +1899,7 @@ dxf_helix_get_material
         if (helix->material ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the material member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
