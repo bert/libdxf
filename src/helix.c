e0cc47e28bf4c32727e7e748446f7acb0c8ae6d9
bert <bert.timmerman@xs4all.nl>
2018-08-23 22:36:48 +0200

    HELIX: correction of an error message in dxf_helix_get_last().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -3918,7 +3918,7 @@ dxf_helix_get_last
         if (helix->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return ((DxfHelix *) helix);
         }
