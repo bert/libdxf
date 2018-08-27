43683e5cac785cf0544d7ef20cd08c24a94975b2
bert <bert.timmerman@xs4all.nl>
2018-08-27 07:24:04 +0200

    IDBUFFER: correction of an error message in dxf_idbuffer_get_next().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/idbuffer.c
+++ src/idbuffer.c
@@ -790,7 +790,7 @@ dxf_idbuffer_get_next
         if (idbuffer->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
