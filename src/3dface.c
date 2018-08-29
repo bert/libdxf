0277502c817669ece8613240fb677a0ed08dca6f
bert <bert.timmerman@xs4all.nl>
2018-08-29 23:01:14 +0200

    3DFACE: correction in dxf_3dface_free_chain().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -789,7 +789,7 @@ dxf_3dface_free_chain
         }
         while (faces != NULL)
         {
-                struct Dxf3dface *iter = faces->next;
+                Dxf3dface *iter = (Dxf3dface *) faces->next;
                 dxf_3dface_free (faces);
                 faces = (Dxf3dface *) iter;
         }
