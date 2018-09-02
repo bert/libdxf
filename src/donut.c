e5b7cd9a4a7ed271b09afc071e6c3f472499d777
bert <bert.timmerman@xs4all.nl>
2018-09-02 19:25:56 +0200

    DONUT: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -1,7 +1,7 @@
 /*!
  * \file donut.c
  * 
- * \author Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  * 
  * \brief Functions for a libDXF donut entity.
