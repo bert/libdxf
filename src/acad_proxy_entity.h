845303c45c0994101ebf567acde69b8eb741410a
bert <bert.timmerman@xs4all.nl>
2018-09-24 21:01:10 +0200

    ACAD_PROXY_ENTITY: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/acad_proxy_entity.h
+++ src/acad_proxy_entity.h
@@ -1,7 +1,7 @@
 /*!
  * \file acad_proxy_entity.h
  *
- * \author Copyright (C) 2013, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF acad_proxy_entity entity
