5b654c6540e649a539b49c8b0a2987c230658b70
bert <bert.timmerman@xs4all.nl>
2018-09-29 08:01:38 +0200

    HATCH: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.h
+++ src/hatch.h
@@ -1,7 +1,7 @@
 /*!
  * \file hatch.h
  *
- * \author Copyright (C) 2008, 2012, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2008, 2012, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
