0686b2b260c6a2ff93ded26b0640b87a93d3eef7
bert <bert.timmerman@xs4all.nl>
2018-10-02 21:39:36 +0200

    IMAGEDEF_REACTOR: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef_reactor.h
+++ src/imagedef_reactor.h
@@ -1,7 +1,8 @@
 /*!
  * \file imagedef_reactor.h
  *
- * \author Copyright (C) 2015, 2016, 2017 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2015, 2016, 2017, 2018
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF imagedef_reactor object (\c IMAGEDEF_REACTOR).
  *
