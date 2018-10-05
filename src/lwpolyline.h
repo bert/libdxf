9d35d28658fae29e9a6864678171fd7f8946b8a1
bert <bert.timmerman@xs4all.nl>
2018-10-05 21:03:40 +0200

    LWPOLYLINE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/lwpolyline.h
+++ src/lwpolyline.h
@@ -1,7 +1,7 @@
 /*!
  * \file lwpolyline.h
  *
- * \author Copyright (C) 2010, 2012, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2010, 2012, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF light weight polyline entity (\c LWPOLYLINE).
