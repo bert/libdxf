1487decfa3b28cf1f9b565cfc2dc20d18495f127
bert <bert.timmerman@xs4all.nl>
2018-10-11 21:33:06 +0200

    SHAPE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/shape.h
+++ src/shape.h
@@ -1,7 +1,7 @@
 /*!
  * \file shape.h
  *
- * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Header file for a DXF shape entity (\c SHAPE).
