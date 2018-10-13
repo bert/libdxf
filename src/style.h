ddcd2443aea5723c0a31714ba5f547e05acbbe6d
bert <bert.timmerman@xs4all.nl>
2018-10-13 08:03:55 +0200

    STYLE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/style.h
+++ src/style.h
@@ -1,7 +1,7 @@
 /*!
  * \file style.h
  *
- * \author Copyright (C) 2008, 2012, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2008, 2012, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Definition of a DXF (text) style entity \c STYLE.
