a688b3a40fffc97045f60a402fba675bcaf9df53
bert <bert.timmerman@xs4all.nl>
2018-10-14 18:34:48 +0200

    TEXT: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.h
+++ src/text.h
@@ -1,7 +1,7 @@
 /*!
  * \file text.h
  *
- * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2016, 2017
+ * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2016, 2017, 2018
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2012 by Paolo Caroni <kenren89@gmail.com>.
