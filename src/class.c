f966eb8ea9767bfa29083c2ede0230a7e55f8b4e
bert <bert.timmerman@xs4all.nl>
2018-10-17 21:31:58 +0200

    CLASS: correction in a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/class.c
+++ src/class.c
@@ -86,7 +86,7 @@ dxf_class_new ()
  * entity.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfClass *
 dxf_class_init
