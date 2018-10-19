17477c05fd9c59c4f6d670cef0f20b0a0a640e3c
bert <bert.timmerman@xs4all.nl>
2018-10-19 22:16:11 +0200

    POLYLINE: correction of a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/polyline.c
+++ src/polyline.c
@@ -83,7 +83,7 @@ dxf_polyline_new ()
  * \brief Allocate memory and initialize data fields in a \c POLYLINE entity.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfPolyline *
 dxf_polyline_init
