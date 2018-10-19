b9e418801d8ff53fa9462e3b330c0af4691b657e
bert <bert.timmerman@xs4all.nl>
2018-10-19 22:47:38 +0200

    REGION: correction of a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -84,7 +84,7 @@ dxf_region_new ()
  * entity.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfRegion *
 dxf_region_init
