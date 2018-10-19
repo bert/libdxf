c07e87bd15e70749b89b16ea021f87b6de9192ff
bert <bert.timmerman@xs4all.nl>
2018-10-19 20:18:27 +0200

    MLINE: correction of misspellings.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mline.c
+++ src/mline.c
@@ -50,7 +50,7 @@
  * Fill the memory contents with zeros.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfMline *
 dxf_mline_new ()
@@ -86,7 +86,7 @@ dxf_mline_new ()
  * \brief Allocate memory and initialize data fields in a \c MLINE entity.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfMline *
 dxf_mline_init
