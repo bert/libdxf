1adbf34767ea6fab1fac1aad47d77f7766b339a6
bert <bert.timmerman@xs4all.nl>
2018-10-20 11:43:29 +0200

    SORTENTSTABLE: correction of a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sortentstable.c
+++ src/sortentstable.c
@@ -93,7 +93,7 @@ dxf_sortentstable_new ()
  * object.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfSortentsTable *
 dxf_sortentstable_init
