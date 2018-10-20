9094386919a9222e560748eac4e29cd2bf2d6777
bert <bert.timmerman@xs4all.nl>
2018-10-20 12:52:36 +0200

    SPATIAL_INDEX: correction of a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spatial_index.c
+++ src/spatial_index.c
@@ -87,7 +87,7 @@ dxf_spatial_index_new ()
  * object.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfSpatialIndex *
 dxf_spatial_index_init
