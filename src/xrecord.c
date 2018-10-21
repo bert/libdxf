110657cceac41f0597150246e6e4446eaef9af17
bert <bert.timmerman@xs4all.nl>
2018-10-21 16:00:24 +0200

    XRECORD: correction of a misspelling.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/xrecord.c
+++ src/xrecord.c
@@ -84,7 +84,7 @@ dxf_xrecord_new ()
  * object.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfXrecord *
 dxf_xrecord_init
