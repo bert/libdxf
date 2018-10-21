666f2abcacc47ee43b1d7e270adfe5880286c6e6
bert <bert.timmerman@xs4all.nl>
2018-10-21 15:46:49 +0200

    VIEW: correction of misspellings.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/view.c
+++ src/view.c
@@ -48,7 +48,7 @@
  * Fill the memory contents with zeros.
  *
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfView *
 dxf_view_new ()
@@ -85,7 +85,7 @@ dxf_view_new ()
  * entity.
  * 
  * \return \c NULL when no memory was allocated, a pointer to the
- * allocated memory when succesful.
+ * allocated memory when successful.
  */
 DxfView *
 dxf_view_init
