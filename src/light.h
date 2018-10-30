8f8d9a0b012c8f475c0d7e494830c32df9cc029e
bert <bert.timmerman@xs4all.nl>
2018-10-30 22:47:08 +0100

    LIGHT: added dxf_light_get_shadow_mode().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/light.h
+++ src/light.h
@@ -281,6 +281,7 @@ int dxf_light_get_paperspace (DxfLight *light);
 DxfLight *dxf_light_set_paperspace (DxfLight *light, int paperspace);
 int dxf_light_get_graphics_data_size (DxfLight *light);
 DxfLight *dxf_light_set_graphics_data_size (DxfLight *light, int graphics_data_size);
+int16_t dxf_light_get_shadow_mode (DxfLight *light);
 
 
 #ifdef __cplusplus
