0e36f4d0ccbc38cf237f48b4ac94deec3aef82eb
bert <bert.timmerman@xs4all.nl>
2018-11-02 23:10:53 +0100

    LIGHT: added dxf_light_set_transparency().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/light.c
+++ src/light.c
@@ -2320,4 +2320,38 @@ dxf_light_get_transparency
 }
 
 
+/*!
+ * \brief Set the \c transparency for a DXF \c LIGHT entity.
+ *
+ * \return a pointer to \c light when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfLight *
+dxf_light_set_transparency
+(
+        DxfLight *light,
+                /*!< a pointer to a DXF \c LIGHT entity. */
+        long transparency
+                /*!< the \c transparency to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (light == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        light->transparency = transparency;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (light);
+}
+
+
 /* EOF*/
