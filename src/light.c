bc1e46c2d214de405b42912928dbb9b1bacb2914
bert <bert.timmerman@xs4all.nl>
2018-11-09 20:24:39 +0100

    LIGHT: added dxf_light_get_light_type().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/light.c
+++ src/light.c
@@ -3325,4 +3325,47 @@ dxf_light_set_falloff_angle
 }
 
 
+/*!
+ * \brief Get the \c light_type from a DXF \c LIGHT entity.
+ *
+ * \return \c light_type when successful, or \c EXIT_FAILURE when an error
+ * occurred.
+ */
+int16_t
+dxf_light_get_light_type
+(
+        DxfLight *light
+                /*!< a pointer to a DXF \c LIGHT entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (light == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (light->light_type < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (light->light_type > 3)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (light->light_type);
+}
+
+
 /* EOF*/
