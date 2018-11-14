dbc67815b6ebb2676b12caeacb213a1ec7334bce
bert <bert.timmerman@xs4all.nl>
2018-11-14 21:13:50 +0100

    MESH: added dxf_mesh_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.h
+++ src/mesh.h
@@ -254,6 +254,7 @@ DxfMesh *dxf_mesh_new ();
 DxfMesh *dxf_mesh_init (DxfMesh *mesh);
 int dxf_mesh_free (DxfMesh *mesh);
 void dxf_mesh_free_list (DxfMesh *meshes);
+int dxf_mesh_get_id_code (DxfMesh *mesh);
 
 
 #ifdef __cplusplus
