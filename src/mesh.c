86143ece56c3f152d6483090e119b2fcc05d69ae
bert <bert.timmerman@xs4all.nl>
2018-11-15 21:12:56 +0100

    MESH: added dxf_mesh_get_elevation().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -475,4 +475,34 @@ dxf_mesh_set_layer
 }
 
 
+/*!
+ * \brief Get the \c elevation from a DXF \c MESH entity.
+ *
+ * \return \c elevation.
+ */
+double
+dxf_mesh_get_elevation
+(
+        DxfMesh *mesh
+                /*!< a pointer to a DXF \c MESH entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mesh == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mesh->elevation);
+}
+
+
 /* EOF*/
