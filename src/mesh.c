d48ffd7797eb43e7065e60285dc145bea8dd9156
bert <bert.timmerman@xs4all.nl>
2018-11-16 19:44:22 +0100

    MESH: added dxf_mesh_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -734,4 +734,50 @@ dxf_mesh_get_visibility
 }
 
 
+/*!
+ * \brief Set the \c visibility for a DXF \c MESH entity.
+ *
+ * \return a pointer to \c mesh when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfMesh *
+dxf_mesh_set_visibility
+(
+        DxfMesh *mesh,
+                /*!< a pointer to a DXF \c MESH entity. */
+        int16_t visibility
+                /*!< the \c visibility to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mesh == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (visibility < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        if (visibility > 1)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was passed.\n")),
+                  __FUNCTION__);
+        }
+        mesh->visibility = visibility;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mesh);
+}
+
+
 /* EOF*/
