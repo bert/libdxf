5756aafe34aa78d7128faf395f34c74dde90470f
bert <bert.timmerman@xs4all.nl>
2018-11-25 22:39:35 +0100

    MESH: added dxf_mesh_get_edge_vertex_index().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -2423,4 +2423,34 @@ dxf_mesh_set_face_list_item
 }
 
 
+/*!
+ * \brief Get the \c edge_vertex_index from a DXF \c MESH entity.
+ *
+ * \return \c edge_vertex_index.
+ */
+int32_t
+dxf_mesh_get_edge_vertex_index
+(
+        DxfMesh *mesh
+                /*!< a pointer to a DXF \c MESH entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mesh == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mesh->edge_vertex_index);
+}
+
+
 /* EOF*/
