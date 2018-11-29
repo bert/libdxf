0c4e147f92189562f2186b7322e464d0b53af085
bert <bert.timmerman@xs4all.nl>
2018-11-29 21:37:55 +0100

    MESH: added dxf_mesh_get_sub_entity_marker().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -2731,4 +2731,34 @@ dxf_mesh_set_subdivision_level
 }
 
 
+/*!
+ * \brief Get the \c sub_entity_marker from a DXF \c MESH entity.
+ *
+ * \return \c sub_entity_marker.
+ */
+int32_t
+dxf_mesh_get_sub_entity_marker
+(
+        DxfMesh *mesh
+                /*!< a pointer to a DXF \c MESH entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mesh == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mesh->sub_entity_marker);
+}
+
+
 /* EOF*/
--- src/mesh.h
+++ src/mesh.h
@@ -318,6 +318,7 @@ int32_t dxf_mesh_get_property_type (DxfMesh *mesh);
 DxfMesh *dxf_mesh_set_property_type (DxfMesh *mesh, int32_t property_type);
 int32_t dxf_mesh_get_subdivision_level (DxfMesh *mesh);
 DxfMesh *dxf_mesh_set_subdivision_level (DxfMesh *mesh, int32_t subdivision_level);
+int32_t dxf_mesh_get_sub_entity_marker (DxfMesh *mesh);
 
 
 #ifdef __cplusplus
