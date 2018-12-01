5599c0d31214a98e88565112b2ea1f480664e8ee
bert <bert.timmerman@xs4all.nl>
2018-12-01 09:46:27 +0100

    MESH: added dxf_mesh_get_edge_create_value().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -3122,4 +3122,35 @@ dxf_mesh_set_edge_crease_count_level_0
 }
 
 
+/*!
+ * \brief Get the \c edge_create_value of a DXF \c MESH entity.
+ *
+ * \return \c edge_create_value.
+ */
+double
+dxf_mesh_get_edge_create_value
+(
+        DxfMesh *mesh
+                /*!< a pointer to a DXF \c MESH entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (mesh == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mesh->edge_create_value);
+}
+
+
 /* EOF*/
