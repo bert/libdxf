002e729f5a076636fb4f65cb00e9b554e951fc4b
bert <bert.timmerman@xs4all.nl>
2018-12-04 21:50:54 +0100

    UTIL: added dxf_int32_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/util.c
+++ src/util.c
@@ -535,6 +535,47 @@ dxf_int32_new ()
 }
 
 
+/*!
+ * \brief Allocate memory and initialize data fields in a \c DxfInt32
+ * object.
+ * 
+ * \return \c NULL when no memory was allocated, a pointer to the
+ * allocated memory when successful.
+ */
+DxfInt32 *
+dxf_int32_init
+(
+        DxfInt32 *i
+                /*!< a pointer to the DxfInt32 object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (i == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                i = dxf_int_new ();
+        }
+        if (i == NULL)
+        {
+              fprintf (stderr,
+                (_("Error in %s () could not allocate memory for a DxfInt32 struct.\n")),
+                __FUNCTION__);
+              return (NULL);
+        }
+        i->value = 0;
+        i->next = NULL;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (i);
+}
+
+
 int
 dxf_read_is_double (int type)
 {
