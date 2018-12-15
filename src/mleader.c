e8b2e6738d1a6cc4dc3b5c025e2bb1f14706abb2
bert <bert.timmerman@xs4all.nl>
2018-12-15 23:22:27 +0100

    MLEADER: added dxf_mleader_get_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -718,4 +718,46 @@ dxf_mleader_set_linetype_scale
 }
 
 
+/*!
+ * \brief Get the \c visibility from a DXF \c MLEADER entity.
+ *
+ * \return \c visibility.
+ */
+int16_t
+dxf_mleader_get_visibility
+(
+        DxfMLeader *mleader
+                /*!< a pointer to a DXF \c MLEADER entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (mleader->visibility < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+        if (mleader->visibility > 1)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleader->visibility);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -546,6 +546,7 @@ double dxf_mleader_get_thickness (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_thickness (DxfMLeader *mleader, double thickness);
 double dxf_mleader_get_linetype_scale (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_linetype_scale (DxfMLeader *mleader, double linetype_scale);
+int16_t dxf_mleader_get_visibility (DxfMLeader *mleader);
 
 
 #ifdef __cplusplus
