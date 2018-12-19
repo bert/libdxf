e81055ce78919dc01be4d25fe3103cb19424576b
bert <bert.timmerman@xs4all.nl>
2018-12-19 21:07:14 +0100

    MLEADER: added dxf_mleader_get_transparency().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -1857,4 +1857,35 @@ dxf_mleader_set_color_name
 }
 
 
+/*!
+ * \brief Get the \c transparency from a DXF \c MLEADER entity.
+ *
+ * \return \c transparency when successful, or \c EXIT_FAILURE when an
+ * error occurred.
+ */
+long
+dxf_mleader_get_transparency
+(
+        DxfMLeader *mleader
+                /*!< a pointer to a DXF \c MLEADER entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleader->transparency);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -574,6 +574,7 @@ long dxf_mleader_get_color_value (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_color_value (DxfMLeader *mleader, long color_value);
 char *dxf_mleader_get_color_name (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_color_name (DxfMLeader *mleader, char *color_name);
+long dxf_mleader_get_transparency (DxfMLeader *mleader);
 
 
 #ifdef __cplusplus
