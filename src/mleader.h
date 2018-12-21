6052378d277d6d8e655d58a93fc57ea282720326
bert <bert.timmerman@xs4all.nl>
2018-12-21 22:07:12 +0100

    MLEADER: added dxf_mleader_set_text_color().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.h
+++ src/mleader.h
@@ -591,6 +591,7 @@ DxfMLeader *dxf_mleader_set_property_override_flag (DxfMLeader *mleader, int32_t
 int32_t dxf_mleader_get_leader_line_color (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_leader_line_color (DxfMLeader *mleader, int32_t leader_line_color);
 int32_t dxf_mleader_get_text_color (DxfMLeader *mleader);
+DxfMLeader *dxf_mleader_set_text_color (DxfMLeader *mleader, int32_t text_color);
 
 
 #ifdef __cplusplus
