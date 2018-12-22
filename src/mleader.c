1e3ba5f244632a7737b954020e85415db6cbdd34
bert <bert.timmerman@xs4all.nl>
2018-12-22 21:07:17 +0100

    MLEADER: added dxf_mleader_get_content_type().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -2769,4 +2769,34 @@ dxf_mleader_set_leader_line_weight
 }
 
 
+/*!
+ * \brief Get the \c content_type from a DXF \c MLEADER entity.
+ *
+ * \return \c content_type.
+ */
+int16_t
+dxf_mleader_get_content_type
+(
+        DxfMLeader *mleader
+                /*!< a pointer to a DXF \c MLEADER entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleader->content_type);
+}
+
+
 /* EOF*/
