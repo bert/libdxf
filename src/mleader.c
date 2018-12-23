ce224ccc6bdd962af7c75fc66c0e347e61f848d9
bert <bert.timmerman@xs4all.nl>
2018-12-23 15:28:00 +0100

    MLEADER: added dxf_mleader_get_text_angle_type().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -2901,4 +2901,35 @@ dxf_mleader_set_text_left_attachment_type
 }
 
 
+/*!
+ * \brief Get the \c text_angle_type from a DXF \c MLEADER
+ * entity.
+ *
+ * \return \c text_angle_type.
+ */
+int16_t
+dxf_mleader_get_text_angle_type
+(
+        DxfMLeader *mleader
+                /*!< a pointer to a DXF \c MLEADER entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleader->text_angle_type);
+}
+
+
 /* EOF*/
