6e8ae6f24abcae4f187abd2ff8d4eb80fb0549b8
bert <bert.timmerman@xs4all.nl>
2018-12-25 13:24:21 +0100

    MLEADER: added dxf_mleader_set_text_align_in_IPE().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -3199,4 +3199,39 @@ dxf_mleader_get_text_align_in_IPE
 }
 
 
+/*!
+ * \brief Set the \c text_align_in_IPE for a DXF \c MLEADER entity.
+ *
+ * \return a pointer to \c mleader when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfMLeader *
+dxf_mleader_set_text_align_in_IPE
+(
+        DxfMLeader *mleader,
+                /*!< a pointer to a DXF \c MLEADER entity. */
+        int16_t text_align_in_IPE
+                /*!< the \c text_align_in_IPE to be set for the
+                 * entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        mleader->text_align_in_IPE = text_align_in_IPE;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleader);
+}
+
+
 /* EOF*/
