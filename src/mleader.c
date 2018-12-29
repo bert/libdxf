2336382e81f9369f46a76685a55a04bf1e3b43b6
bert <bert.timmerman@xs4all.nl>
2018-12-29 22:27:06 +0100

    MLEADER: added dxf_mleader_get_arrow_head_id().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -4534,4 +4534,42 @@ dxf_mleader_set_block_content_id
 }
 
 
+/*!
+ * \brief Get the \c arrow_head_id from a DXF \c MLEADER entity.
+ *
+ * \return \c arrow_head_id when successful, \c NULL when an error
+ * occurred.
+ */
+char *
+dxf_mleader_get_arrow_head_id
+(
+        DxfMLeader *mleader
+                /*!< a pointer to a DXF \c MLEADER entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleader == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (mleader->arrow_head_id ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (mleader->arrow_head_id));
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -648,6 +648,7 @@ char *dxf_mleader_get_text_style_id (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_text_style_id (DxfMLeader *mleader, char *text_style_id);
 char *dxf_mleader_get_block_content_id (DxfMLeader *mleader);
 DxfMLeader *dxf_mleader_set_block_content_id (DxfMLeader *mleader, char *block_content_id);
+char *dxf_mleader_get_arrow_head_id (DxfMLeader *mleader);
 
 
 #ifdef __cplusplus
