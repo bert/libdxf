cff6dc8bc46a7f9ca47c47f3346d8c3f2cd49f77
bert <bert.timmerman@xs4all.nl>
2019-01-03 21:26:05 +0100

    MLEADER: added dxf_mleader_context_data_set_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -5244,4 +5244,47 @@ dxf_mleader_context_data_get_z0
 }
 
 
+/*!
+ * \brief Set the Z-value \c z0 of a DxfMLeaderContextData object of a
+ * DXF \c MLEADER entity.
+ *
+ * \return a pointer to \c data when successful, or \c NULL when an error
+ * occurred.
+ */
+DxfMLeaderContextData *
+dxf_mleader_context_data_set_z0
+(
+        DxfMLeaderContextData *data,
+                /*!< a pointer to a DxfMLeaderContextData object. */
+        double z0
+                /*!< the Z-value of the \c z0 of a DxfMLeaderContextData
+                 * object of a DXF \c MLEADER entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data->p0 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->p0->z0 = z0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -664,6 +664,7 @@ DxfMLeaderContextData *dxf_mleader_context_data_set_x0 (DxfMLeaderContextData *d
 double dxf_mleader_context_data_get_y0 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_y0 (DxfMLeaderContextData *data, double y0);
 double dxf_mleader_context_data_get_z0 (DxfMLeaderContextData *data);
+DxfMLeaderContextData *dxf_mleader_context_data_set_z0 (DxfMLeaderContextData *data, double z0);
 
 
 #ifdef __cplusplus
