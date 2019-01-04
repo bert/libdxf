6ef571255c3f32a32dcf984682e30e86825e5bae
bert <bert.timmerman@xs4all.nl>
2019-01-04 23:18:28 +0100

    MLEADER: added dxf_mleader_context_data_get_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -5780,4 +5780,43 @@ dxf_mleader_context_data_set_x1
 }
 
 
+/*!
+ * \brief Get the Y-value \c y1 of a DxfMLeaderContextData object of a
+ * DXF \c MLEADER entity.
+ *
+ * \return the Y-value \c y1.
+ */
+double
+dxf_mleader_context_data_get_y1
+(
+        DxfMLeaderContextData *data
+                /*!< a pointer to a \c DxfMLeaderContextData object. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (data->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data->p1->y0);
+}
+
+
 /* EOF*/
