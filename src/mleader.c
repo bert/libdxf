7209752184a4aa8d141baa87bf99153d50572ae9
bert <bert.timmerman@xs4all.nl>
2019-01-06 22:36:10 +0100

    MLEADER: added dxf_mleader_context_data_get_p3().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -6271,4 +6271,43 @@ dxf_mleader_context_data_set_z2
 }
 
 
+/*!
+ * \brief Get the \c p3 of a \c DxfMLeaderContextData object of a DXF
+ * \c MLEADER entity.
+ *
+ * \return \c p3.
+ */
+DxfPoint *
+dxf_mleader_context_data_get_p3
+(
+        DxfMLeaderContextData *data
+                /*!< a pointer to a \c DxfMLeaderContextData object of a
+                 * DXF \c MLEADER entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data->p3 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data->p3);
+}
+
+
 /* EOF*/
