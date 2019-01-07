1bc48491137f73fea55905c61240620d8b6c8b21
bert <bert.timmerman@xs4all.nl>
2019-01-07 22:39:58 +0100

    MLEADER: added dxf_mleader_context_data_get_z3().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.h
+++ src/mleader.h
@@ -695,6 +695,7 @@ double dxf_mleader_context_data_get_x3 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_x3 (DxfMLeaderContextData *data, double x3);
 double dxf_mleader_context_data_get_y3 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_y3 (DxfMLeaderContextData *data, double y3);
+double dxf_mleader_context_data_get_z3 (DxfMLeaderContextData *data);
 
 
 #ifdef __cplusplus
