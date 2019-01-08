67f056c1c5bb33ac4df4cf0601fa41fa80bf9c5f
bert <bert.timmerman@xs4all.nl>
2019-01-08 21:15:50 +0100

    MLEADER: added dxf_mleader_context_data_set_p4().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -6637,4 +6637,46 @@ dxf_mleader_context_data_get_p4
 }
 
 
+/*!
+ * \brief Set the \c p4 of a \c DxfMLeaderContextData object of a DXF
+ * \c MLEADER entity.
+ *
+ * \return a pointer to \c data when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfMLeaderContextData *
+dxf_mleader_context_data_set_p4
+(
+        DxfMLeaderContextData *data,
+                /*!< a pointer to a \c DxfMLeaderContextData object. */
+        DxfPoint *p4
+                /*!< a pointer to a DXF \c POINT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (p4 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->p4 = p4;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -698,6 +698,7 @@ DxfMLeaderContextData *dxf_mleader_context_data_set_y3 (DxfMLeaderContextData *d
 double dxf_mleader_context_data_get_z3 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_z3 (DxfMLeaderContextData *data, double z3);
 DxfPoint *dxf_mleader_context_data_get_p4 (DxfMLeaderContextData *data);
+DxfMLeaderContextData *dxf_mleader_context_data_set_p4 (DxfMLeaderContextData *data, DxfPoint *p4);
 
 
 #ifdef __cplusplus
