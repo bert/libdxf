ce2a6942c685e233f7e80e254826ae8fe5a12a55
bert <bert.timmerman@xs4all.nl>
2019-01-10 19:49:51 +0100

    MLEADER: added dxf_mleader_context_data_get_block_content_scale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -7252,4 +7252,35 @@ dxf_mleader_context_data_set_z5
 }
 
 
+/*!
+ * \brief Get the \c block_content_scale of a DXF
+ * \c DxfMLeaderContextData object.
+ *
+ * \return the \c block_content_scale.
+ */
+double
+dxf_mleader_context_data_get_block_content_scale
+(
+        DxfMLeaderContextData *data
+                /*!< a pointer to a DXF \c DxfMLeaderContextData object. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data->block_content_scale);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -713,6 +713,7 @@ double dxf_mleader_context_data_get_y5 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_y5 (DxfMLeaderContextData *data, double y5);
 double dxf_mleader_context_data_get_z5 (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_z5 (DxfMLeaderContextData *data, double z5);
+double dxf_mleader_context_data_get_block_content_scale (DxfMLeaderContextData *data);
 
 
 #ifdef __cplusplus
