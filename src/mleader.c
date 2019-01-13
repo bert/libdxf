820f2dfefa61d0cea177c2c1903157c72d4eae81
bert <bert.timmerman@xs4all.nl>
2019-01-13 22:30:19 +0100

    MLEADER: added dxf_mleader_context_data_set_text_background_transparency().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -8084,4 +8084,40 @@ dxf_mleader_context_data_get_text_background_transparency
 }
 
 
+/*!
+ * \brief Set the \c text_background_transparency of a DXF
+ * \c DxfMLeaderContextData object.
+ *
+ * \return a pointer to \c data when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfMLeaderContextData *
+dxf_mleader_context_data_set_text_background_transparency
+(
+        DxfMLeaderContextData *data,
+                /*!< a pointer to a DXF \c DxfMLeaderContextData object. */
+        int32_t text_background_transparency
+                /*!< the \c text_background_transparency to be set for
+                 * the object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->text_background_transparency = text_background_transparency;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -738,6 +738,7 @@ DxfMLeaderContextData *dxf_mleader_context_data_set_break_point_index (DxfMLeade
 int32_t dxf_mleader_context_data_get_text_background_color (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_text_background_color (DxfMLeaderContextData *data, int32_t text_background_color);
 int32_t dxf_mleader_context_data_get_text_background_transparency (DxfMLeaderContextData *data);
+DxfMLeaderContextData *dxf_mleader_context_data_set_text_background_transparency (DxfMLeaderContextData *data, int32_t text_background_transparency);
 
 
 #ifdef __cplusplus
