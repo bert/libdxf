8a4d6de46fb0eef04680fcbcbb018d1547552762
bert <bert.timmerman@xs4all.nl>
2019-01-17 20:32:36 +0100

    MLEADER: added dxf_mleader_context_data_set_text_attachment().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.h
+++ src/mleader.h
@@ -762,6 +762,7 @@ DxfMLeaderContextData *dxf_mleader_context_data_set_landing_gap (DxfMLeaderConte
 int16_t dxf_mleader_context_data_get_text_line_spacing_style (DxfMLeaderContextData *data);
 DxfMLeaderContextData *dxf_mleader_context_data_set_text_line_spacing_style (DxfMLeaderContextData *data, int16_t text_line_spacing_style);
 int16_t dxf_mleader_context_data_get_text_attachment (DxfMLeaderContextData *data);
+DxfMLeaderContextData *dxf_mleader_context_data_set_text_attachment (DxfMLeaderContextData *data, int16_t text_attachment);
 
 
 #ifdef __cplusplus
