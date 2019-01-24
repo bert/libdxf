83a13e1ac869fe90ecc8e259d9cfba1f36d34f2a
bert <bert.timmerman@xs4all.nl>
2019-01-24 22:01:59 +0100

    MLEADER: added dxf_mleader_leader_node_get_p2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleader.c
+++ src/mleader.c
@@ -10744,4 +10744,43 @@ dxf_mleader_leader_node_set_z1
 }
 
 
+/*!
+ * \brief Get the \c p2 of a \c DxfMLeaderLeaderNode object of a DXF
+ * \c MLEADER entity.
+ *
+ * \return \c p2.
+ */
+DxfPoint *
+dxf_mleader_leader_node_get_p2
+(
+        DxfMLeaderLeaderNode *node
+                /*!< a pointer to a \c DxfMLeaderLeaderNode object of a
+                 * DXF \c MLEADER entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (node == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (node->p2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (node->p2);
+}
+
+
 /* EOF*/
--- src/mleader.h
+++ src/mleader.h
@@ -812,6 +812,7 @@ double dxf_mleader_leader_node_get_y1 (DxfMLeaderLeaderNode *node);
 DxfMLeaderLeaderNode *dxf_mleader_leader_node_set_y1 (DxfMLeaderLeaderNode *node, double y1);
 double dxf_mleader_leader_node_get_z1 (DxfMLeaderLeaderNode *node);
 DxfMLeaderLeaderNode *dxf_mleader_leader_node_set_z1 (DxfMLeaderLeaderNode *node, double z1);
+DxfPoint *dxf_mleader_leader_node_get_p2 (DxfMLeaderLeaderNode *node);
 
 
 #ifdef __cplusplus
