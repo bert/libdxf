46dfe3f110dda5f9e996d3126ec083570d08b161
bert <bert.timmerman@xs4all.nl>
2019-02-14 21:55:37 +0100

    MLEADERSTYLE: added dxf_mleaderstyle_set_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleaderstyle.c
+++ src/mleaderstyle.c
@@ -1280,4 +1280,47 @@ dxf_mleaderstyle_get_dictionary_owner_soft
 }
 
 
+/*!
+ * \brief Set the pointer to the \c dictionary_owner_soft for a DXF
+ * \c MLEADERSTYLE entity.
+ *
+ * \return a pointer to \c mleaderstyle when successful, or \c NULL when an error
+ * occurred.
+ */
+DxfMLeaderstyle *
+dxf_mleaderstyle_set_dictionary_owner_soft
+(
+        DxfMLeaderstyle *mleaderstyle,
+                /*!< a pointer to a DXF \c MLEADERSTYLE entity. */
+        char *dictionary_owner_soft
+                /*!< a string containing the pointer to the \c
+                 * dictionary_owner_soft for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleaderstyle == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (dictionary_owner_soft == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        mleaderstyle->dictionary_owner_soft = strdup (dictionary_owner_soft);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleaderstyle);
+}
+
+
 /* EOF*/
--- src/mleaderstyle.h
+++ src/mleaderstyle.h
@@ -361,6 +361,7 @@ DxfMLeaderstyle *dxf_mleaderstyle_set_shadow_mode (DxfMLeaderstyle *mleaderstyle
 DxfBinaryGraphicsData *dxf_mleaderstyle_get_binary_graphics_data (DxfMLeaderstyle *mleaderstyle);
 DxfMLeaderstyle *dxf_mleaderstyle_set_binary_graphics_data (DxfMLeaderstyle *mleaderstyle, DxfBinaryGraphicsData *data);
 char *dxf_mleaderstyle_get_dictionary_owner_soft (DxfMLeaderstyle *mleaderstyle);
+DxfMLeaderstyle *dxf_mleaderstyle_set_dictionary_owner_soft (DxfMLeaderstyle *mleaderstyle, char *dictionary_owner_soft);
 
 
 #ifdef __cplusplus
