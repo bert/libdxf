8f6f6276c7ecff1195b493ec0b163dd64026cee4
bert <bert.timmerman@xs4all.nl>
2019-02-26 22:33:24 +0100

    MLEADERSTYLE: added dxf_mleaderstyle_set_text_angle_type().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleaderstyle.c
+++ src/mleaderstyle.c
@@ -3481,4 +3481,38 @@ dxf_mleaderstyle_get_text_angle_type
 }
 
 
+/*!
+ * \brief Set the \c text_angle_type for a DXF \c MLEADERSTYLE entity.
+ *
+ * \return a pointer to \c mleaderstyle when successful, or \c NULL when
+ * an error occurred.
+ */
+DxfMLeaderstyle *
+dxf_mleaderstyle_set_text_angle_type
+(
+        DxfMLeaderstyle *mleaderstyle,
+                /*!< a pointer to a DXF \c MLEADERSTYLE entity. */
+        int16_t text_angle_type
+                /*!< the \c text_angle_type to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleaderstyle == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        mleaderstyle->text_angle_type = text_angle_type;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleaderstyle);
+}
+
+
 /* EOF*/
--- src/mleaderstyle.h
+++ src/mleaderstyle.h
@@ -425,6 +425,7 @@ DxfMLeaderstyle *dxf_mleaderstyle_set_leader_line_color (DxfMLeaderstyle *mleade
 int16_t dxf_mleaderstyle_get_text_left_attachment_type (DxfMLeaderstyle *mleaderstyle);
 DxfMLeaderstyle *dxf_mleaderstyle_set_text_left_attachment_type (DxfMLeaderstyle *mleaderstyle, int16_t text_left_attachment_type);
 int16_t dxf_mleaderstyle_get_text_angle_type (DxfMLeaderstyle *mleaderstyle);
+DxfMLeaderstyle *dxf_mleaderstyle_set_text_angle_type (DxfMLeaderstyle *mleaderstyle, int16_t text_angle_type);
 
 
 #ifdef __cplusplus
