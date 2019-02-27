36667cf2fe27483d7e740df75e0833e2599613bf
bert <bert.timmerman@xs4all.nl>
2019-02-27 21:40:59 +0100

    MLEADERSTYLE: added dxf_mleaderstyle_get_text_attachment_direction().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleaderstyle.c
+++ src/mleaderstyle.c
@@ -3714,4 +3714,35 @@ dxf_mleaderstyle_set_block_content_connection_type
 }
 
 
+/*!
+ * \brief Get the \c text_attachment_direction from a DXF
+ * \c MLEADERSTYLE entity.
+ *
+ * \return \c text_attachment_direction.
+ */
+int16_t
+dxf_mleaderstyle_get_text_attachment_direction
+(
+        DxfMLeaderstyle *mleaderstyle
+                /*!< a pointer to a DXF \c MLEADERSTYLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (mleaderstyle == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (mleaderstyle->text_attachment_direction);
+}
+
+
 /* EOF*/
--- src/mleaderstyle.h
+++ src/mleaderstyle.h
@@ -432,6 +432,7 @@ int16_t dxf_mleaderstyle_get_text_alignment_type (DxfMLeaderstyle *mleaderstyle)
 DxfMLeaderstyle *dxf_mleaderstyle_set_text_alignment_type (DxfMLeaderstyle *mleaderstyle, int16_t text_alignment_type);
 int16_t dxf_mleaderstyle_get_block_content_connection_type (DxfMLeaderstyle *mleaderstyle);
 DxfMLeaderstyle *dxf_mleaderstyle_set_block_content_connection_type (DxfMLeaderstyle *mleaderstyle, int16_t block_content_connection_type);
+int16_t dxf_mleaderstyle_get_text_attachment_direction (DxfMLeaderstyle *mleaderstyle);
 
 
 #ifdef __cplusplus
