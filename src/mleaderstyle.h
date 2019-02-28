52b9bc0f987b690e9cf89371c6ae102b8864e631
bert <bert.timmerman@xs4all.nl>
2019-02-28 22:55:36 +0100

    MLEADERSTYLE: added dxf_mleaderstyle_get_enable_landing().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mleaderstyle.h
+++ src/mleaderstyle.h
@@ -438,6 +438,7 @@ int16_t dxf_mleaderstyle_get_bottom_text_attachment_direction (DxfMLeaderstyle *
 DxfMLeaderstyle *dxf_mleaderstyle_set_bottom_text_attachment_direction (DxfMLeaderstyle *mleaderstyle, int16_t bottom_text_attachment_direction);
 int16_t dxf_mleaderstyle_get_top_text_attachment_direction (DxfMLeaderstyle *mleaderstyle);
 DxfMLeaderstyle *dxf_mleaderstyle_set_top_text_attachment_direction (DxfMLeaderstyle *mleaderstyle, int16_t top_text_attachment_direction);
+int dxf_mleaderstyle_get_enable_landing (DxfMLeaderstyle *mleaderstyle);
 
 
 #ifdef __cplusplus
