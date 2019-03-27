748c27bcfa38d306583d080356cee85c63f48bfe
bert <bert.timmerman@xs4all.nl>
2019-03-27 22:36:51 +0100

    RTEXT: added dxf_rtext_set_extr_x0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rtext.c
+++ src/rtext.c
@@ -2527,4 +2527,40 @@ dxf_rtext_get_extr_x0
 }
 
 
+/*!
+ * \brief Set the X-value of the extrusion vector \c extr_x0 of a DXF
+ * \c RTEXT entity.
+ *
+ * \return a pointer to \c rtext when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfRText *
+dxf_rtext_set_extr_x0
+(
+        DxfRText *rtext,
+                /*!< a pointer to a DXF \c RTEXT entity. */
+        double extr_x0
+                /*!< the X-value of the extrusion vector \c extr_x0 of a
+                 * DXF \c RTEXT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (rtext == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        rtext->extr_x0 = extr_x0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (rtext);
+}
+
+
 /* EOF */
--- src/rtext.h
+++ src/rtext.h
@@ -295,6 +295,7 @@ DxfRText *dxf_rtext_set_rot_angle (DxfRText *rtext, double rot_angle);
 int dxf_rtext_get_type_flags (DxfRText *rtext);
 DxfRText *dxf_rtext_set_type_flags (DxfRText *rtext, int type_flags);
 double dxf_rtext_get_extr_x0 (DxfRText *rtext);
+DxfRText *dxf_rtext_set_extr_x0 (DxfRText *rtext, double extr_x0);
 
 
 #endif /* LIBDXF_SRC_RTEXT_H */
