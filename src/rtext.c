5383b96c57a656cc6f7fbc14f5f1f3ad23198898
bert <bert.timmerman@xs4all.nl>
2019-03-28 22:15:19 +0100

    RTEXT: added dxf_rtext_set_next().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rtext.c
+++ src/rtext.c
@@ -2817,4 +2817,43 @@ dxf_rtext_get_next
 }
 
 
+/*!
+ * \brief Set the pointer to the next \c TEXT for a DXF \c RTEXT
+ * entity.
+ */
+DxfRText *
+dxf_rtext_set_next
+(
+        DxfRText *rtext,
+                /*!< a pointer to a DXF \c RTEXT entity. */
+        DxfRText *next
+                /*!< a pointer to the next \c RTEXT for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (rtext == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (next == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        rtext->next = (struct DxfRText *) next;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (rtext);
+}
+
+
 /* EOF */
