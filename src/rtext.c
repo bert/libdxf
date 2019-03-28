b013924ffa80e4eb376ddfba465ad153a7d88db7
bert <bert.timmerman@xs4all.nl>
2019-03-28 21:55:21 +0100

    RTEXT: added dxf_rtext_set_extr_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rtext.c
+++ src/rtext.c
@@ -2663,4 +2663,40 @@ dxf_rtext_get_extr_z0
 }
 
 
+/*!
+ * \brief Set the Z-value of the extrusion vector \c extr_z0 of a DXF
+ * \c RTEXT entity.
+ *
+ * \return a pointer to \c rtext when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfRText *
+dxf_rtext_set_extr_z0
+(
+        DxfRText *rtext,
+                /*!< a pointer to a DXF \c RTEXT entity. */
+        double extr_z0
+                /*!< the Z-value of the extrusion vector \c extr_z0 of a
+                 * DXF \c RTEXT entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (rtext == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        rtext->extr_z0 = extr_z0;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (rtext);
+}
+
+
 /* EOF */
