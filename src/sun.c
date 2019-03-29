95ceb47fde211f4994f16a8d5b174ab26a9b295f
bert <bert.timmerman@xs4all.nl>
2019-03-30 00:00:42 +0100

    SUN: added sun.[ch]
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/sun.c
@@ -0,0 +1,44 @@
+/*!
+ * \file sun.c
+ *
+ * \author Copyright (C) 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Functions for a DXF sun entity (\c SUN).
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+
+#include "sun.h"
+
+
+/* EOF */
---
+++ src/sun.h
@@ -0,0 +1,201 @@
+/*!
+ * \file sun.h
+ *
+ * \author Copyright (C) 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF sun entity (\c SUN).
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+
+#ifndef LIBDXF_SRC_SUN_H
+#define LIBDXF_SRC_SUN_H
+
+
+#include "global.h"
+#include "point.h"
+#include "binary_graphics_data.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*!
+ * \brief DXF definition of an AutoCAD sun entity.
+ */
+typedef struct
+dxf_sun_struct
+{
+        /* Members common for all DXF drawable entities. */
+        int id_code;
+                /*!< Identification number for the entity.\n
+                 * This is to be an unique (sequential) number in the DXF
+                 * file.\n
+                 * Group code = 5. */
+        char *linetype;
+                /*!< The linetype of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Group code = 6. */
+        char *layer;
+                /*!< Layer on which the entity is drawn.\n
+                 * Defaults to layer "0" if no valid layername is given.\n
+                 * Group code = 8. */
+        double elevation;
+                /*!< Elevation of the entity in the local Z-direction.\n
+                 * Defaults to 0.0 if omitted in the DXF file, or prior
+                 * to DXF version R12, or DXF_FLATLAND equals 0 (default).\n
+                 * Group code = 38.\n
+                 * \deprecated As of version R11. */
+        double thickness;
+                /*!< Thickness of the entity in the local Z-direction.\n
+                 * Defaults to 0.0 if ommitted in the DXF file.\n
+                 * Group code = 39. */
+        double linetype_scale;
+                /*!< Linetype scale (optional).\n
+                 * Group code = 48.\n
+                 * \since Introduced in version R13. */
+        int16_t visibility;
+                /*!< Object visibility (optional):\n
+                 * <ol>
+                 * <li value = "0"> Visible</li>
+                 * <li value = "1"> Invisible</li>
+                 * </ol>
+                 * Group code = 60.\n
+                 * \since Introduced in version R13. */
+        int color;
+                /*!< Color of the entity.\n
+                 * Defaults to \c BYLAYER if ommitted in the DXF file.\n
+                 * Note that entities encapsulated in a block with the
+                 * color \c BYBLOCK are represented in the "native" color of
+                 * the \c BLOCK entity.\n
+                 * Group code = 62. */
+        int paperspace;
+                /*!< Entities are to be drawn on either \c PAPERSPACE or
+                 * \c MODELSPACE.\n
+                 * Optional, defaults to \c DXF_MODELSPACE (0).\n
+                 * Group code = 67.\n
+                 * \since Introduced in version R13. */
+        int graphics_data_size;
+                /*!< Number of bytes in the proxy entity graphics
+                 * represented in the sub-sequent 310 groups, which are
+                 * binary chunk records (optional).\n
+                 * Group code = 92.\n
+                 * \since Introduced in version R2000.\n
+                 * \warning On some 64 bit workstations output is
+                 * generated with group code "160", thus omitting group
+                 * code "92". */
+        int16_t shadow_mode;
+                /*!< Shadow mode:\n
+                 * <ol>
+                 * <li value = "0"> Casts and receives shadows.</li>
+                 * <li value = "1"> Casts shadows.</li>
+                 * <li value = "2"> Receives shadows.</li>
+                 * <li value = "3"> Ignores shadows.</li>
+                 * </ol>\n
+                 * Group code = 284.\n
+                 * \since Introduced in version R2009. */
+        DxfBinaryGraphicsData *binary_graphics_data;
+                /*!< Proxy entity graphics data.\n
+                 * Multiple lines of 256 characters maximum per line
+                 * (optional).\n
+                 * Group code = 310.\n
+                 * \since Introduced in version R2000. */
+        char *dictionary_owner_soft;
+                /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
+                 * Group code = 330.\n
+                 * \since Introduced in version R14. */
+        char *material;
+                /*!< Hard-pointer ID/handle to material object (present if
+                 * not BYLAYER).\n
+                 * Group code = 347.\n
+                 * \since Introduced in version R2008. */
+        char *dictionary_owner_hard;
+                /*!< Hard owner ID/handle to owner dictionary (optional).\n
+                 * Group code = 360.\n
+                 * \since Introduced in version R14. */
+        int16_t lineweight;
+                /*!< Lineweight enum value.\n
+                 * Stored and moved around as a 16-bit integer.\n
+                 * Group code = 370.\n
+                 * \since Introduced in version R2002. */
+        char *plot_style_name;
+                /*!< Hard pointer ID / handle of PlotStyleName object.\n
+                 * Group code = 390.\n
+                 * \since Introduced in version R2009. */
+        long color_value;
+                /*!< A 24-bit color value that should be dealt with in
+                 * terms of bytes with values of 0 to 255.\n
+                 * The lowest byte is the blue value, the middle byte is
+                 * the green value, and the third byte is the red value.\n
+                 * The top byte is always 0.\n
+                 * The group code cannot be used by custom entities for
+                 * their own data because the group code is reserved for
+                 * AcDbEntity, class-level color data and AcDbEntity,
+                 * class-level transparency data.\n
+                 * Group code = 420.\n
+                 * \since Introduced in version R2004. */
+        char *color_name;
+                /*!< Color name.\n
+                 * The group code cannot be used by custom entities for
+                 * their own data because the group code is reserved for
+                 * AcDbEntity, class-level color data and AcDbEntity,
+                 * class-level transparency data.\n
+                 * Group code = 430.\n
+                 * \since Introduced in version R2004. */
+        long transparency;
+                /*!< Transparency value.\n
+                 * The group code cannot be used by custom entities for
+                 * their own data because the group code is reserved for
+                 * AcDbEntity, class-level color data and AcDbEntity,
+                 * class-level transparency data.\n
+                 * Group code = 440.\n
+                 * \since Introduced in version R2004. */
+        /* Specific members for a DXF sun. */
+        struct DxfSun *next;
+                /*!< pointer to the next DxfSun.\n
+                 * \c NULL in the last DxfSun. */
+
+} DxfSun;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* LIBDXF_SRC_SUN_H */
+
+
+/* EOF */
