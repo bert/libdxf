0d80bad17838f4b9bbeb2c02f041b07577ac8b05
bert <bert.timmerman@xs4all.nl>
2019-03-30 20:17:53 +0100

    SUN: added dxf_sun_get_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -313,4 +313,41 @@ dxf_sun_set_id_code
 }
 
 
+/*!
+ * \brief Get the \c linetype from a DXF \c SUN entity.
+ *
+ * \return \c linetype when successful, \c NULL when an error occurred.
+ */
+char *
+dxf_sun_get_linetype
+(
+        DxfSun *sun
+                /*!< a pointer to a DXF \c SUN entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (sun == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (sun->linetype ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (sun->linetype));
+}
+
+
 /* EOF */
--- src/sun.h
+++ src/sun.h
@@ -242,6 +242,7 @@ int dxf_sun_free (DxfSun *sun);
 void dxf_sun_free_list (DxfSun *suns);
 int dxf_sun_get_id_code (DxfSun *sun);
 DxfSun *dxf_sun_set_id_code (DxfSun *sun, int id_code);
+char *dxf_sun_get_linetype (DxfSun *sun);
 
 
 #endif /* LIBDXF_SRC_SUN_H */
