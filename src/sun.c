c6228fdc9ac6bf8524e8a3aadf78a6ed1b7a0ec6
bert <bert.timmerman@xs4all.nl>
2019-03-31 18:56:06 +0200

    SUN: added dxf_sun_set_thickness().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -572,4 +572,44 @@ dxf_sun_get_thickness
 }
 
 
+/*!
+ * \brief Set the \c thickness for a DXF \c SUN entity.
+ *
+ * \return a pointer to \c sun when successful, or \c NULL when an error
+ * occurred.
+ */
+DxfSun *
+dxf_sun_set_thickness
+(
+        DxfSun *sun,
+                /*!< a pointer to a DXF \c SUN entity. */
+        double thickness
+                /*!< the \c thickness to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (sun == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (thickness < 0.0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        sun->thickness = thickness;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (sun);
+}
+
+
 /* EOF */
