52b9a7728e76b705cd61283207111cc3d8bfc0e3
bert <bert.timmerman@xs4all.nl>
2019-04-01 20:08:52 +0200

    SUN: added dxf_sun_get_color().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -776,4 +776,40 @@ dxf_sun_set_visibility
 }
 
 
+/*!
+ * \brief Get the \c color from a DXF \c SUN entity.
+ *
+ * \return color.
+ */
+int
+dxf_sun_get_color
+(
+        DxfSun *sun
+                /*!< a pointer to a DXF \c SUN entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (sun == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (sun->color < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (sun->color);
+}
+
+
 /* EOF */
