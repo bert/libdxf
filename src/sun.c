7b12349ddd1a59cafc8ed19b69ed512c8931d02a
bert <bert.timmerman@xs4all.nl>
2019-04-02 20:02:20 +0200

    SUN: added dxf_sun_get_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -1203,4 +1203,44 @@ dxf_sun_set_binary_graphics_data
 }
 
 
+/*!
+ * \brief Get the pointer to the \c dictionary_owner_soft from a DXF 
+ * \c SUN entity.
+ *
+ * \return pointer to the \c dictionary_owner_soft.
+ *
+ * \warning No checks are performed on the returned pointer (string).
+ */
+char *
+dxf_sun_get_dictionary_owner_soft
+(
+        DxfSun *sun
+                /*!< a pointer to a DXF \c SUN entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (sun == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (sun->dictionary_owner_soft ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (sun->dictionary_owner_soft));
+}
+
+
 /* EOF */
--- src/sun.h
+++ src/sun.h
@@ -264,6 +264,7 @@ int16_t dxf_sun_get_shadow_mode (DxfSun *sun);
 DxfSun *dxf_sun_set_shadow_mode (DxfSun *sun, int16_t shadow_mode);
 DxfBinaryGraphicsData *dxf_sun_get_binary_graphics_data (DxfSun *sun);
 DxfSun *dxf_sun_set_binary_graphics_data (DxfSun *sun, DxfBinaryGraphicsData *data);
+char *dxf_sun_get_dictionary_owner_soft (DxfSun *sun);
 
 
 #endif /* LIBDXF_SRC_SUN_H */
