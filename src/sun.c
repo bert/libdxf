95336b7948022f03fd0f07c9b20c4eb9d4301ba9
bert <bert.timmerman@xs4all.nl>
2019-04-05 18:50:34 +0200

    SUN: added dxf_sun_set_shadow_type().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -2069,6 +2069,52 @@ dxf_sun_get_shadow_type
 
 
 /*!
+ * \brief Set the \c shadow_type for a DXF \c SUN entity.
+ *
+ * \return a pointer to \c sun when successful, or \c NULL when an error
+ * occurred.
+ */
+DxfSun *
+dxf_sun_set_shadow_type
+(
+        DxfSun *sun,
+                /*!< a pointer to a DXF \c SUN entity. */
+        int shadow_type
+                /*!< the \c shadow_type to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (sun == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (shadow_type < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        if (shadow_type > 1)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was passed.\n")),
+                  __FUNCTION__);
+        }
+        sun->shadow_type = shadow_type;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (sun);
+}
+
+
+/*!
  * \brief Get the \c shadow_map_size from a DXF \c SUN entity.
  *
  * \return \c shadow_map_size.
