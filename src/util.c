d5784e22c0d32259f0d5fecee65abf2063cd9e0d
bert <bert.timmerman@xs4all.nl>
2019-04-20 19:47:44 +0200

    UTIL: correction of an error message in dxf_int16_new().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/util.c
+++ src/util.c
@@ -520,7 +520,7 @@ dxf_int16_new ()
         if ((i = malloc (size)) == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () could not allocate memory for a DxfInt16 struct.\n")),
+                  (_("Error in %s () could not allocate memory.\n")),
                   __FUNCTION__);
                 i = NULL;
         }
