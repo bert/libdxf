1e9bf6e1ddda274d3574799f5b843250a90124ea
bert <bert.timmerman@xs4all.nl>
2019-04-20 19:48:53 +0200

    UTIL: correction of an error message in dxf_int16_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/util.c
+++ src/util.c
@@ -563,7 +563,7 @@ dxf_int16_init
         if (i == NULL)
         {
               fprintf (stderr,
-                (_("Error in %s () could not allocate memory for a DxfInt16 struct.\n")),
+                (_("Error in %s () could not allocate memory.\n")),
                 __FUNCTION__);
               return (NULL);
         }
