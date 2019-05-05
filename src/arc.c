9e16a7cd1f51e7f535d507df2e9c0ccb0a5e6080
bert <bert.timmerman@xs4all.nl>
2019-05-05 18:27:35 +0200

    ARC: added a flow diagram for dxf_arc_set_elevation().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -1136,6 +1136,9 @@ dxf_arc_get_elevation
 /*!
  * \brief Set the \c elevation for a DXF \c ARC entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_arc_set_elevation.png
+ *
  * \return a pointer to \c arc when successful, or \c NULL when an error
  * occurred.
  */
