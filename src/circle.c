d46cb5062a2c9bfe9f1440c7b98fa0d0ef37050e
bert <bert.timmerman@xs4all.nl>
2019-05-10 17:25:49 +0200

    CIRCLE: limit string length for fscanf in dxf_circle_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/circle.c
+++ src/circle.c
@@ -222,13 +222,13 @@ dxf_circle_read
                         /* Now follows a string containing a linetype
                          * name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->linetype);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->linetype);
                 }
                 else if (strcmp (temp_string, "8") == 0)
                 {
                         /* Now follows a string containing a layer name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->layer);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->layer);
                 }
                 else if (strcmp (temp_string, "10") == 0)
                 {
@@ -315,7 +315,7 @@ dxf_circle_read
                         /* Now follows a string containing the
                          * subclass marker value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if ((strcmp (temp_string, "AcDbEntity") != 0)
                         && (strcmp (temp_string, "AcDbCircle") != 0))
                         {
@@ -364,7 +364,7 @@ dxf_circle_read
                         /* Now follows a string containing binary
                          * graphics data. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", iter310->data_line);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, iter310->data_line);
                         dxf_binary_graphics_data_init ((DxfBinaryGraphicsData *) iter310->next);
                         iter310 = (DxfBinaryGraphicsData *) iter310->next;
                 }
@@ -375,14 +375,14 @@ dxf_circle_read
                                 /* Now follows a string containing a soft-pointer
                                  * ID/handle to owner dictionary. */
                                 (fp->line_number)++;
-                                fscanf (fp->fp, "%s\n", circle->dictionary_owner_soft);
+                                fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->dictionary_owner_soft);
                         }
                         if (iter330 == 1)
                         {
                                 /* Now follows a string containing a soft-pointer
                                  * ID/handle to owner object. */
                                 (fp->line_number)++;
-                                fscanf (fp->fp, "%s\n", circle->object_owner_soft);
+                                fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->object_owner_soft);
                         }
                         iter330++;
                 }
@@ -391,14 +391,14 @@ dxf_circle_read
                         /* Now follows a string containing a
                          * hard-pointer ID/handle to material object. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->material);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->material);
                 }
                 else if (strcmp (temp_string, "360") == 0)
                 {
                         /* Now follows a string containing Hard owner
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->dictionary_owner_hard);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->dictionary_owner_hard);
                 }
                 else if (strcmp (temp_string, "370") == 0)
                 {
@@ -412,7 +412,7 @@ dxf_circle_read
                         /* Now follows a string containing a plot style
                          * name value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->plot_style_name);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->plot_style_name);
                 }
                 else if (strcmp (temp_string, "420") == 0)
                 {
@@ -425,7 +425,7 @@ dxf_circle_read
                         /* Now follows a string containing a color
                          * name value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", circle->color_name);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, circle->color_name);
                 }
                 else if (strcmp (temp_string, "440") == 0)
                 {
@@ -438,7 +438,7 @@ dxf_circle_read
                 {
                         /* Now follows a string containing a comment. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         fprintf (stdout, "DXF comment: %s\n", temp_string);
                 }
                 else
