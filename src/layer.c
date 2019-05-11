61a65fc5ad403c7625859454b3ad285478a4a134
bert <bert.timmerman@xs4all.nl>
2019-05-11 10:13:47 +0200

    LAYER: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/layer.c
+++ src/layer.c
@@ -1,7 +1,7 @@
 /*!
  * \file layer.c
  *
- * \author Copyright (C) 2008, 2012, 2013, 2014, 2015, 2017, 2018
+ * \author Copyright (C) 2008, 2012, 2013, 2014, 2015, 2017, 2018, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF layer table (\c LAYER).
