9d6f40b4fd365da9d84447614f48dbe34be84b7f
bert <bert.timmerman@xs4all.nl>
2019-05-12 19:41:58 +0200

    STYLE: limit string length for fscanf in dxf_style_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/style.c
+++ src/style.c
@@ -207,21 +207,21 @@ dxf_style_read
                 {
                         /* Now follows a string containing a style name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", style->style_name);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, style->style_name);
                 }
                 else if (strcmp (temp_string, "3") == 0)
                 {
                         /* Now follows a string containing a primary
                          * font filename. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", style->primary_font_filename);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, style->primary_font_filename);
                 }
                 else if (strcmp (temp_string, "4") == 0)
                 {
                         /* Now follows a string containing a big font
                          * filename. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", style->big_font_filename);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, style->big_font_filename);
                 }
                 else if (strcmp (temp_string, "40") == 0)
                 {
@@ -271,7 +271,7 @@ dxf_style_read
                         /* Now follows a string containing the
                          * subclass marker value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if ((strcmp (temp_string, "AcDbSymbolTableRecord") != 0)
                         && ((strcmp (temp_string, "AcDbTextStyleTableRecord") != 0)))
                         {
@@ -285,20 +285,20 @@ dxf_style_read
                         /* Now follows a string containing Soft-pointer
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", style->dictionary_owner_soft);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, style->dictionary_owner_soft);
                 }
                 else if (strcmp (temp_string, "360") == 0)
                 {
                         /* Now follows a string containing Hard owner
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", style->dictionary_owner_hard);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, style->dictionary_owner_hard);
                 }
                 else if (strcmp (temp_string, "999") == 0)
                 {
                         /* Now follows a string containing a comment. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         fprintf (stdout, "DXF comment: %s\n", temp_string);
                 }
                 else if (strcmp (temp_string, "1071") == 0)
