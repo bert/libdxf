3a05a4297d3f8596d5cf45b2e13bb808f4303316
bert <bert.timmerman@xs4all.nl>
2019-05-12 16:54:45 +0200

    SEQEND: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/seqend.c
+++ src/seqend.c
@@ -1,7 +1,7 @@
 /*!
  * \file seqend.c
  *
- * \author Copyright (C) 2008, 2010, 2013, 2014, 2015, 2017
+ * \author Copyright (C) 2008, 2010, 2013, 2014, 2015, 2017, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
