0435afbdb9d1c9dbb63b9277bf764b065955853a
bert <bert.timmerman@xs4all.nl>
2019-05-12 15:32:19 +0200

    RASTERVARIABLES: limit string length for fscanf in dxf_rastervariables_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/rastervariables.c
+++ src/rastervariables.c
@@ -231,7 +231,7 @@ dxf_rastervariables_read
                         /* Now follows a string containing the
                          * subclass marker value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if (strcmp (temp_string, "AcDbIdBuffer") != 0)
                         {
                                 fprintf (stderr,
@@ -244,7 +244,7 @@ dxf_rastervariables_read
                         /* Now follows a string containing Soft-pointer
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", rastervariables->dictionary_owner_soft);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, rastervariables->dictionary_owner_soft);
                         i++;
                 }
                 else if (strcmp (temp_string, "360") == 0)
@@ -252,13 +252,13 @@ dxf_rastervariables_read
                         /* Now follows a string containing Hard owner
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", rastervariables->dictionary_owner_hard);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, rastervariables->dictionary_owner_hard);
                 }
                 else if (strcmp (temp_string, "999") == 0)
                 {
                         /* Now follows a string containing a comment. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         fprintf (stdout, (_("DXF comment: %s\n")), temp_string);
                 }
                 else
