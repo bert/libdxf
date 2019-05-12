145310f9f803abedad07ef6bfdece8415525b5e9
bert <bert.timmerman@xs4all.nl>
2019-05-12 20:21:37 +0200

    TRACE: limit string length for fscanf in dxf_trace_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/trace.c
+++ src/trace.c
@@ -224,13 +224,13 @@ dxf_trace_read
                         /* Now follows a string containing a linetype
                          * name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->linetype);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->linetype);
                 }
                 else if (strcmp (temp_string, "8") == 0)
                 {
                         /* Now follows a string containing a layer name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->layer);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->layer);
                 }
                 else if (strcmp (temp_string, "10") == 0)
                 {
@@ -373,7 +373,7 @@ dxf_trace_read
                         /* Now follows a string containing the
                          * subclass marker value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if ((strcmp (temp_string, "AcDbEntity") != 0)
                         && ((strcmp (temp_string, "AcDbTrace") != 0)))
                         {
@@ -422,7 +422,7 @@ dxf_trace_read
                         /* Now follows a string containing binary
                          * graphics data. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->binary_graphics_data->data_line);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->binary_graphics_data->data_line);
                         dxf_binary_graphics_data_init ((DxfBinaryGraphicsData *) trace->binary_graphics_data->next);
                         trace->binary_graphics_data = (DxfBinaryGraphicsData *) trace->binary_graphics_data->next;
                 }
@@ -431,21 +431,21 @@ dxf_trace_read
                         /* Now follows a string containing Soft-pointer
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->dictionary_owner_soft);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->dictionary_owner_soft);
                 }
                 else if (strcmp (temp_string, "347") == 0)
                 {
                         /* Now follows a string containing a
                          * hard-pointer ID/handle to material object. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->material);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->material);
                 }
                 else if (strcmp (temp_string, "360") == 0)
                 {
                         /* Now follows a string containing Hard owner
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->dictionary_owner_hard);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->dictionary_owner_hard);
                 }
                 else if (strcmp (temp_string, "370") == 0)
                 {
@@ -459,7 +459,7 @@ dxf_trace_read
                         /* Now follows a string containing a plot style
                          * name value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->plot_style_name);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->plot_style_name);
                 }
                 else if (strcmp (temp_string, "420") == 0)
                 {
@@ -472,7 +472,7 @@ dxf_trace_read
                         /* Now follows a string containing a color
                          * name value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", trace->color_name);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, trace->color_name);
                 }
                 else if (strcmp (temp_string, "440") == 0)
                 {
@@ -485,7 +485,7 @@ dxf_trace_read
                 {
                         /* Now follows a string containing a comment. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         fprintf (stdout, "DXF comment: %s\n", temp_string);
                 }
                 else
