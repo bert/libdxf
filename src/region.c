8c384d6f27b1bed07ac674ec85aa4ac53be7c94b
bert <bert.timmerman@xs4all.nl>
2019-05-12 16:48:11 +0200

    REGION: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/region.c
+++ src/region.c
@@ -1,7 +1,7 @@
 /*!
  * \file region.c
  *
- * \author Copyright (C) 2013, 2014, 2015, 2017, 2018
+ * \author Copyright (C) 2013, 2014, 2015, 2017, 2018, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF region entity (\c REGION).
