d7adf92da5d2996b70155124da4fe605ef134241
bert <bert.timmerman@xs4all.nl>
2019-05-12 19:07:49 +0200

    SORTENTSTABLE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sortentstable.c
+++ src/sortentstable.c
@@ -1,7 +1,7 @@
 /*!
  * \file sortentstable.c
  *
- * \author Copyright (C) 2015, 2017, 2018
+ * \author Copyright (C) 2015, 2017, 2018, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF sortentstable object (\c SORTENTSTABLE).
