3b819e1d96ab2fe32f9dc55b6cdc1a2f3ce2ab67
bert <bert.timmerman@xs4all.nl>
2019-05-12 15:48:36 +0200

    RAY: bumped the coyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ray.c
+++ src/ray.c
@@ -1,7 +1,7 @@
 /*!
  * \file ray.c
  *
- * \author Copyright (C) 2013, 2014, 2015, 2017, 2018
+ * \author Copyright (C) 2013, 2014, 2015, 2017, 2018, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF ray entity (\c RAY).
