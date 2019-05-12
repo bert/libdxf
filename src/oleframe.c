6fc72f5db5057e59f952a01f290b8a6cc532334b
bert <bert.timmerman@xs4all.nl>
2019-05-12 14:10:09 +0200

    OLEFRAME: limit string length for fscanf in dxf_oleframe_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/oleframe.c
+++ src/oleframe.c
@@ -198,7 +198,7 @@ dxf_oleframe_read
                         /* Now follows a string containing a End of Ole
                          * data marker. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if (strcmp (temp_string, "OLE") != 0)
                         {
                                 fprintf (stderr,
@@ -218,13 +218,13 @@ dxf_oleframe_read
                         /* Now follows a string containing a linetype
                          * name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", oleframe->linetype);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, oleframe->linetype);
                 }
                 else if (strcmp (temp_string, "8") == 0)
                 {
                         /* Now follows a string containing a layer name. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", oleframe->layer);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, oleframe->layer);
                 }
                 else if ((fp->acad_version_number <= AutoCAD_11)
                         && (strcmp (temp_string, "38") == 0)
@@ -290,7 +290,7 @@ dxf_oleframe_read
                         /* Now follows a string containing the
                          * subclass marker value. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         if ((strcmp (temp_string, "AcDbEntity") != 0)
                         && ((strcmp (temp_string, "AcDbOleFrame") != 0)))
                         {
@@ -303,7 +303,7 @@ dxf_oleframe_read
                 {
                         /* Now follows a string containing binary data. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", iter->value);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, iter->value);
                         iter->next = (struct DxfChar *) dxf_char_init ((DxfChar *) iter->next);
                         iter = (DxfChar *) iter->next;
                 }
@@ -312,20 +312,20 @@ dxf_oleframe_read
                         /* Now follows a string containing Soft-pointer
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", oleframe->dictionary_owner_soft);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, oleframe->dictionary_owner_soft);
                 }
                 else if (strcmp (temp_string, "360") == 0)
                 {
                         /* Now follows a string containing Hard owner
                          * ID/handle to owner dictionary. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", oleframe->dictionary_owner_hard);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, oleframe->dictionary_owner_hard);
                 }
                 else if (strcmp (temp_string, "999") == 0)
                 {
                         /* Now follows a string containing a comment. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%s\n", temp_string);
+                        fscanf (fp->fp, DXF_MAX_STRING_FORMAT, temp_string);
                         fprintf (stdout, "DXF comment: %s\n", temp_string);
                 }
                 else
