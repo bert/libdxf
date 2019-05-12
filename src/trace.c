0cd50df2cfad0bcdec24f9c5b46b2fcd61ed92f8
bert <bert.timmerman@xs4all.nl>
2019-05-12 20:20:31 +0200

    TRACE: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/trace.c
+++ src/trace.c
@@ -1,7 +1,7 @@
 /*!
  * \file trace.c
  *
- * \author Copyright (C) 2008, 2012, 2014, 2015, 2017, 2018
+ * \author Copyright (C) 2008, 2012, 2014, 2015, 2017, 2018, 2019
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
