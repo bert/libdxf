239beeb25e3f2a7af3e19d36c8cf4e1a8cbc5e65
bert <bert.timmerman@xs4all.nl>
2019-05-12 12:33:39 +0200

    LWPOLYLINE: removed obsolete code in dxf_lwpolyline_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/lwpolyline.c
+++ src/lwpolyline.c
@@ -182,7 +182,6 @@ dxf_lwpolyline_read
                 fprintf (stderr,
                   (_("Warning in %s () a NULL pointer was passed.\n")),
                   __FUNCTION__);
-                lwpolyline = dxf_lwpolyline_new ();
                 lwpolyline = dxf_lwpolyline_init (lwpolyline);
         }
         iter = (DxfVertex *) lwpolyline->vertices;
