5eeecee3ce91dbfb5925a54fbea7f66577fc0906
bert <bert.timmerman@xs4all.nl>
2019-05-13 22:03:40 +0200

    ARC: added a flow diagram for dxf_arc_set_thickness().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -1224,6 +1224,9 @@ dxf_arc_get_thickness
 /*!
  * \brief Set the \c thickness for a DXF \c ARC entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_arc_set_thickness.png
+ *
  * \return a pointer to \c arc when successful, or \c NULL when an error
  * occurred.
  */
