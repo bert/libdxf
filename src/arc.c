b98aa7cfa6edd2df0beff85f8872083dfe0856e5
bert <bert.timmerman@xs4all.nl>
2019-05-14 20:23:37 +0200

    ARC: correction in dxf_arc_get_color_value().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -2410,7 +2410,7 @@ dxf_arc_set_plot_style_name
  * \brief Get the \c color_value from a DXF \c ARC entity.
  *
  * <b>Flow diagram:</b>
- * \image html drakon/dxf_arc_color_value.png
+ * \image html drakon/dxf_arc_get_color_value.png
  *
  * \return \c color_value when successful, or \c EXIT_FAILURE when an
  * error occurred.
