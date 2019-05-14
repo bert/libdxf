f1c126ff87b8b3d4fa4d180cbc684a40f2e59702
bert <bert.timmerman@xs4all.nl>
2019-05-14 23:07:10 +0200

    ARC: added flow diagram for dxf_arc_set_transparency().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -2600,6 +2600,9 @@ dxf_arc_get_transparency
 /*!
  * \brief Set the \c transparency for a DXF \c ARC entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_arc_set_transparency.png
+ *
  * \return a pointer to \c arc when successful, or \c NULL when an error
  * occurred.
  */
