260e8145c2d906cd7a5c70624ac05634ff111013
bert <bert.timmerman@xs4all.nl>
2019-05-15 22:25:19 +0200

    ARC: added a flow diagram for dxf_arc_set_start_angle().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -3112,6 +3112,9 @@ dxf_arc_get_start_angle
 /*!
  * \brief Set the \c start_angle for a DXF \c ARC entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_arc_set_start_angle.png
+ *
  * \return a pointer to \c arc when successful, or \c NULL when an error
  * occurred.
  */
