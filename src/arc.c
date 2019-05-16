40344be770c9b7fb48685c10e95770556d4b0de9
bert <bert.timmerman@xs4all.nl>
2019-05-16 23:13:03 +0200

    ARC: added a flow diagram for dxf_arc_set_extr_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -3498,6 +3498,9 @@ dxf_arc_get_extr_z0
  * \brief Set the Z-value of the extrusion vector \c extr_z0 of a DXF
  * \c ARC entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_arc_set_extr_z0.png
+ *
  * \return a pointer to \c arc when successful, or \c NULL when an error
  * occurred.
  */
