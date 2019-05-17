b748d9a2a6a45c844e94d2572a8d45fe4ec44c46
bert <bert.timmerman@xs4all.nl>
2019-05-17 21:48:30 +0200

    CIRCLE: added a cast in dxf_circle_write().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/circle.c
+++ src/circle.c
@@ -575,7 +575,7 @@ dxf_circle_write
         }
         if (circle->paperspace == DXF_PAPERSPACE)
         {
-                fprintf (fp->fp, " 67\n%hd\n", DXF_PAPERSPACE);
+                fprintf (fp->fp, " 67\n%hd\n", (int16_t) DXF_PAPERSPACE);
         }
         fprintf (fp->fp, "  8\n%s\n", circle->layer);
         if (strcmp (circle->linetype, DXF_DEFAULT_LINETYPE) != 0)
