30ce6821e99cafabf94124472303867ca518bdec
bert <bert.timmerman@xs4all.nl>
2019-05-18 10:03:30 +0200

    SUN: added a cast in dxf_sun_write().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/sun.c
+++ src/sun.c
@@ -603,7 +603,7 @@ dxf_sun_write
         }
         if (sun->paperspace == DXF_PAPERSPACE)
         {
-                fprintf (fp->fp, " 67\n%d\n", DXF_PAPERSPACE);
+                fprintf (fp->fp, " 67\n%d\n", (int16_t) DXF_PAPERSPACE);
         }
         fprintf (fp->fp, "  8\n%s\n", sun->layer);
         if (strcmp (sun->linetype, DXF_DEFAULT_LINETYPE) != 0)
