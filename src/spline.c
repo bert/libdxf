f77cc9c9191ae4dc3f5ecc6d3807940f705a445c
bert <bert.timmerman@xs4all.nl>
2019-05-18 10:02:19 +0200

    SPLINE: added a cast in dxf_spline_write().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/spline.c
+++ src/spline.c
@@ -874,7 +874,7 @@ dxf_spline_write
         }
         if (spline->paperspace == DXF_PAPERSPACE)
         {
-                fprintf (fp->fp, " 67\n%hd\n", DXF_PAPERSPACE);
+                fprintf (fp->fp, " 67\n%hd\n", (int16_t) DXF_PAPERSPACE);
         }
         fprintf (fp->fp, "  8\n%s\n", spline->layer);
         if (strcmp (spline->linetype, DXF_DEFAULT_LINETYPE) != 0)
