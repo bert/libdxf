c5723538e54583b74b490fc46018619542db8168
bert <bert.timmerman@xs4all.nl>
2019-05-23 09:38:41 +0200

    CIRCLE: added a flow diagram for dxf_circle_get_object_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/circle.c
+++ src/circle.c
@@ -1862,6 +1862,9 @@ dxf_circle_set_dictionary_owner_soft
  * \brief Get the soft pointer to the object owner from a DXF 
  * \c CIRCLE entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_circle_get_object_owner_soft.png
+ *
  * \return soft pointer to the object owner.
  *
  * \warning No checks are performed on the returned pointer (string).
