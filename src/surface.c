95634a7d561f42a2eb6293dda0e6f398f95d5fcd
bert <bert.timmerman@xs4all.nl>
2019-05-26 22:01:09 +0200

    SURFACE: added new files.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/surface.c
@@ -0,0 +1,84 @@
+/*!
+ * \file suurface.c
+ *
+ * \author Copyright (C) 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Functions for a DXF surface entity (\c SURFACE).
+ *
+ * \version According to DXF R10.
+ * \version According to DXF R11.
+ * \version According to DXF R12.
+ * \version According to DXF R13.
+ * \version According to DXF R14.
+ *
+ * \since The \c SURFACE entity was introduced in DXF R2007.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com .
+ * <hr>
+ */
+
+
+#include "surface.h"
+
+
+/*!
+ * \brief Allocate memory for a DXF \c SURFACE entity.
+ *
+ * Fill the memory contents with zeros.
+ *
+ * \return \c NULL when no memory was allocated, a pointer to the
+ * allocated memory when successful.
+ */
+DxfSurface *
+dxf_surface_new ()
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        DxfSurface *surface = NULL;
+        size_t size;
+
+        size = sizeof (DxfSurface);
+        /* avoid malloc of 0 bytes */
+        if (size == 0) size = 1;
+        if ((surafce = malloc (size)) == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () could not allocate memory.\n")),
+                  __FUNCTION__);
+                surface = NULL;
+        }
+        else
+        {
+                memset (surface, 0, size);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (surface);
+}
+
+
+/* EOF */
