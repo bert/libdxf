e7840b64e1bb745bba75c1da5f4884862dc3133e
bert <bert.timmerman@xs4all.nl>
2019-06-05 21:09:14 +0200

    SURFACE: added dxf_surface_get_modeler_format_version_number().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -1923,4 +1923,42 @@ dxf_surface_set_additional_proprietary_data
 }
 
 
+/*!
+ * \brief Get the modeler format version number from a DXF \c SURFACE
+ * entity.
+ *
+ * \return \c modeler_format_version_number.
+ */
+int16_t
+dxf_surface_get_modeler_format_version_number
+(
+        DxfSurface *surface
+                /*!< a pointer to a DXF \c SURFACE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (surface->modeler_format_version_number != 1)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () an invalid value was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (surface->modeler_format_version_number);
+}
+
+
 /* EOF */
--- src/surface.h
+++ src/surface.h
@@ -279,6 +279,7 @@ DxfProprietaryData *dxf_surface_get_proprietary_data (DxfSurface *surface);
 DxfSurface *dxf_surface_set_proprietary_data (DxfSurface *surface, DxfProprietaryData *proprietary_data);
 DxfProprietaryData *dxf_surface_get_additional_proprietary_data (DxfSurface *surface);
 DxfSurface *dxf_surface_set_additional_proprietary_data (DxfSurface *surface, DxfProprietaryData *additional_proprietary_data);
+int16_t dxf_surface_get_modeler_format_version_number (DxfSurface *surface);
 
 
 #ifdef __cplusplus
