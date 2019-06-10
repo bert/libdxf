bdec814a14ad4d7bedff61d5fb7f6ecdcd36396a
bert <bert.timmerman@xs4all.nl>
2019-06-10 23:16:47 +0200

    Added binary_data.[ch]
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

---
+++ src/binary_data.c
@@ -0,0 +1,505 @@
+/*!
+ * \file binary_data.c
+ *
+ * \author Copyright (C) 2019
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Functions for a DXF binary data object.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#include "binary_data.h"
+
+
+/*!
+ * \brief Allocate memory for a binary data object.
+ *
+ * Fill the memory contents with zeros.
+ */
+DxfBinaryData *
+dxf_binary_data_new ()
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        DxfBinaryData *data = NULL;
+        size_t size;
+
+        size = sizeof (DxfBinaryData);
+        /* avoid malloc of 0 bytes */
+        if (size == 0) size = 1;
+        if ((data = malloc (size)) == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () could not allocate memory.\n")),
+                  __FUNCTION__);
+                data = NULL;
+        }
+        else
+        {
+                memset (data, 0, size);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
+/*!
+ * \brief Allocate memory and initialize data fields in a binary data
+ * entity.
+ * 
+ * \return \c NULL when no memory was allocated, a pointer to the
+ * allocated memory when successful.
+ */
+DxfBinaryData *
+dxf_binary_data_init
+(
+        DxfBinaryData *data
+                /*!< a pointer to the binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                data = dxf_binary_data_new ();
+        }
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () could not allocate memory.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->data_line = strdup ("");
+        data->length = 0;
+        data->next = NULL;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
+/*!
+ * \brief Write DXF output to fp for a binary data object.
+ *
+ * \return \c EXIT_SUCCESS when done, or \c EXIT_FAILURE when an error
+ * occurred.
+ */
+int
+dxf_binary_data_write
+(
+        DxfFile *fp,
+                /*!< file pointer to output file (or device). */
+        DxfBinaryData *data
+                /*!< a pointer to the binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (fp == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        /* Start writing output. */
+        fprintf (fp->fp, "310\n%s\n", data->data_line);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (EXIT_SUCCESS);
+}
+
+
+/*!
+ * \brief Free the allocated memory for a binary data object
+ * and all it's data fields.
+ *
+ * \return \c EXIT_SUCCESS when done, or \c EXIT_FAILURE when an error
+ * occurred.
+ */
+int
+dxf_binary_data_free
+(
+        DxfBinaryData *data
+                /*!< Pointer to the memory occupied by the binary data
+                 * entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (data->next != NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        free (data->data_line);
+        free (data);
+        data = NULL;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (EXIT_SUCCESS);
+}
+
+
+/*!
+ * \brief Free the allocated memory for a single linked list of binary
+ * data objects and all their data fields.
+ */
+void
+dxf_binary_data_free_list
+(
+        DxfBinaryData *data
+                /*!< pointer to the single linked list of DXF binary
+                 * data objects. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+        }
+        while (data != NULL)
+        {
+                DxfBinaryData *iter = (DxfBinaryData *) data->next;
+                dxf_binary_data_free (data);
+                data = (DxfBinaryData *) iter;
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+}
+
+
+/*!
+ * \brief Get the data_line from a binary data object.
+ *
+ * \return \c data_line when sucessful, \c NULL when an error occurred.
+ */
+char *
+dxf_binary_data_get_data_line
+(
+        DxfBinaryData *data
+                /*!< a pointer to a binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data->data_line ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (strdup (data->data_line));
+}
+
+
+/*!
+ * \brief Set the \c data_line for a binary data object.
+ */
+DxfBinaryData *
+dxf_binary_data_set_data_line
+(
+        DxfBinaryData *data,
+                /*!< a pointer to a binary data object. */
+        char *data_line
+                /*!< a string containing the \c data_line for the
+                 * entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data_line == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->data_line = strdup (data_line);
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
+/*!
+ * \brief Get the \c length from a binary data object.
+ *
+ * \return \c length.
+ */
+int
+dxf_binary_data_get_length
+(
+        DxfBinaryData *data
+                /*!< a pointer to a binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (data->length < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was found.\n")),
+                  __FUNCTION__);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data->length);
+}
+
+
+/*!
+ * \brief Set the \c length for a binary data object.
+ *
+ * \return a pointer to \c data when sucessful, \c NULL when an error
+ * occurred.
+ */
+DxfBinaryData *
+dxf_binary_data_set_length
+(
+        DxfBinaryData *data,
+                /*!< a pointer to a binary data object. */
+        int length
+                /*!< Length to be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (length < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        data->length = length;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
+/*!
+ * \brief Get the pointer to the next binary data object from a
+ * binary data object.
+ *
+ * \return pointer to the \c next binary data object.
+ *
+ * \warning No checks are performed on the returned pointer.
+ */
+DxfBinaryData *
+dxf_binary_data_get_next
+(
+        DxfBinaryData *data
+                /*!< a pointer to a binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data->next == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return ((DxfBinaryData *) data->next);
+}
+
+
+/*!
+ * \brief Set the pointer to the \c next binary data for a binary
+ * entity data entity.
+ */
+DxfBinaryData *
+dxf_binary_data_set_next
+(
+        DxfBinaryData *data,
+                /*!< a pointer to a binary data object. */
+        DxfBinaryData *next
+                /*!< a pointer to the next binary data for the
+                 * entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (next == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        data->next = (struct DxfBinaryData *) next;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (data);
+}
+
+
+/*!
+ * \brief Get the pointer to the last binary data object from a
+ * linked list of binary data objects.
+ *
+ * \return pointer to the \c last binary data object.
+ *
+ * \warning No checks are performed on the returned pointer.
+ */
+DxfBinaryData *
+dxf_binary_data_get_last
+(
+        DxfBinaryData *data
+                /*!< a pointer to a binary data object. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (data->next == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return ((DxfBinaryData *) data);
+        }
+        DxfBinaryData *iter = (DxfBinaryData *) data->next;
+        while (iter->next != NULL)
+        {
+                iter = (DxfBinaryData *) iter->next;
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return ((DxfBinaryData *) data->next);
+}
+
+
+/* EOF */
---
+++ src/binary_data.h
@@ -0,0 +1,89 @@
+/*!
+ * \file binary_data.h
+ *
+ * \author Copyright (C) 2019
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ *
+ * \brief Header file for a DXF binary data object.
+ *
+ * <hr>
+ * <h1><b>Copyright Notices.</b></h1>\n
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.\n\n
+ * This program is distributed in the hope that it will be useful, but
+ * <b>WITHOUT ANY WARRANTY</b>; without even the implied warranty of
+ * <b>MERCHANTABILITY</b> or <b>FITNESS FOR A PARTICULAR PURPOSE</b>.\n
+ * See the GNU General Public License for more details.\n\n
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to:\n
+ * Free Software Foundation, Inc.,\n
+ * 59 Temple Place,\n
+ * Suite 330,\n
+ * Boston,\n
+ * MA 02111 USA.\n
+ * \n
+ * Drawing eXchange Format (DXF) is a defacto industry standard for the
+ * exchange of drawing files between various Computer Aided Drafting
+ * programs.\n
+ * DXF is an industry standard designed by Autodesk(TM).\n
+ * For more details see http://www.autodesk.com.
+ * <hr>
+ */
+
+
+#ifndef LIBDXF_SRC_BINARY_DATA_H
+#define LIBDXF_SRC_BINARY_DATA_H
+
+
+#include "global.h"
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/*!
+ * \brief DXF definition of an AutoCAD binary data object.
+ */
+typedef struct
+dxf_binary_data_struct
+{
+        char *data_line;
+                /*!< Binary entity data.\n
+                 * Multiple lines of 256 characters maximum per line
+                 * (optional).\n
+                 * Group code = 310. */
+        int length;
+                /*!< Length of the \c data_line member. */
+        struct DxfBinaryData *next;
+                /*!< Pointer to the next DxfBinaryData.\n
+                 * \c NULL if the last DxfBinaryData. */
+} DxfBinaryData;
+
+
+DxfBinaryData *dxf_binary_data_new ();
+DxfBinaryData *dxf_binary_data_init (DxfBinaryData *data);
+int dxf_binary_data_write (DxfFile *fp, DxfBinaryData *data);
+int dxf_binary_data_free (DxfBinaryData *data);
+void dxf_binary_data_free_list (DxfBinaryData *data);
+char *dxf_binary_data_get_data_line (DxfBinaryData *data);
+DxfBinaryData *dxf_binary_data_set_data_line (DxfBinaryData *data, char *data_line);
+int dxf_binary_data_get_length (DxfBinaryData *data);
+DxfBinaryData *dxf_binary_data_set_length (DxfBinaryData *data, int length);
+DxfBinaryData *dxf_binary_data_get_next (DxfBinaryData *data);
+DxfBinaryData *dxf_binary_data_set_next (DxfBinaryData *data, DxfBinaryData *next);
+DxfBinaryData *dxf_binary_data_get_last (DxfBinaryData *data);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* LIBDXF_SRC_BINARY_DATA_H */
+
+
+/* EOF */
