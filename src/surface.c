d006eea891d85adf56108c4e61cf9d69cd83d5c0
bert <bert.timmerman@xs4all.nl>
2019-06-22 21:57:28 +0200

    SURFACE: added dxf_surface_extruded_get_transform_matrix().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -3351,6 +3351,45 @@ dxf_surface_extruded_set_z1
 
 
 /*!
+ * \brief Get the transform matrix of extruded entity
+ * \c transform_matrix of a DXF extruded \c SURFACE entity.
+ *
+ * \return the transform matrix of extruded entity \c transform_matrix.
+ */
+DxfDouble *
+dxf_surface_extruded_get_transform_matrix
+(
+        DxfSurfaceExtruded *extruded_surface
+                /*!< a pointer to a DXF extruded \c SURFACE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (extruded_surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (extruded_surface->transform_matrix == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return ((DxfDouble *) extruded_surface->transform_matrix);
+}
+
+
+/*!
  * \brief Get the first value of the transform matrix of extruded entity
  * \c transform_matrix of a DXF extruded \c SURFACE entity.
  *
