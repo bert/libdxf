fecb973242cd51108544fa34f7b60d35b2bd5ef2
bert <bert.timmerman@xs4all.nl>
2019-06-22 22:02:28 +0200

    SURFACE: added dxf_surface_extruded_set_transform_matrix().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -3390,6 +3390,49 @@ dxf_surface_extruded_get_transform_matrix
 
 
 /*!
+ * \brief Set the transform matrix \c transform_matrix of a DXF extruded
+ * \c SURFACE entity.
+ *
+ * \return a pointer to \c extruded_surface when successful, or \c NULL
+ * when an error occurred.
+ */
+DxfSurfaceExtruded *
+dxf_surface_extruded_set_transform_matrix
+(
+        DxfSurfaceExtruded *extruded_surface,
+                /*!< a pointer to a DXF extruded \c SURFACE entity. */
+        DxfDouble transform_matrix
+                /*!< the transform matrix \c transform_matrix of a DXF
+                 * extruded \c SURFACE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (extruded_surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (extruded_surface->transform_matrix == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        extruded_surface->transform_matrix = transform_matrix;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (extruded_surface);
+}
+
+
+/*!
  * \brief Get the first value of the transform matrix of extruded entity
  * \c transform_matrix of a DXF extruded \c SURFACE entity.
  *
