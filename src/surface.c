f49a97ebab59a1286724250fa339337c93c1a4b5
bert <bert.timmerman@xs4all.nl>
2019-08-17 22:08:16 +0200

    SURFACE: added dxf_surface_swept_set_transform_sweep_matrix2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -8945,4 +8945,48 @@ dxf_surface_swept_get_transform_sweep_matrix2
 }
 
 
+/*!
+ * \brief Set the second transform sweep matrix
+ * \c transform_sweep_matrix2 of a DXF swept \c SURFACE entity.
+ *
+ * \return a pointer to \c swept_surface when successful, or \c NULL
+ * when an error occurred.
+ */
+DxfSurfaceSwept *
+dxf_surface_swept_set_transform_sweep_matrix2
+(
+        DxfSurfaceSwept *swept_surface,
+                /*!< a pointer to a DXF swept \c SURFACE entity. */
+        DxfDouble *transform_sweep_matrix2
+                /*!< the second transform sweep matrix
+                 * \c transform_sweep_matrix2 of a DXF swept \c SURFACE
+                 * entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (swept_surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (transform_sweep_matrix2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        swept_surface->transform_sweep_matrix2 = (DxfDouble *) transform_sweep_matrix2;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (swept_surface);
+}
+
+
 /* EOF */
--- src/surface.h
+++ src/surface.h
@@ -769,6 +769,7 @@ DxfSurfaceSwept *dxf_surface_swept_set_end_draft_distance (DxfSurfaceSwept *swep
 double dxf_surface_swept_get_twist_angle (DxfSurfaceSwept *swept_surface);
 DxfSurfaceSwept *dxf_surface_swept_set_twist_angle (DxfSurfaceSwept *swept_surface, double twist_angle);
 DxfDouble *dxf_surface_swept_get_transform_sweep_matrix2 (DxfSurfaceSwept *swept_surface);
+DxfSurfaceSwept *dxf_surface_swept_set_transform_sweep_matrix2 (DxfSurfaceSwept *swept_surface, DxfDouble *transform_sweep_matrix2);
 
 
 #ifdef __cplusplus
