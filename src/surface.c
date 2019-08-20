54fb7f1d86381a2845b82a74eaeda2651dc24b01
bert <bert.timmerman@xs4all.nl>
2019-08-20 19:08:19 +0200

    SURFACE: added dxf_surface_swept_set_scale_factor().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -9104,4 +9104,40 @@ dxf_surface_swept_get_scale_factor
 }
 
 
+/*!
+ * \brief Set the scale factor \c scale_factor of a DXF swept \c SURFACE
+ * entity.
+ *
+ * \return a pointer to \c swept_surface when successful, or \c NULL
+ * when an error occurred.
+ */
+DxfSurfaceSwept *
+dxf_surface_swept_set_scale_factor
+(
+        DxfSurfaceSwept *swept_surface,
+                /*!< a pointer to a DXF swept \c SURFACE entity. */
+        double scale_factor
+                /*!< the scale factor \c scale_factor of a DXF swept
+                 * \c SURFACE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (swept_surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        swept_surface->scale_factor = scale_factor;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (swept_surface);
+}
+
+
 /* EOF */
