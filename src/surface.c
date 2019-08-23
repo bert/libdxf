3d54178b2f03aa684bfca0239ee1c2243d15279c
bert <bert.timmerman@xs4all.nl>
2019-08-23 22:06:40 +0200

    SURFACE: added dxf_surface_swept_get_bank_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -9557,4 +9557,34 @@ dxf_surface_swept_set_align_start_flag
 }
 
 
+/*!
+ * \brief Get the \c bank_flag from a DXF swept \c SURFACE entity.
+ *
+ * \return \c bank_flag.
+ */
+int
+dxf_surface_swept_get_bank_flag
+(
+        DxfSurfaceSwept *swept_surface
+                /*!< a pointer to a DXF swept \c SURFACE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (swept_surface == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (swept_surface->bank_flag);
+}
+
+
 /* EOF */
--- src/surface.h
+++ src/surface.h
@@ -786,6 +786,7 @@ int dxf_surface_swept_get_solid_flag (DxfSurfaceSwept *swept_surface);
 DxfSurfaceSwept *dxf_surface_swept_set_solid_flag (DxfSurfaceSwept *swept_surface, int solid_flag);
 int dxf_surface_swept_get_align_start_flag (DxfSurfaceSwept *swept_surface);
 DxfSurfaceSwept *dxf_surface_swept_set_align_start_flag (DxfSurfaceSwept *swept_surface, int align_start_flag);
+int dxf_surface_swept_get_bank_flag (DxfSurfaceSwept *swept_surface);
 
 
 #ifdef __cplusplus
