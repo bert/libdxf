b9817b4d5743d31816cdba4317fe4978c131f2e7
bert <bert.timmerman@xs4all.nl>
2019-12-04 21:37:04 +0100

    HATCH: correction in dxf_hatch_set_graphics_data_size().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -1230,16 +1230,14 @@ dxf_hatch_set_graphics_data_size
         if (graphics_data_size < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative graphics_data_size value was passed.\n")),
+                  (_("Warning in %s () a negative graphics_data_size value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         if (graphics_data_size == 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a zero graphics_data_size value was passed.\n")),
+                  (_("Warning in %s () a zero graphics_data_size value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         hatch->graphics_data_size = graphics_data_size;
 #if DEBUG
