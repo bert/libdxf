abb14f7913365521d642cd636d4d59d395b26729
bert <bert.timmerman@xs4all.nl>
2019-12-04 21:23:01 +0100

    HATCH: correction in dxf_hatch_set_linetype_scale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -905,9 +905,8 @@ dxf_hatch_set_linetype_scale
         if (linetype_scale < 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative linetype scale value was passed.\n")),
+                  (_("Warning in %s () a negative linetype scale value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         hatch->linetype_scale = linetype_scale;
 #if DEBUG
