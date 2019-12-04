0db5cb09fbe5d6742561f7f3d09a81acf3306558
bert <bert.timmerman@xs4all.nl>
2019-12-04 21:40:52 +0100

    HATCH: correction in an error message in dxf_hatch_get_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -1356,7 +1356,7 @@ dxf_hatch_get_dictionary_owner_soft
         if (hatch->dictionary_owner_soft ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_soft member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
