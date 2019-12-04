38852d2ebf26dab3179bef0904241f3113e947e0
bert <bert.timmerman@xs4all.nl>
2019-12-04 21:33:39 +0100

    HATCH: correction in dxf_hatch_set_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -984,16 +984,14 @@ dxf_hatch_set_visibility
         if (visibility < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative visibility value was passed.\n")),
+                  (_("Warning in %s () a negative visibility value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         if (visibility > 1)
         {
                 fprintf (stderr,
-                  (_("Error in %s () an out of range visibility value was passed.\n")),
+                  (_("Warning in %s () an out of range visibility value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         hatch->visibility = visibility;
 #if DEBUG
