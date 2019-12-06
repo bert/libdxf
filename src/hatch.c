87378a83ea8b9577bbb82d24df423747568f2760
bert <bert.timmerman@xs4all.nl>
2019-12-06 23:37:37 +0100

    HATCH: correction in an error message in dxf_hatch_get_last().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -3435,7 +3435,7 @@ dxf_hatch_get_last
         if (hatch->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return ((DxfHatch *) hatch);
         }
