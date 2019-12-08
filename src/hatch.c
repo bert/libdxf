f4516fd975c124035443d0584e2cfc492c7301be
bert <bert.timmerman@xs4all.nl>
2019-12-08 23:05:04 +0100

    HATCH: corrections in dxf_hatch_pattern_def_line_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -4712,9 +4712,8 @@ dxf_hatch_pattern_def_line_get_id_code
         if (line->id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the id_code member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
