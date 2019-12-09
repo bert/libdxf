3b492b61ba9ee54603e58c565b22ddc6c62be3ea
bert <bert.timmerman@xs4all.nl>
2019-12-09 20:36:22 +0100

    HATCH: correction in dxf_hatch_boundary_path_get_next().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -6367,7 +6367,7 @@ dxf_hatch_boundary_path_get_next
         if (path->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
