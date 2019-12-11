65b59aa49bb7541ff6e81b49a94fbd844c879951
bert <bert.timmerman@xs4all.nl>
2019-12-11 22:54:49 +0100

    HATCH: correction in dxf_hatch_boundary_path_polyline_get_last().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -7325,7 +7325,7 @@ dxf_hatch_boundary_path_polyline_get_last
         if (polyline->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return ((DxfHatchBoundaryPathPolyline *) polyline);
         }
