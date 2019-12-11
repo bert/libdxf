7a1cd64a31e07563b2e3651186570f00d123ca4c
bert <bert.timmerman@xs4all.nl>
2019-12-11 22:57:02 +0100

    HATCH: correction in dxf_hatch_boundary_path_polyline_vertex_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -7407,7 +7407,7 @@ dxf_hatch_boundary_path_polyline_vertex_init
         if (vertex == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () could not allocate memory for a DxfHatchBoundaryPathPolylineVertex struct.\n")),
+                  (_("Error in %s () could not allocate memory.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
