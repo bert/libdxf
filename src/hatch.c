d69c09c3e8a89d01123dc9655a481ab42b519104
bert <bert.timmerman@xs4all.nl>
2019-12-14 23:30:24 +0100

    HATCH: corrections in dxf_hatch_boundary_path_edge_arc_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -9007,9 +9007,8 @@ dxf_hatch_boundary_path_edge_arc_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         arc->id_code = id_code;
 #if DEBUG
