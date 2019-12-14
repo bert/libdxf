78e522c69c1b0ebe0ca8cf5b33ec28e9d30ae068
bert <bert.timmerman@xs4all.nl>
2019-12-14 22:30:45 +0100

    HATCH: correction in dxf_hatch_boundary_path_edge_arc_new().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -8805,7 +8805,7 @@ dxf_hatch_boundary_path_edge_arc_new ()
         if ((arc = malloc (size)) == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () could not allocate memory for a DxfHatchBoundaryPathEdgeArc struct.\n")),
+                  (_("Error in %s () could not allocate memory.\n")),
                   __FUNCTION__);
                 arc = NULL;
         }
