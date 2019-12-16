60146b0ed4818192c503a0d90a5ca208fc4dd55e
bert <bert.timmerman@xs4all.nl>
2019-12-16 22:26:49 +0100

    HATCH: correction in dxf_hatch_boundary_path_edge_ellipse_set_is_ccw().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -10637,7 +10637,7 @@ dxf_hatch_boundary_path_edge_ellipse_set_is_ccw
         if (is_ccw < 0)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a negative is_ccw value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
         }
         ellipse->is_ccw = is_ccw;
