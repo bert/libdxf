ac87634f8d407a344e1b4e8d964841ba9838d14f
bert <bert.timmerman@xs4all.nl>
2019-12-17 21:53:04 +0100

    HATCH: correction in dxf_hatch_boundary_path_edge_line_init().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -10842,7 +10842,7 @@ dxf_hatch_boundary_path_edge_line_init
         if (line == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () could not allocate memory for a DxfHatchBoundaryPathEdgeLine struct.\n")),
+                  (_("Error in %s () could not allocate memory.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
