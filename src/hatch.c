bd6721fad5b2287e04793d49cdadfd5b629f1fa2
bert <bert.timmerman@xs4all.nl>
2019-12-18 22:34:42 +0100

    HATCH: correction in dxf_hatch_boundary_path_edge_line_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -10962,9 +10962,8 @@ dxf_hatch_boundary_path_edge_line_get_id_code
         if (line->id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
