bca27af1687732a2820e89b688a05a3776480dca
bert <bert.timmerman@xs4all.nl>
2019-12-19 06:41:03 +0100

    3DFACE: added a flow diagram for dxf_3dface_set_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -942,6 +942,9 @@ dxf_3dface_get_linetype
 /*!
  * \brief Set the \c linetype for a DXF \c 3DFACE entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_3dface_set_linetype.png
+ *
  * \return a pointer to \c face when sucessful, \c NULL when an error
  * occurred.
  *
