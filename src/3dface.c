d115d39f08c1362a2a9141f8329fcd8f30bf5379
bert <bert.timmerman@xs4all.nl>
2019-12-23 23:10:53 +0100

    3DFACE: added a flow diagram for dxf_3dface_set_p1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -2954,6 +2954,9 @@ dxf_3dface_get_p1
 /*!
  * \brief Set the first alignment point \c p1 of a DXF \c 3DFACE entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_3dface_set_p1.png
+ *
  * \return a pointer to \c face when successful, or \c NULL when an
  * error occurred.
  */
