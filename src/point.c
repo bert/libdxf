369d25d6815198bdd1552f848af641b52d938a99
bert <bert.timmerman@xs4all.nl>
2020-01-02 11:50:55 +0100

    POINT: correction in dxf_point_set_linetype_scale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/point.c
+++ src/point.c
@@ -1205,9 +1205,8 @@ dxf_point_set_linetype_scale
         if (linetype_scale < 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative linetype scale value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         point->linetype_scale = linetype_scale;
 #if DEBUG
