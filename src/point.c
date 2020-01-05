13bdce779d3c598f9532eed0d6aad7da8c4dc204
bert <bert.timmerman@xs4all.nl>
2020-01-05 09:59:49 +0100

    POINT: corrections in dxf_point_get_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/point.c
+++ src/point.c
@@ -2397,8 +2397,6 @@ dxf_point_get_y0
 #if DEBUG
         DXF_DEBUG_BEGIN
 #endif
-        double result;
-
         /* Do some basic checks. */
         if (point == NULL)
         {
@@ -2407,11 +2405,10 @@ dxf_point_get_y0
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
-        result = point->y0;
 #if DEBUG
         DXF_DEBUG_END
 #endif
-        return (result);
+        return (point->y0);
 }
 
 
