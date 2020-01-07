941c65982df66f33d531e57cf4b18485992d4fbb
bert <bert.timmerman@xs4all.nl>
2020-01-07 22:17:07 +0100

    LINE: added an object_owner_soft member to dxf_line_struct.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.h
+++ src/line.h
@@ -137,6 +137,10 @@ dxf_line_struct
                 /*!< Soft-pointer ID/handle to owner dictionary (optional).\n
                  * Group code = 330.\n
                  * \since Introduced in version R14. */
+        char *object_owner_soft;
+                /*!< Soft-pointer ID/handle  to owner BLOCK_RECORD object.\n
+                 * Group code = 330.\n
+                 * \since Introduced in version R2000. */
         char *material;
                 /*!< Hard-pointer ID/handle to material object (present if
                  * not BYLAYER).\n
