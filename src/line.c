930cc2bf9ab750ca853050fb8e2f8275b2826200
bert <bert.timmerman@xs4all.nl>
2020-01-08 21:57:08 +0100

    LINE: correction in dxf_line_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -825,9 +825,8 @@ dxf_line_get_id_code
         if (line->id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the id-code member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
