24d506cb254de68d47edc105cfceda1db694c288
bert <bert.timmerman@xs4all.nl>
2020-01-09 00:22:39 +0100

    LINE: corrections in dxf_line_get_linetype_scale().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -1184,9 +1184,8 @@ dxf_line_get_linetype_scale
         if (line->linetype_scale < 0.0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the linetype scale member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
