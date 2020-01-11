42788093fb29eba8425295486c1dac72e8034d1d
bert <bert.timmerman@xs4all.nl>
2020-01-11 21:16:06 +0100

    LINE: correction in dxf_line_get_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -1766,7 +1766,7 @@ dxf_line_get_dictionary_owner_soft
         if (line->dictionary_owner_soft ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_soft member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
