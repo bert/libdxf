7d71b324d3668fb297d75453a57b1abb2d5d4eef
bert <bert.timmerman@xs4all.nl>
2020-01-12 11:19:06 +0100

    LINE: correction in dxf_line_get_next().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/line.c
+++ src/line.c
@@ -3307,7 +3307,7 @@ dxf_line_get_next
         if (line->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
