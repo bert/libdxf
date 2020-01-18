737be15d5f05822ef2661c33207201209fb94fcc
bert <bert.timmerman@xs4all.nl>
2020-01-18 17:21:35 +0100

    ATTRIB: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/attrib.c
+++ src/attrib.c
@@ -2,7 +2,7 @@
  * \file attrib.c
  *
  * \author Copyright (C) 2008, 2010, 2012, 2013, 2014, 2015, 2016, 2017,
- * 2018, 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * 2018, 2019, 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF attribute entity (\c ATTRIB).
  *
