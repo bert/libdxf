a493bbef7fc0e91c1c6c22cb056418620abd1aa5
bert <bert.timmerman@xs4all.nl>
2020-01-18 22:17:08 +0100

    BLOCK_RECORD: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.c
+++ src/block_record.c
@@ -1,7 +1,7 @@
 /*!
  * \file block_record.c
  *
- * \author Copyright (C) 2015, 2016, 2017, 2018, 2019
+ * \author Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF block record symbol table entry
