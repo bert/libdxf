b0ec92e665b357e6a02c43c5b33bf8b4b216bac9
bert <bert.timmerman@xs4all.nl>
2020-01-20 21:30:46 +0100

    DICTIONARY: correction in dxf_dictionary_get_dictionary_owner_hard().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dictionary.c
+++ src/dictionary.c
@@ -619,7 +619,7 @@ dxf_dictionary_get_dictionary_owner_hard
         if (dictionary->dictionary_owner_hard ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_hard member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
