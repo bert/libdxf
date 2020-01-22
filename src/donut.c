e67bc1f5dbf656edac2dde96fd25c2cbea30e2af
bert <bert.timmerman@xs4all.nl>
2020-01-22 18:42:35 +0100

    DONUT: correction in dxf_donut_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -357,7 +357,7 @@ dxf_donut_free
         if (donut->next != NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () pointer to next was not NULL.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
