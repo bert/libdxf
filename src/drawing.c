004e9fb782b49f594a6d69367c71e579c97fcec4
bert <bert.timmerman@xs4all.nl>
2020-01-23 22:17:07 +0100

    DRAWING: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/drawing.c
+++ src/drawing.c
@@ -1,7 +1,8 @@
 /*!
  * \file drawing.c
  *
- * \author Copyright (C) 2016, 2018 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2016, 2018, 2020
+ * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a libDXF drawing.
  *
