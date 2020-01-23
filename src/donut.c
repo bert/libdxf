f96146b4e612d1d44132b100ce7a9ebbfbe4c2af
bert <bert.timmerman@xs4all.nl>
2020-01-23 21:01:32 +0100

    DONUT: correction in dxf_donut_get_plot_style_name().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -1718,7 +1718,7 @@ dxf_donut_get_plot_style_name
         if (donut->plot_style_name ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the plot_style_name member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
