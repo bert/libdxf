5eb044ed7375ca0e76da84e06b31b0c9fd9577f3
bert <bert.timmerman@xs4all.nl>
2020-01-23 21:08:33 +0100

    DONUT: correction in dxf_donut_get_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -2251,7 +2251,7 @@ dxf_donut_get_z0
         if (donut->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (EXIT_FAILURE);
         }
