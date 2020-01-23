b26beb1f9dda8c6a07cbf133c0e25c2a785bfa64
bert <bert.timmerman@xs4all.nl>
2020-01-23 06:48:16 +0100

    DONUT: corrections in dxf_donut_get_shadow_mode().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -1237,16 +1237,14 @@ dxf_donut_get_shadow_mode
         if (donut->shadow_mode < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the shadow_mode member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
         if (donut->shadow_mode > 3)
         {
                 fprintf (stderr,
-                  (_("Error in %s () an out of range value was found in the shadow_mode member.\n")),
+                  (_("Warning in %s () an out of range value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
