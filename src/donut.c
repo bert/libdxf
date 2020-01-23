f06f8c5db1f8bf48071853407e32bda515137be2
bert <bert.timmerman@xs4all.nl>
2020-01-23 21:07:41 +0100

    DONUT: correction in dxf_donut_set_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/donut.c
+++ src/donut.c
@@ -2211,7 +2211,7 @@ dxf_donut_set_y0
         if (donut->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
