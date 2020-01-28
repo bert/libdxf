0341d1f0f93e104a4ecda485aae535d17f4a59f1
bert <bert.timmerman@xs4all.nl>
2020-01-28 21:07:58 +0100

    HATCH: correction in dxf_hatch_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -585,7 +585,7 @@ dxf_hatch_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
         }
         hatch->id_code = id_code;
