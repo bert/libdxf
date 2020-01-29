2b7dc41d5e6e987d23683d92d164d59aa1925446
bert <bert.timmerman@xs4all.nl>
2020-01-29 06:24:17 +0100

    HATCH: correction in dxf_hatch_get_thickness().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -833,7 +833,7 @@ dxf_hatch_get_thickness
         if (hatch->thickness < 0.0)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a negative value was found in the thickness member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
         }
 #if DEBUG
