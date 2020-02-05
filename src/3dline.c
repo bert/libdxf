7f95cc048c1f4fd285240c174ff8093a30667054
bert <bert.timmerman@xs4all.nl>
2020-02-05 16:02:43 +0100

    3DLINE: added a flow diagram for dxf_3dline_get_graphics_data_size().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dline.c
+++ src/3dline.c
@@ -1523,6 +1523,9 @@ dxf_3dline_set_paperspace
  * \brief Get the \c graphics_data_size value from a DXF \c 3DLINE
  * entity.
  *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_3dline_get_graphics_data_size.png
+ *
  * \return \c graphics_data_size value when successful, or
  * \c EXIT_FAILURE when an error occurred.
  */
