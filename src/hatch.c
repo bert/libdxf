3a958ae2772cd1af2b7a3cb5c33e5d22638b5e6e
bert <bert.timmerman@xs4all.nl>
2020-02-08 22:43:50 +0100

    HATCH: corrections in dxf_hatch_boundary_path_edge_spline_cp_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -14162,9 +14162,8 @@ dxf_hatch_boundary_path_edge_spline_cp_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         control_point->id_code = id_code;
 #if DEBUG
