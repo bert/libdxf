6cd682e64c764fb9600c4fa349d6e491157deede
bert <bert.timmerman@xs4all.nl>
2020-02-17 23:36:55 +0100

    HATCH: correction in dxf_hatch_boundary_path_edge_spline_cp_get_last().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -14640,7 +14640,7 @@ dxf_hatch_boundary_path_edge_spline_cp_get_last
         if (control_point->next == NULL)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a NULL pointer was found in the next member.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return ((DxfHatchBoundaryPathEdgeSplineCp *) control_point);
         }
