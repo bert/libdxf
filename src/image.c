86869b6cd508d6090f5328925f819db51dc410bc
bert <bert.timmerman@xs4all.nl>
2020-02-23 22:39:09 +0100

    IMAGE: corrections in dxf_image_set_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -985,9 +985,8 @@ dxf_image_set_id_code
         if (id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative id-code value was passed.\n")),
+                  (_("Warning in %s () a negative value was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
         }
         image->id_code = id_code;
 #if DEBUG
