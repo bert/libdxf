51f650b9a475429042feb203241a2f2f4ec2dbf3
bert <bert.timmerman@xs4all.nl>
2020-02-23 23:23:49 +0100

    IMAGE: correction in dxf_image_get_linetype().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -1022,7 +1022,7 @@ dxf_image_get_linetype
         if (image->linetype ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the linetype member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
