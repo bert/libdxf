eb9be9021af29deaace29c6b872a7ba4eff19b0a
bert <bert.timmerman@xs4all.nl>
2020-02-24 22:41:32 +0100

    IMAGE: corrections in dxf_image_get_visibility().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -1398,16 +1398,14 @@ dxf_image_get_visibility
         if (image->visibility < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the visibility member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
         if (image->visibility > 1)
         {
                 fprintf (stderr,
-                  (_("Error in %s () an out of range value was found in the visibility member.\n")),
+                  (_("Warning in %s () an out of range value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
