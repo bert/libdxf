f3bd789b877102b0bf2f838bd82ec7ddca26453f
bert <bert.timmerman@xs4all.nl>
2020-02-26 22:20:03 +0100

    IMAGE: correction in dxf_image_get_dictionary_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -1923,7 +1923,7 @@ dxf_image_get_dictionary_owner_soft
         if (image->dictionary_owner_soft ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_soft member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
