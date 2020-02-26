049e469f859095a36b7ac20644bf4d8c2d09fcb0
bert <bert.timmerman@xs4all.nl>
2020-02-26 22:23:52 +0100

    IMAGE: correction in dxf_image_get_plot_style_name().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -2315,7 +2315,7 @@ dxf_image_get_plot_style_name
         if (image->plot_style_name ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the plot_style_name member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
