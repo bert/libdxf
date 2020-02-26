c301acfcdf390aae830e2f9c6e0e99408d8e0bb5
bert <bert.timmerman@xs4all.nl>
2020-02-26 22:22:40 +0100

    IMAGE: correction in dxf_image_get_dictionary_owner_hard().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -2169,7 +2169,7 @@ dxf_image_get_dictionary_owner_hard
         if (image->dictionary_owner_hard ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_hard member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
