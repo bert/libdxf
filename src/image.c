367208acd24501fa56846be29b54cfefcd4b42d2
bert <bert.timmerman@xs4all.nl>
2020-02-28 20:42:21 +0100

    IMAGE: corrections in dxf_image_get_contrast().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -4565,13 +4565,13 @@ dxf_image_get_contrast
         if (image->contrast < 0)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () a negative value was found in the contrast member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
         }
         if (image->contrast > 100)
         {
                 fprintf (stderr,
-                  (_("Warning in %s () an out of range value was found in the contrast member.\n")),
+                  (_("Warning in %s () an out of range value was found.\n")),
                   __FUNCTION__);
         }
 #if DEBUG
