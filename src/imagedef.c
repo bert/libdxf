e8d0ce5aee5828af9ee25e5ea0f128985e8dddeb
bert <bert.timmerman@xs4all.nl>
2020-05-31 10:40:15 +0200

    IMAGEDEF: correction in dxf_imagedef_get_dictionary_owner_hard().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef.c
+++ src/imagedef.c
@@ -722,7 +722,7 @@ dxf_imagedef_get_dictionary_owner_hard
         if (imagedef->dictionary_owner_hard ==  NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found in the dictionary_owner_hard member.\n")),
+                  (_("Error in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
                 return (NULL);
         }
