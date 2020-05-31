6713d37332ff38c0a571d70f612d70f8339dfcb0
bert <bert.timmerman@xs4all.nl>
2020-05-31 10:25:15 +0200

    IMAGEDEF: correction in dxf_imagedef_get_id_code().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/imagedef.c
+++ src/imagedef.c
@@ -556,9 +556,8 @@ dxf_imagedef_get_id_code
         if (imagedef->id_code < 0)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a negative value was found in the id_code member.\n")),
+                  (_("Warning in %s () a negative value was found.\n")),
                   __FUNCTION__);
-                return (EXIT_FAILURE);
         }
 #if DEBUG
         DXF_DEBUG_END
