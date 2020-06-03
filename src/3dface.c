a2d549b282aa62117089e5614f076b7d9120ce66
bert <bert.timmerman@xs4all.nl>
2020-06-03 22:38:41 +0200

    3DFACE: corrections in dxf_3dface_set_y3().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -4079,9 +4079,18 @@ dxf_3dface_set_y3
         if (face->p3 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                face->p3 = dxf_point_init (face->p3);
+                if (face->p3 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         face->p3->y0 = y3;
 #if DEBUG
