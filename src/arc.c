6a5f3d18ed1255f48c8878202b0f038eb619c0d9
bert <bert.timmerman@xs4all.nl>
2020-06-06 20:22:55 +0200

    ARC: corrections in dxf_arc_set_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/arc.c
+++ src/arc.c
@@ -3050,9 +3050,18 @@ dxf_arc_set_z0
         if (arc->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                arc->p0 = dxf_point_init (arc->p0);
+                if (arc->p0 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         arc->p0->z0 = z0;
 #if DEBUG
