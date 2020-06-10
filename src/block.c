20620ee964b653096d953aea9e08ecebf5089709
bert <bert.timmerman@xs4all.nl>
2020-06-10 22:49:07 +0200

    BLOCK: corrections in dxf_block_set_y0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block.c
+++ src/block.c
@@ -1360,9 +1360,18 @@ dxf_block_set_y0
         if (block->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                block->p0 = dxf_point_init (block->p0);
+                if (block->p0 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         block->p0->y0 = y0;
 #if DEBUG
