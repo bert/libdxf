bff90b5a7ebf938bd9a7766cd558b83ddbe89394
bert <bert.timmerman@xs4all.nl>
2020-06-11 23:18:03 +0200

    BODY: corrections in dxf_body_set_binary_graphics_data().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/body.c
+++ src/body.c
@@ -1739,6 +1739,22 @@ dxf_body_set_binary_graphics_data
                   __FUNCTION__);
                 return (NULL);
         }
+        if (body->binary_graphics_data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                fprintf (stderr,
+                  (_("Initializing a DxfBinaryData struct.\n")));
+                body->binary_graphics_data = dxf_binary_data_init (body->binary_graphics_data);
+                if (body->binary_graphics_data == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
+        }
         body->binary_graphics_data = (DxfBinaryData *) data;
 #if DEBUG
         DXF_DEBUG_END
