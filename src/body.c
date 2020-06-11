a9626fe73e68cc6f1810673909ba6777d1494c64
bert <bert.timmerman@xs4all.nl>
2020-06-11 23:23:59 +0200

    BODY: corrections in dxf_body_set_proprietary_data().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/body.c
+++ src/body.c
@@ -2512,6 +2512,22 @@ dxf_body_set_proprietary_data
                   __FUNCTION__);
                 return (NULL);
         }
+        if (body->proprietary_data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                fprintf (stderr,
+                  (_("Initializing a DxfBinaryData struct.\n")));
+                body->proprietary_data = dxf_binary_data_init (body->proprietary_data);
+                if (body->proprietary_data == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
+        }
         body->proprietary_data = proprietary_data;
 #if DEBUG
         DXF_DEBUG_END
