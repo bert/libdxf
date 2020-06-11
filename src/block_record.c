1bb516a302ee75ef1ed1fafc8fb5813eb2540630
bert <bert.timmerman@xs4all.nl>
2020-06-11 23:10:13 +0200

    BLOCK_RECORD: corrections in dxf_block_record_set_binary_graphics_data().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/block_record.c
+++ src/block_record.c
@@ -1256,6 +1256,22 @@ dxf_block_record_set_binary_graphics_data
                   __FUNCTION__);
                 return (NULL);
         }
+        if (block_record->binary_graphics_data == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                fprintf (stderr,
+                  (_("Initializing a DxfBinaryData struct.\n")));
+                block_record->binary_graphics_data = dxf_binary_data_init (block_record->binary_graphics_data);
+                if (block_record->binary_graphics_data == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
+        }
         block_record->binary_graphics_data = (DxfBinaryData *) data;
 #if DEBUG
         DXF_DEBUG_END
