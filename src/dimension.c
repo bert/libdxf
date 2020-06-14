e6f3ba656b0f5b3dc666e6ad06c2b6a0a03a6b6d
bert <bert.timmerman@xs4all.nl>
2020-06-14 18:26:31 +0200

    DIMENSION: correction in dxf_dimension_set_x2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimension.c
+++ src/dimension.c
@@ -4043,9 +4043,18 @@ dxf_dimension_set_x2
         if (dimension->p2 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                dimension->p2 = dxf_point_init (dimension->p2);
+                if (dimension->p2 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         dimension->p2->x0 = x2;
 #if DEBUG
