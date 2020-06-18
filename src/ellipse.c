9b0245c4b4771588b0794d86bdc86494ceb22f9d
bert <bert.timmerman@xs4all.nl>
2020-06-18 22:29:31 +0200

    ELLIPSE: corrections in dxf_ellipse_set_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/ellipse.c
+++ src/ellipse.c
@@ -3078,9 +3078,18 @@ dxf_ellipse_set_y1
         if (ellipse->p1 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                ellipse->p1 = dxf_point_init (ellipse->p1);
+                if (ellipse->p1 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         ellipse->p1->y0 = y1;
 #if DEBUG
