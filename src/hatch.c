22be8e9618e3bf3bc5a01dc75420fa9650816117
bert <bert.timmerman@xs4all.nl>
2020-06-18 22:47:10 +0200

    HATCH: corrections in dxf_hatch_set_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/hatch.c
+++ src/hatch.c
@@ -2427,9 +2427,18 @@ dxf_hatch_set_z0
         if (hatch->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                hatch->p0 = dxf_point_init (hatch->p0);
+                if (hatch->p0 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         hatch->p0->z0 = z0;
 #if DEBUG
