abd69c0e1788527f43b5df06dce52d2572cb5b7d
bert <bert.timmerman@xs4all.nl>
2020-06-20 22:28:11 +0200

    HELIX: corrections in dxf_helix_set_z2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/helix.c
+++ src/helix.c
@@ -3420,6 +3420,22 @@ dxf_helix_set_z2
                   __FUNCTION__);
                 return (NULL);
         }
+        if (helix->p2 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                helix->p2 = dxf_point_init (helix->p2);
+                if (helix->p2 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
+        }
         helix->p2->z0 = z2;
 #if DEBUG
         DXF_DEBUG_END
