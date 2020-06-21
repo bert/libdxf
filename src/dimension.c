164cfba82c2e0cf6664a55bb47a0d52a9cc853a1
bert <bert.timmerman@xs4all.nl>
2020-06-21 16:33:41 +0200

    DIMENSION: corrections in dxf_dimension_set_binary_graphics_data().
    Removed obsolete code.
    
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/dimension.c
+++ src/dimension.c
@@ -2243,22 +2243,6 @@ dxf_dimension_set_binary_graphics_data
                   __FUNCTION__);
                 return (NULL);
         }
-        if (dimension->binary_graphics_data == NULL)
-        {
-                fprintf (stderr,
-                  (_("Warning in %s () a NULL pointer was found.\n")),
-                  __FUNCTION__);
-                fprintf (stderr,
-                  (_("Initializing a DxfBinaryData struct.\n")));
-                dimension->binary_graphics_data = dxf_binary_data_init (dimension->binary_graphics_data);
-                if (dimension->binary_graphics_data == NULL)
-                {
-                        fprintf (stderr,
-                          (_("Error in %s () could not allocate memory.\n")),
-                          __FUNCTION__);
-                        return (NULL);
-                }
-        }
         dimension->binary_graphics_data = (DxfBinaryData *) data;
 #if DEBUG
         DXF_DEBUG_END
