071345ae05e4382773517252466ce7192a24476a
bert <bert.timmerman@xs4all.nl>
2020-06-22 22:10:02 +0200

    IMAGE: corrections in dxf_image_set_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -3003,9 +3003,18 @@ dxf_image_set_z0
         if (image->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                image->p0 = dxf_point_init (image->p0);
+                if (image->p0 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         image->p0->z0 = z0;
 #if DEBUG
