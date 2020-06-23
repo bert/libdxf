5cb085fbd80b5bed76e7305578cd62467e602b00
bert <bert.timmerman@xs4all.nl>
2020-06-23 21:52:09 +0200

    IMAGE: corrections in dxf_image_set_y4().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/image.c
+++ src/image.c
@@ -4231,9 +4231,18 @@ dxf_image_set_y4
         if (image->p4 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                image->p4 = dxf_point_init (image->p4);
+                if (image->p4 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         image->p4->y0 = y4;
 #if DEBUG
