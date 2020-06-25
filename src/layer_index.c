325f5b541f5db0ea22e7bc86f1cc9cdced5ba37a
bert <bert.timmerman@xs4all.nl>
2020-06-25 22:13:36 +0200

    LAYER_INDEX: bumped the copyright year.
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/layer_index.c
+++ src/layer_index.c
@@ -1,7 +1,7 @@
 /*!
  * \file layer_index.c
  *
- * \author Copyright (C) 2015, 2017, 2018, 2019
+ * \author Copyright (C) 2015, 2017, 2018, 2019, 2020
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF layer_index object (\c LAYER_INDEX).
