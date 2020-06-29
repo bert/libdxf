faec5054b419d744aafeb6690358d5533af37f86
bert <bert.timmerman@xs4all.nl>
2020-06-29 22:07:36 +0200

    LEADER: corrections in dxf_leader_set_x2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/leader.c
+++ src/leader.c
@@ -3670,9 +3670,18 @@ dxf_leader_set_x2
         if (leader->p2 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                leader->p2 = dxf_point_init (leader->p2);
+                if (leader->p2 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         leader->p2->x0 = x2;
 #if DEBUG
