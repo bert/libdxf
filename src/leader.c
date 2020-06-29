7c13c80d9ec8261bd5b829a2bc74867978eec02e
bert <bert.timmerman@xs4all.nl>
2020-06-29 22:01:30 +0200

    LEADER: corrections in dxf_leader_set_y1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/leader.c
+++ src/leader.c
@@ -3407,9 +3407,18 @@ dxf_leader_set_y1
         if (leader->p1 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                leader->p1 = dxf_point_init (leader->p1);
+                if (leader->p1 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         leader->p1->y0 = y1;
 #if DEBUG
