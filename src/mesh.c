eaebee77faf9e5abb2a03a9d1a3f7a3ef6f5801a
bert <bert.timmerman@xs4all.nl>
2020-07-06 23:30:00 +0200

    MESH: corrections in dxf_mesh_set_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mesh.c
+++ src/mesh.c
@@ -2795,9 +2795,18 @@ dxf_mesh_set_z0
         if (mesh->p0 == NULL)
         {
                 fprintf (stderr,
-                  (_("Error in %s () a NULL pointer was found.\n")),
+                  (_("Warning in %s () a NULL pointer was found.\n")),
                   __FUNCTION__);
-                return (NULL);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                mesh->p0 = dxf_point_init (mesh->p0);
+                if (mesh->p0 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
         }
         mesh->p0->z0 = z0;
 #if DEBUG
