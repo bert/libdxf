b5aee2d450bfa10d79f504b7daefd0a7d7cb961c
bert <bert.timmerman@xs4all.nl>
2020-07-07 22:18:07 +0200

    ATTRIB: corrections in dxf_attrib_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/attrib.c
+++ src/attrib.c
@@ -291,7 +291,7 @@ dxf_attrib_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &attrib->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &attrib->id_code);
                 }
                 else if (strcmp (temp_string, "6") == 0)
                 {
