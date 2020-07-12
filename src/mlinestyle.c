085514241ca7525e0b4e1292eaa304c02471bfdb
bert <bert.timmerman@xs4all.nl>
2020-07-12 21:33:42 +0200

    MLINESTYLE: correction in dxf_mlinestyle_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/mlinestyle.c
+++ src/mlinestyle.c
@@ -1,7 +1,7 @@
 /*!
  * \file mlinestyle.c
  *
- * \author Copyright (C) 2015, 2017, 2018, 2019
+ * \author Copyright (C) 2015, 2017, 2018, 2019, 2020
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF mlinestyle object (\c MLINESTYLE).
@@ -223,7 +223,7 @@ dxf_mlinestyle_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &mlinestyle->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &mlinestyle->id_code);
                 }
                 if (strcmp (temp_string, "6") == 0)
                 {
