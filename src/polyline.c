9b52807996ac710d47211d8adeb0a99a80561743
bert <bert.timmerman@xs4all.nl>
2020-07-13 22:29:27 +0200

    POLYLINE: correction in dxf_polyline_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/polyline.c
+++ src/polyline.c
@@ -2,7 +2,7 @@
  * \file polyline.c
  *
  * \author Copyright (C) 2008, 2010, 2012, 2014, 2015 2016, 2017, 2018,
- * 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * 2019, 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
@@ -220,7 +220,7 @@ dxf_polyline_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &polyline->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &polyline->id_code);
                 }
                 else if (strcmp (temp_string, "6") == 0)
                 {
