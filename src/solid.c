bc1b8ff3a6190e0acff95ca8a45cea43d1d6d655
bert <bert.timmerman@xs4all.nl>
2020-07-15 22:11:50 +0200

    SOLID: correction in dxf_solid_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.c
+++ src/solid.c
@@ -1,8 +1,8 @@
 /*!
  * \file solid.c
  *
- * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019
- * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019,
+ * 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
@@ -262,7 +262,7 @@ dxf_solid_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &solid->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &solid->id_code);
                 }
                 else if (strcmp (temp_string, "6") == 0)
                 {
