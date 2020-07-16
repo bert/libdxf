a2aa755d2911809005b745143a5fcc056dccdb1b
bert <bert.timmerman@xs4all.nl>
2020-07-16 22:50:29 +0200

    SURFACE: correction in dxf_surface_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/surface.c
+++ src/surface.c
@@ -1,7 +1,7 @@
 /*!
  * \file surface.c
  *
- * \author Copyright (C) 2019 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2019, 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF surface entity (\c SURFACE).
  *
@@ -266,7 +266,7 @@ dxf_surface_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &surface->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &surface->id_code);
                 }
                 else if (strcmp (temp_string, "6") == 0)
                 {
