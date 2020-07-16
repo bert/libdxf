d0e9d7e0a35cea151eca49f400877acb44db7537
bert <bert.timmerman@xs4all.nl>
2020-07-16 22:52:54 +0200

    TEXT: correction in dxf_text_read().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/text.c
+++ src/text.c
@@ -1,8 +1,8 @@
 /*!
  * \file text.c
  *
- * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019
- * by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019,
+ * 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
@@ -230,7 +230,7 @@ dxf_text_read
                         /* Now follows a string containing a sequential
                          * id number. */
                         (fp->line_number)++;
-                        fscanf (fp->fp, "%x\n", &text->id_code);
+                        fscanf (fp->fp, "%x\n", (uint *) &text->id_code);
                 }
                 else if (strcmp (temp_string, "6") == 0)
                 {
