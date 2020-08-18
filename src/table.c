76f67c993cb41f0b0ae2bb9ad46cbd69dc9453e5
bert <bert.timmerman@xs4all.nl>
2020-08-18 22:22:19 +0200

    TABLE: added dxf_table_get_elevation().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -1172,6 +1172,37 @@ dxf_table_set_layer
 
 
 /*!
+ * \brief Get the \c elevation a this DXF \c TABLE entity.
+ *
+ * \return \c elevation when successful, or \c EXIT_FAILURE when an
+ * error occurred.
+ */
+double
+dxf_table_get_elevation
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table->elevation);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
