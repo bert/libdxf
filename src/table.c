99bb4f23aa96099b33b43c69c4b721275b20d700
bert <bert.timmerman@xs4all.nl>
2020-08-26 21:42:40 +0200

    TABLE: added dxf_table_get_binary_graphics_data().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -1857,6 +1857,46 @@ dxf_table_set_shadow_mode
 
 
 /*!
+ * \brief Get the pointer to the \c binary_graphics_data from a DXF
+ * \c TABLE entity.
+ *
+ * \return pointer to the \c binary_graphics_data.
+ *
+ * \warning No checks are performed on the returned pointer.
+ */
+DxfBinaryData *
+dxf_table_get_binary_graphics_data
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (table->binary_graphics_data ==  NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return ((DxfBinaryData *) table->binary_graphics_data);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
--- src/table.h
+++ src/table.h
@@ -572,6 +572,7 @@ int32_t dxf_table_get_graphics_data_size (DxfTable *table);
 DxfTable *dxf_table_set_graphics_data_size (DxfTable *table, int32_t graphics_data_size);
 int16_t dxf_table_get_shadow_mode (DxfTable *table);
 DxfTable *dxf_table_set_shadow_mode (DxfTable *table, int16_t shadow_mode);
+DxfBinaryData *dxf_table_get_binary_graphics_data (DxfTable *table);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
