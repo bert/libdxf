7d783926fdfc88b5632adf814638968314488137
bert <bert.timmerman@xs4all.nl>
2020-09-06 22:49:36 +0200

    TABLE: added dxf_table_get_x0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -2790,6 +2790,45 @@ dxf_table_set_p0
 
 
 /*!
+ * \brief Get the X-value of the insertion point \c x0 of a DXF \c TABLE
+ * entity.
+ *
+ * \return the X-value of the insertion point \c x0.
+ */
+double
+dxf_table_get_x0
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+        if (table->p0 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table->p0->x0);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
