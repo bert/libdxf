48e6150751f690bf0fef8b2f5ba7946dc3aeb699
bert <bert.timmerman@xs4all.nl>
2020-09-27 20:34:50 +0200

    TABLE: added dxf_table_set_z1().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -3365,6 +3365,58 @@ dxf_table_get_z1
 
 
 /*!
+ * \brief Set the Z-value of the horizontal direction vector \c z1 of a
+ * DXF \c 3DFACE entity.
+ *
+ * \return a pointer to \c table when successful, or \c NULL when an
+ * error occurred.
+ */
+DxfTable *
+dxf_table_set_z1
+(
+        DxfTable *table,
+                /*!< a pointer to a DXF \c TABLE entity. */
+        double z1
+                /*!< the Z-value of the horizontal direction vector
+                 * \c z1 of a DXF \c TABLE entity. */
+)
+{
+#ifdef DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (table->p1 == NULL)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a NULL pointer was found.\n")),
+                  __FUNCTION__);
+                fprintf (stderr,
+                  (_("Initializing a DxfPoint.\n")));
+                table->p1 = dxf_point_init (table->p1);
+                if (table->p1 == NULL)
+                {
+                        fprintf (stderr,
+                          (_("Error in %s () could not allocate memory.\n")),
+                          __FUNCTION__);
+                        return (NULL);
+                }
+        }
+        table->p1->z0 = z1;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
--- src/table.h
+++ src/table.h
@@ -596,6 +596,7 @@ DxfTable *dxf_table_set_x1 (DxfTable *table, double x1);
 double dxf_table_get_y1 (DxfTable *table);
 DxfTable *dxf_table_set_y1 (DxfTable *table, double y1);
 double dxf_table_get_z1 (DxfTable *table);
+DxfTable *dxf_table_set_z1 (DxfTable *table, double z1);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
