2f55b561b8e13dd619d9d2a99d7dbe9d04ded6ac
bert <bert.timmerman@xs4all.nl>
2020-10-03 21:26:08 +0200

    TABLE: added dxf_table_set_table_cell_color_bg().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.h
+++ src/table.h
@@ -606,6 +606,7 @@ DxfTable *dxf_table_set_horizontal_cell_margin (DxfTable *table, double horizont
 double dxf_table_get_vertical_cell_margin (DxfTable *table);
 DxfTable *dxf_table_set_vertical_cell_margin (DxfTable *table, double vertical_cell_margin);
 int16_t dxf_table_get_table_cell_color_bg (DxfTable *table);
+DxfTable *dxf_table_set_table_cell_color_bg (DxfTable *table, int16_t table_cell_color_bg);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
