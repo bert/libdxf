827a2e5ba56fe032855c70f3ff8ce5392b4ede5d
bert <bert.timmerman@xs4all.nl>
2020-10-10 23:34:50 +0200

    TABLE: added dxf_table_set_table_cell_border_color_vertical().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -4123,6 +4123,54 @@ dxf_table_get_table_cell_border_color_vertical
 
 
 /*!
+ * \brief Set the \c table_cell_border_color_vertical for a DXF
+ * \c TABLE entity.
+ *
+ * \return a pointer to \c table when sucessful, \c NULL when an error
+ * occurred.
+ */
+DxfTable *
+dxf_table_set_table_cell_border_color_vertical
+(
+        DxfTable *table,
+                /*!< a pointer to a DXF \c TABLE entity. */
+        int16_t table_cell_border_color_vertical
+                /*!< the \c table_cell_border_color_vertical value to
+                 * be set for the entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (NULL);
+        }
+        if (table_cell_border_color_vertical < 0)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () a negative value was passed.\n")),
+                  __FUNCTION__);
+        }
+        if (table_cell_border_color_vertical > DXF_COLOR_INDEX_MAX_NUMBER_OF_COLORS)
+        {
+                fprintf (stderr,
+                  (_("Warning in %s () an out of range value was passed.\n")),
+                  __FUNCTION__);
+        }
+        table->table_cell_border_color_vertical = table_cell_border_color_vertical;
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
