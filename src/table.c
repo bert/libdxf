ead2200c060834624d2234dcf25f7cf385c4013e
bert <bert.timmerman@xs4all.nl>
2020-10-25 20:31:17 +0100

    TABLE: added dxf_table_get_table_value_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -4329,6 +4329,37 @@ dxf_table_set_flow_direction
 
 
 /*!
+ * \brief Get the \c table_value_flag from a DXF \c TABLE entity.
+ *
+ * \return \c table_value_flag if successful, or \c EXIT_FAILURE
+ * when an error occurred.
+ */
+int32_t
+dxf_table_get_table_value_flag
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table->table_value_flag);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
