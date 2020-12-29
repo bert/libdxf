1cbaf0d838f51d5e180225edb87e099604a8e062
Bert Timmerman <bert.timmerman@xs4all.nl>
2020-12-29 17:17:58 +0100

    TABLE: added dxf_table_get_border_color_override_flag().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.h
+++ src/table.h
@@ -625,6 +625,7 @@ int32_t dxf_table_get_number_of_columns (DxfTable *table);
 DxfTable *dxf_table_set_number_of_columns (DxfTable *table, int32_t number_of_columns);
 int32_t dxf_table_get_override_flag (DxfTable *table);
 DxfTable * dxf_table_set_override_flag (DxfTable *table, int32_t override_flag);
+int32_t dxf_table_get_border_color_override_flag (DxfTable *table);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
