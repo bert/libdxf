e285c403597634fea0bf98c6e0bd9a47dcc30065
bert <bert.timmerman@xs4all.nl>
2021-01-08 22:54:05 +0100

    TABLE: added dxf_table_get_table_cell_border_lineweight_right().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -5050,6 +5050,38 @@ dxf_table_set_table_cell_alignment
 
 
 /*!
+ *
+ * \brief Get the \c table_cell_border_lineweight_right from a DXF
+ * \c TABLE entity.
+ * \return \c table_cell_border_lineweight_right if successful, or
+ * \c EXIT_FAILURE when an error occurred.
+ */
+double
+dxf_table_get_table_cell_border_lineweight_right
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table->table_cell_border_lineweight_right);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
--- src/table.h
+++ src/table.h
@@ -639,6 +639,7 @@ double dxf_table_get_column_height (DxfTable *table);
 DxfTable *dxf_table_set_column_height (DxfTable *table, double column_height);
 int dxf_table_get_table_cell_alignment (DxfTable *table);
 DxfTable *dxf_table_set_table_cell_alignment (DxfTable *table, int table_cell_alignment);
+double dxf_table_get_table_cell_border_lineweight_right (DxfTable *table);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
