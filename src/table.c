d31d853da1d6e6d6d814b6391ee6dc256c840e09
bert <bert.timmerman@xs4all.nl>
2021-01-13 21:21:14 +0100

    TABLE: added dxf_table_get_tablestyle_object_pointer().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/table.c
+++ src/table.c
@@ -5384,6 +5384,38 @@ dxf_table_set_table_cell_color_fill_override
 
 
 /*!
+ *
+ * \brief Get the \c tablestyle_object_pointer from a DXF \c TABLE
+ * entity.
+ * \return \c tablestyle_object_pointer if successful, or
+ * \c EXIT_FAILURE when an error occurred.
+ */
+char *
+dxf_table_get_tablestyle_object_pointer
+(
+        DxfTable *table
+                /*!< a pointer to a DXF \c TABLE entity. */
+)
+{
+#if DEBUG
+        DXF_DEBUG_BEGIN
+#endif
+        /* Do some basic checks. */
+        if (table == NULL)
+        {
+                fprintf (stderr,
+                  (_("Error in %s () a NULL pointer was passed.\n")),
+                  __FUNCTION__);
+                return (EXIT_FAILURE);
+        }
+#if DEBUG
+        DXF_DEBUG_END
+#endif
+        return (table->tablestyle_object_pointer);
+}
+
+
+/*!
  * \brief Get the pointer to the next \c TABLE entity from a DXF 
  * \c TABLE entity.
  *
--- src/table.h
+++ src/table.h
@@ -649,6 +649,7 @@ int16_t dxf_table_get_suppress_header_row (DxfTable *table);
 DxfTable *dxf_table_set_suppress_header_row (DxfTable *table, int16_t suppress_header_row);
 int16_t dxf_table_get_table_cell_color_fill_override (DxfTable *table);
 DxfTable *dxf_table_set_table_cell_color_fill_override (DxfTable *table, int16_t table_cell_color_fill_override);
+char *dxf_table_get_tablestyle_object_pointer (DxfTable *table);
 DxfTable *dxf_table_get_next (DxfTable *table);
 DxfTable *dxf_table_set_next (DxfTable *table, DxfTable *next);
 DxfTable *dxf_table_get_last (DxfTable *table);
