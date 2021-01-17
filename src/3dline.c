653b1ec98bc5821aa3afdb36a2b3b16bd614efa2
bert <bert.timmerman@xs4all.nl>
2021-01-17 13:49:28 +0100

    3DLINE: added a flow diagram for dxf_3dline_set_object_owner_soft().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dline.c
+++ src/3dline.c
@@ -1980,6 +1980,9 @@ dxf_3dline_get_object_owner_soft
 /*!
  * \brief Set the pointer to the \c object_owner_soft for a DXF
  * \c 3DLINE entity.
+ *
+ * <b>Flow diagram:</b>
+ * \image html drakon/dxf_3dline_set_object_owner_soft.png
  */
 Dxf3dline *
 dxf_3dline_set_object_owner_soft
