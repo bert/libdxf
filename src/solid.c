549ea9e85b5dbff22b9a985e43d891552f569836
bert <bert.timmerman@xs4all.nl>
2021-06-11 22:46:18 +0200

    SOLID: correction in dxf_solid_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/solid.c
+++ src/solid.c
@@ -2,7 +2,7 @@
  * \file solid.c
  *
  * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019,
- * 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * 2020, 2021 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
@@ -823,7 +823,6 @@ dxf_solid_free
         free (solid->dictionary_owner_soft);
         free (solid->dictionary_owner_hard);
         free (solid);
-        solid = NULL;
 #if DEBUG
         DXF_DEBUG_END
 #endif
