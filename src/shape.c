683722612ac7b03607466f41ffbf9f5a89365641
bert <bert.timmerman@xs4all.nl>
2021-06-11 22:43:24 +0200

    SHAPE: correction in dxf_shape_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/shape.c
+++ src/shape.c
@@ -2,7 +2,7 @@
  * \file shape.c
  *
  * \author Copyright (C) 2008, 2010, 2012, 2014, 2015, 2017, 2018, 2019,
- * 2020 by Bert Timmerman <bert.timmerman@xs4all.nl>.
+ * 2020, 2021 by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \author Copyright (C) 2010 by Luis Matos <gass@otiliamatos.ath.cx>.
  *
@@ -753,7 +753,6 @@ dxf_shape_free
         free (shape->shape_name);
         dxf_point_free (shape->p0);
         free (shape);
-        shape = NULL;
 #if DEBUG
         DXF_DEBUG_END
 #endif
