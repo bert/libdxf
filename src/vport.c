974952fd940d5bb6ace2971ae22cdddc0d6a3296
bert <bert.timmerman@xs4all.nl>
2021-06-12 15:38:57 +0200

    VPORT: correction in dxf_vport_free().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/vport.c
+++ src/vport.c
@@ -1,7 +1,7 @@
 /*!
  * \file vport.c
  *
- * \author Copyright (C) 2015, 2017, 2018, 2019, 2020
+ * \author Copyright (C) 2015, 2017, 2018, 2019, 2020, 2021
  * by Bert Timmerman <bert.timmerman@xs4all.nl>.
  *
  * \brief Functions for a DXF Viewport symbol table entry (\c VPORT).
@@ -848,7 +848,6 @@ dxf_vport_free
         free (vport->dictionary_owner_soft);
         free (vport->dictionary_owner_hard);
         free (vport);
-        vport = NULL;
 #if DEBUG
         DXF_DEBUG_END
 #endif
