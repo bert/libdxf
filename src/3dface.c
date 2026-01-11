4e4c1dc524c340a6c30dc30d2e43a83804d18454
bert <bert.timmerman@xs4all.nl>
2026-01-11 23:09:54 +0100

    Corrections in documentation for dxf_3dface_get_p2().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dface.c
+++ src/3dface.c
@@ -3664,9 +3664,9 @@ int
 dxf_3dface_get_p2
 (
         Dxf3dface *face,
-                /*!< a pointer to a DXF \c 3DFACE entity. */
+                /*!< [in] a pointer to a DXF \c 3DFACE entity. */
         DxfPoint *point
-                /*!< a pointer to a DXF \c POINT entity. */
+                /*!< [out] a pointer to a DXF \c POINT entity. */
 )
 {
 #ifdef DEBUG
