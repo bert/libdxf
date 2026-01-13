4bd050f8410d932a89428d0b49eee2151a08ca89
bert <bert.timmerman@xs4all.nl>
2026-01-13 22:41:45 +0100

    API change for dxf_3dline_set_extr_z0().
    Signed-off-by: bert <bert.timmerman@xs4all.nl>

--- src/3dline.c
+++ src/3dline.c
@@ -3724,16 +3724,16 @@ dxf_3dline_get_extr_z0
  * \brief Set the Z-value of the extrusion vector of a DXF \c 3DLINE
  * entity.
  *
- * \return a pointer to a DXF \c 3DLINE entity when successful, \c NULL
- * when an error occurred.
+ * \return \c EXIT_SUCCESS when sucessful, \c EXIT_FAILURE when an error
+ * occurred.
  */
-Dxf3dline *
+int
 dxf_3dline_set_extr_z0
 (
         Dxf3dline *line,
-                /*!< a pointer to a DXF \c 3DLINE entity. */
+                /*!< [in,out] a pointer to a DXF \c 3DLINE entity. */
         double extr_z0
-                /*!< the Z-value of the extrusion vector of a DXF
+                /*!< [in] the Z-value of the extrusion vector of a DXF
                  * \c 3DLINE entity. */
 )
 {
@@ -3746,13 +3746,13 @@ dxf_3dline_set_extr_z0
                 fprintf (stderr,
                   (_("Error in %s () a NULL pointer was passed.\n")),
                   __FUNCTION__);
-                return (NULL);
+                return (EXIT_FAILURE);
         }
         line->extr_z0 = extr_z0;
 #if DEBUG
         DXF_DEBUG_END
 #endif
-        return (line);
+        return (EXIT_SUCCESS);
 }
 
 
--- src/3dline.h
+++ src/3dline.h
@@ -285,7 +285,7 @@ int dxf_3dline_set_extr_x0 (Dxf3dline *line, double extr_x0);
 int dxf_3dline_get_extr_y0 (Dxf3dline *line, double *extr_y0);
 int dxf_3dline_set_extr_y0 (Dxf3dline *line, double extr_y0);
 int dxf_3dline_get_extr_z0 (Dxf3dline *line, double *extr_z0);
-Dxf3dline *dxf_3dline_set_extr_z0 (Dxf3dline *line, double extr_z0);
+int dxf_3dline_set_extr_z0 (Dxf3dline *line, double extr_z0);
 DxfPoint *dxf_3dline_get_extrusion_vector_as_point (Dxf3dline *line);
 Dxf3dline *dxf_3dline_set_extrusion_vector_from_point (Dxf3dline *line, DxfPoint *point);
 Dxf3dline *dxf_3dline_set_extrusion_vector (Dxf3dline *line, double extr_x0, double extr_y0, double extr_z0);
